48abb4d2bc201c660b3b737bcf788730b7a72611
Marek Fišera <maraf@users.noreply.github.com>
2013-02-03 15:52:24 +0000

    Added support Nuget

--- Neptuo.DataAccess/Properties/AssemblyInfo.cs
+++ Neptuo.DataAccess/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Neptuo.DataAccess")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Abstraction for data access")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Neptuo")]
 [assembly: AssemblyProduct("Neptuo.DataAccess")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
