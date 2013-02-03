80ee37fc76a0d57c141540212ab039e7152c7355
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-02-03 10:53:55 +0000

    Neptuo.Bootstrap

---
+++ trunk/Neptuo.Bootstrap/BaseBootstraper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Bootstrap
+{
+    public abstract class BaseBootstraper : IBootstraper
+    {
+        private Func<Type, IBootstrapTask> factory;
+
+        protected List<Type> Tasks { get; private set; }
+
+        public BaseBootstraper(Func<Type, IBootstrapTask> factory)
+        {
+            this.factory = factory;
+
+            Tasks = new List<Type>();
+        }
+
+        public virtual void Initialize()
+        {
+            if (factory == null)
+                throw new ArgumentNullException("factory");
+
+            List<IBootstrapTask> instances = new List<IBootstrapTask>();
+            foreach (Type type in Tasks)
+            {
+                IBootstrapTask task = factory(type);
+                if (task != null)
+                    instances.Add(task);
+            }
+
+            foreach (IBootstrapTask task in instances)
+                task.Initialize();
+        }
+    }
+}
---
+++ trunk/Neptuo.Bootstrap/ManualBootstraper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Bootstrap
+{
+    public class ManualBootstraper : BaseBootstraper, IBootstraper
+    {
+        public ManualBootstraper(Func<Type, IBootstrapTask> factory)
+            : base(factory)
+        { }
+
+        public void Register(Type type)
+        {
+            Tasks.Add(type);
+        }
+
+        public void Register<T>()
+            where T : IBootstrapTask
+        {
+            Register(typeof(T));
+        }
+    }
+}
