830be1a49268ec02abb3bdfd36f93326cc9fbbb5
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-02-03 10:55:48 +0000

    Neptuo.DataAccess.EntityFramework

---
+++ trunk/Neptuo.DataAccess.EntityFramework/QueryResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.DataAccess.EntityFramework
+{
+    public class QueryResult<TEntity> : IQueryResult<TEntity>
+    {
+        public IEnumerable<TEntity> Items { get; protected set; }
+        public int TotalCount { get; protected set; }
+
+        public QueryResult(IEnumerable<TEntity> items, int totalCount)
+        {
+            Items = items;
+            TotalCount = totalCount;
+        }
+    }
+}
---
+++ trunk/Neptuo.DataAccess.EntityFramework/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.DataAccess.EntityFramework
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DbContext dbContext;
+
+        public UnitOfWorkState State { get; protected set; }
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            State = UnitOfWorkState.Started;
+        }
+
+        public void Commit()
+        {
+            if (State != UnitOfWorkState.Started)
+                throw new UnitOfWorkException("Unit of work can commit only from started state!");
+
+            dbContext.SaveChanges();
+            State = UnitOfWorkState.Commited;
+        }
+
+        public void Dispose()
+        {
+            State = UnitOfWorkState.Disposed;
+        }
+    }
+
+    public enum UnitOfWorkState
+    {
+        Started, Commited, Disposed
+    }
+}
