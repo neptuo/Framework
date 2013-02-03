3fee1248826f04196b3439bae3233ec93c294971
Marek Fišera <maraf@users.noreply.github.com>
2013-02-03 10:53:55 +0000

    Neptuo.Bootstrap

---
+++ Neptuo.Bootstrap/AutomaticBootstraper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Bootstrap
+{
+    public class AutomaticBootstraper : BaseBootstraper, IBootstraper
+    {
+        public AutomaticBootstraper(Func<Type, IBootstrapTask> factory)
+            : base(factory)
+        { }
+
+        public override void Initialize()
+        {
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                try
+                {
+                    foreach (Type type in assembly.GetTypes())
+                    {
+                        if (typeof(IBootstrapTask).IsAssignableFrom(type))
+                            Tasks.Add(type);
+                    }
+                }
+                catch (Exception) { }
+            }
+
+            base.Initialize();
+        }
+    }
+}
