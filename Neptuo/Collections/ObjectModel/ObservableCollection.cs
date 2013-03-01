a64cc88215712d1406c7286c0ee9c37dec41a9a9
Marek Fišera <maraf@users.noreply.github.com>
2013-03-01 23:09:08 +0000

    Neptuo 1.1.0: ObservableCollection

---
+++ Neptuo/Collections/ObjectModel/ObservableCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Neptuo.Collections.ObjectModel
+{
+    public class ObservableCollection<T> : System.Collections.ObjectModel.ObservableCollection<T>
+    {
+        public ObservableCollection() { }
+
+        public ObservableCollection(IEnumerable<T> items)
+        {
+            AddRange(items);
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            foreach (T item in items)
+                Add(item);
+        }
+
+        protected override void InsertItem(int index, T item)
+        {
+            base.InsertItem(index, item);
+            OnPropertyChanged(new PropertyChangedEventArgs("Count"));
+        }
+    }
+}
