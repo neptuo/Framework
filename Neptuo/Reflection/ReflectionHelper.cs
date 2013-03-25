60740f126dfeaf7b96c0a5938d1a32624d0e2ba7
Marek Fišera <maraf@users.noreply.github.com>
2013-03-25 14:38:42 +0000

    Fix ns

--- Neptuo/Reflection/ReflectionHelper.cs
+++ Neptuo/Reflection/ReflectionHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 
-namespace Neptuo.Web.Framework.Utils
+namespace Neptuo.Reflection
 {
     public static class ReflectionHelper
     {
