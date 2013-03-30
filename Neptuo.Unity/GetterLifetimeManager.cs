309d78660063bb72b0c95326d3e42ac8dfdc6859
Marek Fišera <maraf@users.noreply.github.com>
2013-03-30 20:11:01 +0000

    Neptuo.Unity

---
+++ Neptuo.Unity/GetterLifetimeManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Unity
+{
+    public class GetterLifetimeManager : TransientLifetimeManager
+    {
+        private Func<object> factory;
+
+        public GetterLifetimeManager(Func<object> factory)
+        {
+            this.factory = factory;
+        }
+
+        public override object GetValue()
+        {
+            return factory();
+        }
+    }
+}
