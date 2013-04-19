14bedd98473e5310015828aa515dae229fc9051d
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-04-19 10:12:31 +0000

    Ignore automatic constraint

---
+++ trunk/Neptuo.Bootstrap/IgnoreAutomaticConstraintAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Neptuo.Bootstrap.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Bootstrap
+{
+    public class IgnoreAutomaticConstraintAttribute : ConstraintAttribute, IBootstrapConstraint
+    {
+        public bool Satisfies(IBootstrapConstraintContext context)
+        {
+            return !(context.Bootstrapper is AutomaticBootstrapper);
+        }
+    }
+}
