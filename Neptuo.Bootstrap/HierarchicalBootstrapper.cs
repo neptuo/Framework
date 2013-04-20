be5eb9edc8463f96b325731c77631c3910ab2459
Marek Fišera <maraf@users.noreply.github.com>
2013-04-20 10:33:30 +0000

    HierarchicalBootstrapper (preview)

---
+++ Neptuo.Bootstrap/HierarchicalBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Bootstrap
+{
+    /// <summary>
+    /// TODO: Implement Hierarchical bootstrapper.
+    /// </summary>
+    internal class HierarchicalBootstrapper
+    {
+        // Tree like registering of tasks
+        // Add type or factory method
+        // Evaluate in order and levels
+    }
+}
