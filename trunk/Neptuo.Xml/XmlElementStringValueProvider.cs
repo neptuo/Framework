c44c68bc1dfdfe80f3b4140ee5fc2e48ff95a3d4
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-06-15 13:03:43 +0000

    Neptuo: StringConvert

---
+++ trunk/Neptuo.Xml/XmlElementStringValueProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Neptuo.Xml
+{
+    public class XmlElementStringValueProvider : IStringValueProvider<XmlElement>
+    {
+        public string GetValue(XmlElement model, string key)
+        {
+            string result = model.GetAttribute(key);
+            return String.IsNullOrEmpty(result) ? null : result;
+        }
+    }
+}
