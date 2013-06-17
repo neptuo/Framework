5eaf14775f1fc5cd06576e7288cae8cb3b0a7ec2
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-06-17 18:05:36 +0000

    Neptuo.Windows.Threading

---
+++ trunk/Neptuo.Windows.Threading/Properties/VersionInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Windows.Threading
+{
+    public class VersionInfo
+    {
+        internal const string Version = "1.0.0";
+
+        public static Version GetVersion()
+        {
+            return new Version(Version);
+        }
+    }
+}
