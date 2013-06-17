8ef8c1ba8fc465d5501c39e91372db843919ce68
Marek Fišera <maraf@users.noreply.github.com>
2013-06-17 18:05:36 +0000

    Neptuo.Windows.Threading

---
+++ Neptuo.Windows.Controls/Window.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using UIWindow = System.Windows.Window;
+
+namespace Neptuo.Windows.Controls
+{
+    public static class Window
+    {
+        public static Key GetCloseKey(DependencyObject obj)
+        {
+            return (Key)obj.GetValue(CloseKeyProperty);
+        }
+
+        public static void SetCloseKey(DependencyObject obj, Key value)
+        {
+            obj.SetValue(CloseKeyProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for CloseKey.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CloseKeyProperty =
+            DependencyProperty.RegisterAttached("CloseKey", typeof(Key), typeof(Window), new PropertyMetadata(OnCloseKeyChanged));
+
+        private static void OnCloseKeyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UIWindow window = (UIWindow)d;
+            window.PreviewKeyDown += window_PreviewKeyDown;
+        }
+
+        private static void window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            UIWindow window = (UIWindow)sender;
+            if (e.Key == GetCloseKey(window))
+                window.Close();
+        }
+    }
+}
---
+++ Neptuo.Windows.Threading/DispatcherHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Neptuo.Windows.Threading
+{
+    /// <summary>
+    /// Encapsulates usage of Dispatcher.
+    /// </summary>
+    public class DispatcherHelper
+    {
+        private const int intervalCheckDelay = 1000;
+
+        private DispatcherPriority priority;
+        private Dispatcher dispatcher;
+
+        /// <summary>
+        /// Creates new instance of helper.
+        /// </summary>
+        /// <param name="dispatcher">Dispatcher to use.</param>
+        /// <param name="priority">Priority of operations.</param>
+        public DispatcherHelper(Dispatcher dispatcher, DispatcherPriority priority = DispatcherPriority.Normal)
+        {
+            this.dispatcher = dispatcher;
+            this.priority = priority;
+        }
+
+        /// <summary>
+        /// Runs <paramref name="action"/>, eventually after <paramref name="delay"/>.
+        /// Parameter <paramref name="priority"/> overrides priority from ctor.
+        /// </summary>
+        /// <param name="action">Action to run.</param>
+        /// <param name="delay">Delay, before executing <paramref name="action"/>.</param>
+        /// <param name="priority">Priority override.</param>
+        public void Run(Action action, int delay = 0, DispatcherPriority priority = DispatcherPriority.Normal)
+        {
+            DispatcherHelper.Run(dispatcher, action, delay, priority);
+        }
+
+        /// <summary>
+        /// Runs <paramref name="action"/> on <paramref name="dispatcher"/>, eventually after <paramref name="delay"/>.
+        /// Parameter <paramref name="priority"/> sets action priority.
+        /// </summary>
+        /// <param name="dispatcher">Dispatcher to use.</param>
+        /// <param name="action">Action to run.</param>
+        /// <param name="delay">Delay, before executing <paramref name="action"/>.</param>
+        /// <param name="priority">Opeartion priority.</param>
+        public static void Run(Dispatcher dispatcher, Action action, int delay = 0, DispatcherPriority priority = DispatcherPriority.Normal)
+        {
+            ThreadStart start = delegate()
+            {
+                if (delay >= 0)
+                    Thread.Sleep(delay);
+
+                DispatcherOperation op = dispatcher.BeginInvoke(priority, action);
+                DispatcherOperationStatus status = op.Status;
+                while (status != DispatcherOperationStatus.Completed)
+                    status = op.Wait(TimeSpan.FromMilliseconds(intervalCheckDelay));
+            };
+            new Thread(start).Start();
+        }
+    }
+}
