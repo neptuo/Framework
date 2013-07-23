ea46802651b65ce9e90229e5be0ce7181d95047f
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-07-23 19:52:50 +0000

    Xml features

---
+++ trunk/Neptuo.Xml/XmlDocumentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Neptuo.Xml
+{
+    public static class XmlDocumentExtensions
+    {
+        public static XmlAttribute CreateAttribute(this XmlDocument document, string name, string value)
+        {
+            XmlAttribute attribute = document.CreateAttribute(name);
+            attribute.Value = value;
+            return attribute;
+        }
+    }
+}
---
+++ trunk/Neptuo.Xml/XmlElementExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Neptuo.Xml
+{
+    public static class XmlElementExtensions
+    {
+        public static XmlElement GetElementByTagName(this XmlElement element, string name)
+        {
+            return element.GetElementsByTagName(name).ToEnumerable().FirstOrDefault() as XmlElement;
+        }
+    }
+}
