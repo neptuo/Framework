a3ff3809fb23012e726f56c1db3b29fae9099733
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-08-01 14:50:38 +0000

    Fix MethodName in TypeHelper

--- trunk/Neptuo/Linq/Expressions/TypeHelper.cs
+++ trunk/Neptuo/Linq/Expressions/TypeHelper.cs
@@ -21,42 +21,42 @@ namespace Neptuo.Linq.Expressions
 
         public static string MethodName<T, TResult>(Expression<Func<T, Func<TResult>>> propertyGetter)
         {
-            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Arguments[2] as ConstantExpression).Value as MethodInfo).Name;
+            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Object as ConstantExpression).Value as MethodInfo).Name;
         }
 
         public static string MethodName<T, TParam1, TResult>(Expression<Func<T, Func<TParam1, TResult>>> propertyGetter)
         {
-            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Arguments[2] as ConstantExpression).Value as MethodInfo).Name;
+            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Object as ConstantExpression).Value as MethodInfo).Name;
         }
 
         public static string MethodName<T, TParam1, TParam2, TResult>(Expression<Func<T, System.Func<TParam1, TParam2, TResult>>> propertyGetter)
         {
-            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Arguments[2] as ConstantExpression).Value as MethodInfo).Name;
+            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Object as ConstantExpression).Value as MethodInfo).Name;
         }
 
         public static string MethodName<T>(Expression<Func<T, Action>> propertyGetter)
         {
-            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Arguments[2] as ConstantExpression).Value as MethodInfo).Name;
+            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Object as ConstantExpression).Value as MethodInfo).Name;
         }
 
         public static string MethodName<T, TParam1>(Expression<Func<T, Action<TParam1>>> propertyGetter)
         {
-            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Arguments[2] as ConstantExpression).Value as MethodInfo).Name;
+            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Object as ConstantExpression).Value as MethodInfo).Name;
         }
 
         public static string MethodName<T, TParam1, TParam2>(Expression<Func<T, Action<TParam1, TParam2>>> propertyGetter)
         {
-            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Arguments[2] as ConstantExpression).Value as MethodInfo).Name;
+            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Object as ConstantExpression).Value as MethodInfo).Name;
         }
 
         public static string MethodName<T, TParam1, TParam2, TParam3>(Expression<Func<T, Action<TParam1, TParam2, TParam3>>> propertyGetter)
         {
-            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Arguments[2] as ConstantExpression).Value as MethodInfo).Name;
+            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Object as ConstantExpression).Value as MethodInfo).Name;
         }
 
         public static string MethodName<T, TParam1, TParam2, TParam3, TParam4>(Expression<Func<T, Action<TParam1, TParam2, TParam3, TParam4>>> propertyGetter)
         {
-            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Arguments[2] as ConstantExpression).Value as MethodInfo).Name;
+            return (((((UnaryExpression)propertyGetter.Body).Operand as MethodCallExpression).Object as ConstantExpression).Value as MethodInfo).Name;
         }
     }
 }
