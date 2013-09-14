33b3aca83fab2b1b18c3ff4b602088f01bdabcc7
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-09-14 07:00:47 +0000

    SharpKitCompiler

---
+++ trunk/Neptuo.SharpKit.CodeGenerator/SharpKitProcessBuilder.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neptuo.SharpKit.CodeGenerator
+{
+    public class SharpKitProcessBuilder
+    {
+        private string executable;
+        private StringBuilder arguments = new StringBuilder();
+
+        public SharpKitProcessBuilder Executable(string executable)
+        {
+            this.executable = executable;
+            return this;
+        }
+
+        public SharpKitProcessBuilder AddReference(params string[] referencePath)
+        {
+            foreach (string reference in referencePath)
+                arguments.AppendFormat("/reference:\"{0}\" ", reference);
+                
+            return this;
+        }
+
+        public SharpKitProcessBuilder Rebuild()
+        {
+            arguments.Append("/rebuild ");
+            return this;
+        }
+
+        public SharpKitProcessBuilder OutputGeneratedJsFile(string jsPath)
+        {
+            arguments.AppendFormat("/OutputGeneratedJsFile:\"{0}\" ", jsPath);
+            return this;
+        }
+
+        public SharpKitProcessBuilder ManifestFile(string manifestPath)
+        {
+            arguments.AppendFormat("/ManifestFile:\"{0}\" ", manifestPath);
+            return this;
+        }
+
+        public SharpKitProcessBuilder AddSourceFile(params string[] sourceCodePath)
+        {
+            foreach (string sourceCode in sourceCodePath)
+                arguments.AppendFormat("\"{0}\" ", sourceCode);
+
+            return this;
+        }
+
+        public SharpKitProcessBuilder OutDll(string dllPath)
+        {
+            arguments.AppendFormat("/out:{0} ", dllPath);
+            return this;
+        }
+
+        public string ToCommandLine()
+        {
+            return String.Format("{0} {1}", Executable(), Arguments());
+        }
+
+        public string Executable()
+        {
+            return executable;
+        }
+
+        public string Arguments()
+        {
+            return arguments.ToString();
+        }
+    }
+}
