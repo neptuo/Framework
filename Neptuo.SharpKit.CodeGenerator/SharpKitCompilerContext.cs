4df6de8c86f204b984f35149fde5c7830ef6cfe4
Marek Fišera <maraf@users.noreply.github.com>
2013-09-14 07:00:47 +0000

    SharpKitCompiler

---
+++ Neptuo.SharpKit.CodeGenerator/SharpKitCompilerContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Neptuo.SharpKit.CodeGenerator
+{
+    public class SharpKitCompilerContext
+    {
+        public TextReader Input { get; private set; }
+        public TextWriter Output { get; private set; }
+
+        public SharpKitCompilerContext(TextReader input, TextWriter output)
+        {
+            Input = input;
+            Output = output;
+        }
+    }
+}
