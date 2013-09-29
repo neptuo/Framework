a765877fbe9f0c56b3ed8dcae3e5f891b4659634
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-09-29 20:16:12 +0000

    New Neptuo.Data layer (partial, missing query)

---
+++ trunk/Neptuo.Data/Commands/Validation/ICommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Data.Commands.Validation
+{
+    public interface ICommandValidator<TCommand, TResult>
+        where TCommand : ICommand
+        where TResult : IValidationResult
+    {
+        TResult Validate(TCommand command);
+    }
+}
---
+++ trunk/Neptuo.Data/Key.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Data
+{
+    public class Key
+    {
+        public int ID { get; protected set; }
+        public int Version { get; protected set; }
+        public string EntityUri { get; protected set; }
+
+        public Key(int id, int version, string entityUri = null)
+        {
+            ID = id;
+            Version = version;
+            EntityUri = entityUri;
+        }
+    }
+}
