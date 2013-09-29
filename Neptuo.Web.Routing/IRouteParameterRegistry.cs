0701c754d69604ff33d4ff3b93614adf509fbcf1
Marek Fišera <maraf@users.noreply.github.com>
2013-09-29 08:06:36 +0000

    Neptuo.Web.Routing: IRouteParameterFactory + case insensitive parameter finding

--- Neptuo.Web.Routing/IRouteParameterRegistry.cs
+++ Neptuo.Web.Routing/IRouteParameterRegistry.cs
@@ -9,5 +9,6 @@ namespace Neptuo.Web.Routing
     public interface IRouteParameterRegistry
     {
         void Add(string parameterName, IRouteParameter parameter);
+        void Add(string parameterName, IRouteParameterFactory factory);
     }
 }
