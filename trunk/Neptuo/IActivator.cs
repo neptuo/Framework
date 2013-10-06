e8611d9b1f728793bd24cc7639d04896a611a9b8
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-10-06 08:18:13 +0000

    Neptuo: IActivator

--- trunk/Neptuo/IActivator.cs
+++ trunk/Neptuo/IActivator.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Neptuo.Data
+namespace Neptuo
 {
-    public interface IObjectFactory<T>
+    public interface IActivator<T>
     {
         T Create();
     }
