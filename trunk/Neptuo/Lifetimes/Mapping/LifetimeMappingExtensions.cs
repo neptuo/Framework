a1cd9674bbb5a1b4b97e648c336ca18463b2d2e6
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-10-06 13:33:12 +0000

    Improved lifetime mapping

---
+++ trunk/Neptuo/Lifetimes/Mapping/LifetimeMappingExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Lifetimes.Mapping
+{
+    public static class LifetimeMappingExtensions
+    {
+        public static void Map<TBaseLifetimeManager, TLifetime>(this ILifetimeMapping<TBaseLifetimeManager> mapping, LifetimeMapperBase<TBaseLifetimeManager, TLifetime> mapper)
+        {
+            mapping.Map(typeof(TLifetime), mapper);
+        }
+    }
+}
