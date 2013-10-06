4619c546eb1478c365f327debbf1106452b18bd1
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-10-06 09:16:17 +0000

    Neptuo.Data: UnitOfWork

---
+++ trunk/Neptuo.Data.Entity/DbContextUnitOfWorkFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Data.Entity
+{
+    public class DbContextUnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        protected DbContext DbContext { get; private set; }
+
+        public DbContextUnitOfWorkFactory(DbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public IUnitOfWork Create()
+        {
+            return new DbContextUnitOfWork(DbContext);
+        }
+    }
+}
