7102bacca4956bb1dacccb2c3034f525b73f1c34
Marek Fišera <maraf@users.noreply.github.com>
2013-10-06 13:33:12 +0000

    Improved lifetime mapping

--- Neptuo/Properties/VersionInfo.cs
+++ Neptuo/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo
 {
     public static class VersionInfo
     {
-        internal const string Version = "2.1.0";
+        internal const string Version = "2.2.0";
 
         public static Version GetVersion()
         {
