31a51978a71bd37306285041dc996bd8ad587a73
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-10-11 16:34:36 +0000

    Neptuo.PresentationModels: ProxyModelDefinition

---
+++ trunk/Neptuo.PresentationModels/ProxyModelDefinition.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.PresentationModels
+{
+    public class ProxyModelDefinition : IModelDefinition
+    {
+        private string identifier;
+        private IEnumerable<IFieldDefinition> fields;
+        private IModelMetadataCollection metadata;
+
+        protected bool RequiresRefresh { get; set; }
+        protected IModelDefinition ModelDefinition { get; private set; }
+
+        public string Identifier
+        {
+            get
+            {
+                if (RequiresRefresh)
+                {
+                    identifier = RefreshIdentifier();
+                    RequiresRefresh = false;
+                }
+                return identifier;
+            }
+        }
+
+        public IEnumerable<IFieldDefinition> Fields
+        {
+            get
+            {
+                if (RequiresRefresh)
+                {
+                    fields = RefreshFields();
+                    RequiresRefresh = false;
+                }
+                return fields;
+            }
+        }
+
+        public IModelMetadataCollection Metadata
+        {
+            get
+            {
+                if (RequiresRefresh)
+                {
+                    metadata = RefreshMetadata();
+                    RequiresRefresh = false;
+                }
+                return metadata;
+            }
+        }
+
+        public ProxyModelDefinition(IModelDefinition modelDefinition)
+        {
+            if (modelDefinition == null)
+                throw new ArgumentNullException("modelDefinition");
+
+            ModelDefinition = modelDefinition;
+            RequiresRefresh = true;
+        }
+
+        protected virtual string RefreshIdentifier()
+        {
+            return ModelDefinition.Identifier;
+        }
+
+        protected virtual IEnumerable<IFieldDefinition> RefreshFields()
+        {
+            return ModelDefinition.Fields;
+        }
+
+        protected virtual IModelMetadataCollection RefreshMetadata()
+        {
+            return ModelDefinition.Metadata;
+        }
+    }
+}
