cd12066b699f52324b4373b23b6cec19e2a45937
Marek Fišera <maraf@users.noreply.github.com>
2013-11-10 10:38:51 +0000

    Neptuo.PresentationModels: BindingConverterCollection supports multiple converters for single type

--- Neptuo.PresentationModels/Properties/VersionInfo.cs
+++ Neptuo.PresentationModels/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo.PresentationModels
 {
     public static class VersionInfo
     {
-        internal const string Version = "3.0.0";
+        internal const string Version = "3.0.1";
 
         public static Version GetVersion()
         {
