1a90c7a3cd28541e01d69a4a4e4b3e8cb92fe069
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-11-10 09:38:19 +0000

    Neptuo.PresentationModels: Support for 'binding model getter'

---
+++ trunk/Neptuo.PresentationModels/BindingConverterCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.PresentationModels
+{
+    public class BindingConverterCollection : IBindingConverterCollection
+    {
+        protected Dictionary<IFieldType, IBindingConverter> Storage { get; private set; }
+
+        public BindingConverterCollection()
+        {
+            Storage = new Dictionary<IFieldType, IBindingConverter>();
+        }
+
+        public bool TryConvert(string sourceValue, IFieldDefinition targetField, out object targetValue)
+        {
+            IBindingConverter converter;
+            if (Storage.TryGetValue(targetField.FieldType, out converter) && converter.TryConvert(sourceValue, targetField, out targetValue))
+                return true;
+
+            targetValue = null;
+            return false;
+        }
+    }
+}
