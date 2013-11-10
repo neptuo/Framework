0613658f7b24f218963cebeb67c18519c36fe3cc
Marek Fišera <maraf@users.noreply.github.com>
2013-11-10 15:14:46 +0000

    Neptuo.PresentationModels.TypeModels: IModelValueProviderFactory

---
+++ Neptuo.PresentationModels.TypeModels/ReflectionModelValueProviderFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.PresentationModels.TypeModels
+{
+    public class ReflectionModelValueProviderFactory : IModelValueProviderFactory
+    {
+        public IModelValueProvider Create(object instance)
+        {
+            return new ReflectionModelValueProvider(instance);
+        }
+    }
+}
