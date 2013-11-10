c6829e9db1ac98911881c5218b5d34bad55c5357
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2013-11-10 10:38:51 +0000

    Neptuo.PresentationModels: BindingConverterCollection supports multiple converters for single type

--- trunk/Neptuo.PresentationModels/BindingConverterCollection.cs
+++ trunk/Neptuo.PresentationModels/BindingConverterCollection.cs
@@ -8,18 +8,41 @@ namespace Neptuo.PresentationModels
 {
     public class BindingConverterCollection : IBindingConverterCollection
     {
-        protected Dictionary<IFieldType, IBindingConverter> Storage { get; private set; }
+        protected Dictionary<IFieldType, List<IBindingConverter>> Storage { get; private set; }
 
         public BindingConverterCollection()
         {
-            Storage = new Dictionary<IFieldType, IBindingConverter>();
+            Storage = new Dictionary<IFieldType, List<IBindingConverter>>();
+        }
+
+        public void Add(IFieldType fieldType, IBindingConverter converter)
+        {
+            if (fieldType == null)
+                throw new ArgumentNullException("fieldType");
+
+            if (converter == null)
+                throw new ArgumentNullException("converter");
+
+            List<IBindingConverter> list;
+            if (!Storage.TryGetValue(fieldType, out list))
+            {
+                list = new List<IBindingConverter>();
+                Storage.Add(fieldType, list);
+            }
+            list.Add(converter);
         }
 
         public bool TryConvert(string sourceValue, IFieldDefinition targetField, out object targetValue)
         {
-            IBindingConverter converter;
-            if (Storage.TryGetValue(targetField.FieldType, out converter) && converter.TryConvert(sourceValue, targetField, out targetValue))
-                return true;
+            List<IBindingConverter> converters;
+            if (Storage.TryGetValue(targetField.FieldType, out converters))
+            {
+                foreach (IBindingConverter converter in converters)
+                {
+                    if (converter.TryConvert(sourceValue, targetField, out targetValue))
+                        return true;
+                }
+            }
 
             targetValue = null;
             return false;
