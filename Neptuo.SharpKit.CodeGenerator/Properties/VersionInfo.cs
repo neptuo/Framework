b6d26e29e15294ff2cc9119e2d76a6a521cc1137
Marek Fišera <maraf@users.noreply.github.com>
2014-02-02 13:57:24 +0000

    SharpKitCompiler.RemoveReference

--- Neptuo.SharpKit.CodeGenerator/Properties/VersionInfo.cs
+++ Neptuo.SharpKit.CodeGenerator/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo.SharpKit.CodeGenerator
 {
     public static class VersionInfo
     {
-        internal const string Version = "1.0.1";
+        internal const string Version = "1.1.1";
 
         public static Version GetVersion()
         {
