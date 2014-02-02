331ad0719c13150ef39204cabf38d508ed48f665
Marek Fišera <maraf@users.noreply.github.com>
2014-02-02 13:57:13 +0000

    Neptuo Clr -> StringWriter

---
+++ Neptuo.System.Client/System/IO/StringWriter.cs
@@ -0,0 +1,70 @@
+﻿using SharpKit.JavaScript;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Client.System.IO
+{
+    [JsType(Name = "System.IO.StringWriter", Filename = "~/res/System.IO.js")]
+    public class JsImplStringWriter
+    {
+        private JsArray<string> array;
+
+        public int Length { get; private set; }
+
+        public JsImplStringWriter()
+        {
+            array = new JsArray<string>();
+        }
+
+        public JsImplStringWriter(string s)
+        {
+            array = new JsArray<string>(s);
+            Length = s == null ? 0 : s.Length;
+        }
+
+        public void Write(char s)
+        {
+            Length++;
+            array.push(s.As<string>());
+        }
+
+        public void Write(string s)
+        {
+            if (s != null)
+            {
+                array.push(s);
+                Length += s.Length;
+            }
+        }
+
+        public void Write(object obj)
+        {
+            if (obj != null)
+            {
+                string s = obj.ToString();
+                array.push(s);
+                Length += s.Length;
+            }
+        }
+
+        [JsMethod(Name = "Append$$Object")]
+        public void Append(object obj)
+        {
+            if (obj != null)
+            {
+                string s = obj.ToString();
+                array.push(s);
+                Length += s.Length;
+            }
+        }
+
+        public override string ToString()
+        {
+            return array.join("");
+        }
+    }
+}
