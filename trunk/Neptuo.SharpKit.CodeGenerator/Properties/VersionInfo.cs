10624c04e24f0e2e4f10b713419445309df1e6c7
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2014-02-03 15:47:44 +0000

    SharpKitCompiler, support for plugins

--- trunk/Neptuo.SharpKit.CodeGenerator/Properties/VersionInfo.cs
+++ trunk/Neptuo.SharpKit.CodeGenerator/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo.SharpKit.CodeGenerator
 {
     public static class VersionInfo
     {
-        internal const string Version = "1.1.1";
+        internal const string Version = "1.2.0";
 
         public static Version GetVersion()
         {
