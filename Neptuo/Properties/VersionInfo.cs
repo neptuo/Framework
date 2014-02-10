b9389efe894bb2e73556595c423b72c188ac5d6b
Marek Fišera <maraf@users.noreply.github.com>
2014-02-10 10:37:44 +0000

    Guard.NotNull with ArgumentException

--- Neptuo/Properties/VersionInfo.cs
+++ Neptuo/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo
 {
     public static class VersionInfo
     {
-        internal const string Version = "2.6.0";
+        internal const string Version = "2.6.1";
 
         public static Version GetVersion()
         {
