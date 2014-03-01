e216f7788cfc78039617a9e295ca776f1374017e
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2014-03-01 15:27:03 +0000

    Neptuo.Converts

---
+++ trunk/Neptuo/ComponentModel/Converters/IConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.ComponentModel.Converters
+{
+    public interface IConverter
+    {
+        bool TryConvertGeneral(Type sourceType, Type targetType, object sourceValue, out object targetValue);
+    }
+
+    public interface IConverter<TSource, TTarget> : IConverter
+    {
+        bool TryConvert(TSource sourceValue, out TTarget targetValue);
+    }
+}
