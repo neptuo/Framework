86259356af2dcf18dc02a7e6a28af2353c2347c5
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2014-04-10 20:59:28 +0000

    Neptuo.Data.Entity: EntityQueryBase

---
+++ trunk/Neptuo.Data.Entity/Queries/EntityQueryBase.cs
@@ -0,0 +1,53 @@
+﻿using Neptuo.Data.Queries;
+using Neptuo.Linq.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Data.Entity.Queries
+{
+    public abstract class EntityQueryBase<TEntity, TFilter>
+        where TEntity : IKey<Key>
+    {
+        private TFilter filter;
+
+        public TFilter Filter
+        {
+            get
+            {
+                if (filter == null)
+                    filter = CreateFilter();
+
+                return filter;
+            }
+            set { filter = value; }
+        }
+
+        protected virtual IQueryable<TEntity> AppendWhere(IQueryable<TEntity> items)
+        {
+            ParameterExpression parameter = Expression.Parameter(typeof(TEntity));
+            Expression predicate = BuildWhereExpression(parameter);
+
+            if (predicate != null)
+            {
+                MethodCallExpression whereCallExpression = Expression.Call(
+                   typeof(Queryable),
+                   TypeHelper.MethodName<IQueryable<TEntity>, Expression<Func<TEntity, bool>>, IQueryable<TEntity>>(q => q.Where),
+                   new Type[] { typeof(TEntity) },
+                   items.Expression,
+                   Expression.Lambda<Func<TEntity, bool>>(predicate, new ParameterExpression[] { parameter })
+                );
+
+                return items.Provider.CreateQuery<TEntity>(whereCallExpression);
+            }
+            return items;
+        }
+
+        protected abstract Expression BuildWhereExpression(Expression parameter);
+
+        protected abstract TFilter CreateFilter();
+    }
+}
