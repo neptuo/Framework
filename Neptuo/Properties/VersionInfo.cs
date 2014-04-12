3dd878e604e6d13724a4f63a7bb1d4c048c48d7f
Marek Fišera <maraf@users.noreply.github.com>
2014-04-12 09:57:49 +0000

    Neptuo.Validation: ValidationException

--- Neptuo/Properties/VersionInfo.cs
+++ Neptuo/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo
 {
     public static class VersionInfo
     {
-        internal const string Version = "2.10.0";
+        internal const string Version = "2.11.0";
 
         public static Version GetVersion()
         {
---
+++ Neptuo/Validation/ValidationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Validation
+{
+    public class ValidationException : Exception
+    {
+        public IValidationResult Result { get; private set; }
+
+        public ValidationException(IValidationResult result)
+        {
+            Result = result;
+        }
+    }
+}
