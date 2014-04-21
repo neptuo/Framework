83733ccbc46e4926b27f4261fd8844841d76f6b2
Marek Fišera <maraf@users.noreply.github.com>
2014-04-21 19:28:43 +0000

    ObservableObject

---
+++ Neptuo/ComponentModel/ObservableObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.ComponentModel
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            Guard.NotNull(propertyName, "propertyName");
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
