69e3ade03e977d21c4af3df8d213cc8b2030baa2
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2014-05-22 09:57:22 +0000

    Added comments + IPoolCommandExecutorContext

--- trunk/Neptuo.Commands/Execution/ICommandHandlerAware.cs
+++ trunk/Neptuo.Commands/Execution/ICommandHandlerAware.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Commands.Execution
 {
+    /// <summary>
+    /// Describes object that has Command and associated CommandHandler.
+    /// </summary>
     public interface ICommandHandlerAware
     {
+        /// <summary>
+        /// Command handler to handle <see cref="Command"/>.
+        /// </summary>
         object CommandHandler { get; }
+
+        /// <summary>
+        /// Command to handle.
+        /// </summary>
         object Command { get; }
     }
 }
