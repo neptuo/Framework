821dfe55cea14cb481a94772ba8b33dca11bcf99
Marek Fišera <maraf@users.noreply.github.com>
2014-05-23 10:21:08 +0000

    Neptuo VersionInfo

--- Neptuo/Properties/VersionInfo.cs
+++ Neptuo/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo
 {
     public static class VersionInfo
     {
-        internal const string Version = "2.15.1";
+        internal const string Version = "2.15.2";
 
         public static Version GetVersion()
         {
