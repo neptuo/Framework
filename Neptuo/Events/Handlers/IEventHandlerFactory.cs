e6fd2953be8e8e65f197b93d7b659a6e2facc411
Marek Fišera <maraf@users.noreply.github.com>
2014-05-24 18:30:01 +0000

    Neptuo.Events: Non generic registration interface

--- Neptuo/Events/Handlers/IEventHandlerFactory.cs
+++ Neptuo/Events/Handlers/IEventHandlerFactory.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Events.Handlers
 {
-    public interface IEventHandlerFactory<TEvent>
+    /// <summary>
+    /// Marker interface for event handler factories.
+    /// </summary>
+    public interface IEventHandlerFactory
+    { }
+
+    /// <summary>
+    /// Event handler factory for events of type <typeparamref name="TEvent"/>.
+    /// </summary>
+    /// <typeparam name="TEvent">Type of event data.</typeparam>
+    public interface IEventHandlerFactory<TEvent> : IEventHandlerFactory
     {
+        /// <summary>
+        /// Creates event handler for <paramref name="eventData"/> of type <typeparamref name="TEvent"/>.
+        /// </summary>
+        /// <param name="eventData">Instance of event data.</param>
+        /// <param name="currentManager">Current event manager that invokes this method.</param>
+        /// <returns>Event handler for <paramref name="eventData"/>.</returns>
         IEventHandler<TEvent> CreateHandler(TEvent eventData, IEventManager currentManager);
     }
 }
--- Neptuo/Events/IEventDispatcher.cs
+++ Neptuo/Events/IEventDispatcher.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Events
 {
+    /// <summary>
+    /// Provides ability to publish events.
+    /// </summary>
     public interface IEventDispatcher
     {
+        /// <summary>
+        /// Publishes <paramref name="eventData"/> to event bus.
+        /// </summary>
+        /// <typeparam name="TEvent">Type of event data.</typeparam>
+        /// <param name="eventData">Instance of event data.</param>
         void Publish<TEvent>(TEvent eventData);
     }
 }
