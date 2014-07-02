382da43ed002275af31b87b356a476b2242a549a
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2014-07-02 10:16:58 +0000

    Neptuo.Web.Resources.Bundling + Reimplemented initialization of ResourceTable.

---
+++ trunk/Neptuo.Web.Resources.Bundling/Formatters/IBundlePathFormatter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Web.Resources.Bundling.Formatters
+{
+    /// <summary>
+    /// Converts resource names to virtual bundle paths.
+    /// </summary>
+    public interface IBundlePathFormatter
+    {
+        /// <summary>
+        /// Converts <paramref name="resourceName"/> to bundle virtual path for javascript file.
+        /// </summary>
+        /// <param name="resourceName">Registered resource name.</param>
+        /// <returns>Bundle virtual path for javascript file.</returns>
+        string FormatJavascriptPath(string resourceName);
+
+        /// <summary>
+        /// Converts <paramref name="resourceName"/> to bundle virtual path for stylesheet file.
+        /// </summary>
+        /// <param name="resourceName">Registered resource name.</param>
+        /// <returns>Bundle virtual path for stylesheet file.</returns>
+        string FormatStylesheetPath(string resourceName);
+    }
+}
---
+++ trunk/Neptuo.Web.Resources.Bundling/Formatters/StringBundlePathFormatter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Web.Resources.Bundling.Formatters
+{
+    /// <summary>
+    /// Formats bundle paths using two format strings with single argument.
+    /// </summary>
+    public class StringBundlePathFormatter : IBundlePathFormatter
+    {
+        private string javascriptFormat;
+        private string stylesheetFormat;
+
+        /// <summary>
+        /// Creates new instance using <paramref name="javascriptFormat"/> for formatting javascript bundle paths
+        /// and <paramref name="stylesheetFormat"/> for formatting stylesheet bundle paths.
+        /// </summary>
+        /// <param name="javascriptFormat">Format string with one argument for javascript bundle paths.</param>
+        /// <param name="stylesheetFormat">Format string with one argument for stylesheet bundle paths.</param>
+        public StringBundlePathFormatter(string javascriptFormat, string stylesheetFormat)
+        {
+            Guard.NotNullOrEmpty(javascriptFormat, "javascriptFormat");
+            Guard.NotNullOrEmpty(stylesheetFormat, "stylesheetFormat");
+            this.javascriptFormat = javascriptFormat;
+            this.stylesheetFormat = stylesheetFormat;
+        }
+
+        public string FormatJavascriptPath(string resourceName)
+        {
+            return String.Format(javascriptFormat, resourceName);
+        }
+
+        public string FormatStylesheetPath(string resourceName)
+        {
+            return String.Format(stylesheetFormat, resourceName);
+        }
+    }
+}
