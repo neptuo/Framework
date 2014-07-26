5bc3e03bc60e51474fca267f2849c7b5c99fa292
Marek Fišera <maraf@users.noreply.github.com>
2014-07-26 14:09:26 +0000

    Put a Delete behaviors + Extension to map them all.

---
+++ Neptuo.Web.Services/Hosting/Behaviors/Providers/InterfaceBehaviorProviderExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Web.Services.Hosting.Behaviors.Providers
+{
+    /// <summary>
+    /// <see cref="InterfaceBehaviorProvider"/> extensions for mapping http methods.
+    /// </summary>
+    public static class InterfaceBehaviorProviderExtensions
+    {
+        /// <summary>
+        /// Maps <typeparamref name="TBehavior"/> to <typeparamref name="TImplementation"/>.
+        /// </summary>
+        /// <typeparam name="TBehavior">Behavior definition.</typeparam>
+        /// <typeparam name="TImplementation">Behavior implementation.</typeparam>
+        /// <param name="provider">Interface behavior provider.</param>
+        /// <returns><paramref name="provider"/> (for fluency).</returns>
+        public static InterfaceBehaviorProvider AddMapping<TBehavior, TImplementation>(this InterfaceBehaviorProvider provider)
+            where TImplementation : IBehavior<TBehavior>
+        {
+            Guard.NotNull(provider, "provider");
+            return provider
+                .AddMapping(typeof(TBehavior), typeof(TImplementation));
+        }
+
+        /// <summary>
+        /// Maps known (get, post, put, delete) behaviors to its mappings.
+        /// </summary>
+        /// <param name="provider">Interface behavior provider.</param>
+        /// <returns><paramref name="provider"/> (for fluency).</returns>
+        public static InterfaceBehaviorProvider AddHttpMethods(this InterfaceBehaviorProvider provider)
+        {
+            Guard.NotNull(provider, "provider");
+            return provider
+                .AddMapping<IGet, GetBehavior>()
+                .AddMapping<IPost, PostBehavior>()
+                .AddMapping<IPut, PutBehavior>()
+                .AddMapping<IDelete, DeleteBehavior>();
+        }
+    }
+}
---
+++ Neptuo.Web.Services/Hosting/Behaviors/PutBehavior.cs
@@ -0,0 +1,29 @@
+﻿using Neptuo.Web.Services.Hosting.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Web.Services.Hosting.Behaviors
+{
+    /// <summary>
+    /// Executes <see cref="IPut"/> handler.
+    /// </summary>
+    public class PutBehavior : IBehavior<IPut>
+    {
+        /// <summary>
+        /// Executes <see cref="IPut.Put"/> method on <paramref name="handler"/> if current request is PUT request.
+        /// </summary>
+        /// <param name="handler">Behavior interface.</param>
+        /// <param name="context">Current Http context.</param>
+        /// <param name="pipeline">Processing pipeline.</param>
+        public void Execute(IPut handler, IHttpContext context, IBehaviorContext pipeline)
+        {
+            if (context.Request.Method == HttpMethod.Post)
+                handler.Put();
+            else
+                pipeline.Next();
+        }
+    }
+}
