7f45e93bdea420bd4bf029b313c4c0d83c5a6529
Marek Fišera <maraf@users.noreply.github.com>
2014-08-01 09:50:37 +0000

    Some comments.

--- Neptuo.Commands/ICommandDispatcher.cs
+++ Neptuo.Commands/ICommandDispatcher.cs
@@ -7,8 +7,15 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Commands
 {
+    /// <summary>
+    /// Main entry point to commanding infrastructure.
+    /// </summary>
     public interface ICommandDispatcher
     {
+        /// <summary>
+        /// Posts <paramref name="command"/> to command infrastructure for execution.
+        /// </summary>
+        /// <param name="command">Instance describing requested operation.</param>
         void Handle(object command);
     }
 }
