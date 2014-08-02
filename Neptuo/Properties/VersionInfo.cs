9db306c21c08ecf35aab860cbd75e5eb7b90c2b5
Marek Fišera <maraf@users.noreply.github.com>
2014-08-02 06:40:16 +0000

    Add Envelope to Neptuo.

--- Neptuo/Properties/VersionInfo.cs
+++ Neptuo/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo
 {
     public static class VersionInfo
     {
-        internal const string Version = "3.0.2";
+        internal const string Version = "3.0.3";
 
         public static Version GetVersion()
         {
