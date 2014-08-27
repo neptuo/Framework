db471dc30399a884d00f516561c0f53a14587d9d
Marek Fišera <maraf@users.noreply.github.com>
2014-08-27 18:51:49 +0000

    KeyValueCollection + IParametrizedActivator + Extensions for KeyValueCollection.

---
+++ Neptuo/Collections/Specialized/IReadOnlyKeyValueCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Collections.Specialized
+{
+    /// <summary>
+    /// Readonly version of keyed collection.
+    /// </summary>
+    public interface IReadOnlyKeyValueCollection
+    {
+        /// <summary>
+        /// Tries to get value associated with <paramref name="key"/>.
+        /// </summary>
+        /// <param name="key">Key which value should be returned.</param>
+        /// <param name="value">Output value associted with <paramref name="key"/>.</param>
+        /// <returns><c>true</c> if collection contains value with <paramref name="key"/> as key; <c>false</c> otherwise.</returns>
+        /// <exception cref="ArgumentNullException">When <paramref name="key"/> is <c>null</c>.</exception>
+        bool TryGet(string key, out object value);
+    }
+}
---
+++ Neptuo/Collections/Specialized/ReadOnlyKeyValueCollectionExtensions.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Collections.Specialized
+{
+    public static class ReadOnlyKeyValueCollectionExtensions
+    {
+        public static bool TryGet(this IKeyValueCollection collection, string key, out int intValue)
+        {
+            Guard.NotNull(collection, "collection");
+
+            object value;
+            if(collection.TryGet(key, out value))
+                return Converts.Try(value, out intValue);
+
+            intValue = default(int);
+            return false;
+        }
+
+        public static int Get(this IKeyValueCollection collection, string key, int? defaultValue)
+        {
+            int intValue;
+            if (TryGet(collection, key, out intValue))
+                return intValue;
+
+            if (defaultValue != null)
+                return defaultValue.Value;
+
+            throw new InvalidOperationException(String.Format("Collection doesn't contain value of type int with key '{0}'", key));
+        }
+
+        public static bool TryGet(this IKeyValueCollection collection, string key, out string stringValue)
+        {
+            Guard.NotNull(collection, "collection");
+
+            object value;
+            if (collection.TryGet(key, out value))
+                return Converts.Try(value, out stringValue);
+
+            stringValue = default(string);
+            return false;
+        }
+
+        public static string Get(this IKeyValueCollection collection, string key, string defaultValue)
+        {
+            string stringValue;
+            if (TryGet(collection, key, out stringValue))
+                return stringValue;
+
+            if (defaultValue != null)
+                return defaultValue;
+
+            throw new InvalidOperationException(String.Format("Collection doesn't contain value of type string with key '{0}'", key));
+        }
+
+        public static bool TryGet(this IKeyValueCollection collection, string key, out bool boolValue)
+        {
+            Guard.NotNull(collection, "collection");
+
+            object value;
+            if (collection.TryGet(key, out value))
+                return Converts.Try(value, out boolValue);
+
+            boolValue = default(bool);
+            return false;
+        }
+
+        public static bool Get(this IKeyValueCollection collection, string key, bool? defaultValue)
+        {
+            bool boolValue;
+            if (TryGet(collection, key, out boolValue))
+                return boolValue;
+
+            if (defaultValue != null)
+                return defaultValue.Value;
+
+            throw new InvalidOperationException(String.Format("Collection doesn't contain value of type bool with key '{0}'", key));
+        }
+    }
+}
