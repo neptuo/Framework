a0730d07ba28f3bca45841d1022efd696e9f2967
Mara <Mara@ce2e7961-3028-d745-b011-a761b73f448b>
2014-09-15 09:43:25 +0000

    IDisposable and DisposableBase implementation.

---
+++ trunk/Neptuo/ComponentModel/DisposableBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.ComponentModel
+{
+    /// <summary>
+    /// Base class for implementing <see cref="IDisposable"/>.
+    /// Provides posibility to distinguish between disposiing managed and unmanaged resources.
+    /// Provides flag to see if object is already disposed.
+    /// Once object is dispose, calling <see cref="IDisposable.Disponse"/> has no effect.
+    /// </summary>
+    public abstract class DisposableBase : IDisposable
+    {
+        public bool IsDisposed { get; private set; }
+
+        ~DisposableBase()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (IsDisposed)
+                return;
+
+            if (disposing)
+                DisposeManagedResources();
+
+            DisposeUnmanagedResources();
+            IsDisposed = true;
+        }
+
+        /// <summary>
+        /// Disposes the managed resources.
+        /// </summary>
+        protected virtual void DisposeManagedResources()
+        { }
+
+        /// <summary>
+        /// Disposes the unmanaged resources.
+        /// </summary>
+        protected virtual void DisposeUnmanagedResources()
+        { }
+
+    }
+}
--- trunk/Neptuo/Properties/VersionInfo.cs
+++ trunk/Neptuo/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo
 {
     public static class VersionInfo
     {
-        internal const string Version = "3.1.0";
+        internal const string Version = "3.1.1";
 
         public static Version GetVersion()
         {
