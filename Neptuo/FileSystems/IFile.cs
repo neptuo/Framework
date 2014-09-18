94edbc9328eb9af1b1b4e374dfc192de9c579aa2
Marek Fišera <maraf@users.noreply.github.com>
2014-09-18 08:11:45 +0000

    File content write methods on IFile + Guard.NotDisposed for testing disposed objects.

--- Neptuo/FileSystems/IFile.cs
+++ Neptuo/FileSystems/IFile.cs
@@ -12,5 +12,22 @@ namespace Neptuo.FileSystems
     /// </summary>
     public interface IFile : IReadOnlyFile
     {
+        /// <summary>
+        /// Overrides file content to <paramref name="fileContent"/>.
+        /// </summary>
+        /// <param name="fileContent">New file content.</param>
+        void SetContent(string fileContent);
+
+        /// <summary>
+        /// Overrides file content to <paramref name="fileContent"/>.
+        /// </summary>
+        /// <param name="fileContent">New file content.</param>
+        void SetContentFromByteArray(byte[] fileContent);
+
+        /// <summary>
+        /// Overrides file content to <paramref name="fileContent"/>.
+        /// </summary>
+        /// <param name="fileContent">New file content.</param>
+        void SetContentFromStream(Stream fileContent);
     }
 }
