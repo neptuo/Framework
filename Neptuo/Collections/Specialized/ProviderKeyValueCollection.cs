774848e3678aff2075122d3e74b5964ced8b2e29
Marek Fišera <maraf@users.noreply.github.com>
2014-10-25 20:37:35 +0000

    ProviderKeyValueCollection implementation.

--- Neptuo/Collections/Specialized/ProviderKeyValueCollection.cs
+++ Neptuo/Collections/Specialized/ProviderKeyValueCollection.cs
@@ -9,22 +9,66 @@ namespace Neptuo.Collections.Specialized
     /// <summary>
     /// Extensible <see cref="IKeyValueCollection"/>.
     /// When reading value, which has not been set yet, calls registered provider.
+    /// Providers are of the two groups (distinguished by registration):
+    /// a) for specific key (registered with key).
+    /// b) general (for all keys).
+    /// When trying to get value which key is not present, 
+    /// first gets called provider registered with specific key, 
+    /// than those registered without key.
     /// </summary>
     public class ProviderKeyValueCollection : KeyValueCollection
     {
-        public bool IsReadOnly { get; set; }
+        /// <summary>
+        /// Backing storage for providers.
+        /// </summary>
+        private readonly Dictionary<string, List<OutFunc<string, object, bool>>> storage = new Dictionary<string, List<OutFunc<string, object, bool>>>();
 
-        public IKeyValueCollection Set(string key, object value)
+        /// <summary>
+        /// Mapps <paramref name="provider"/>.
+        /// This provider will be called after providers registered for specific key.
+        /// </summary>
+        /// <param name="provider">Provider for getting values.</param>
+        public void AddProvider(OutFunc<string, object, bool> provider)
         {
-            if (IsReadOnly)
-                throw Guard.Exception.InvalidOperation("Collection is in read-only mode.");
-
-            throw Guard.Exception.NotImplemented();
+            Guard.NotNull(provider, "provider");
+            AddProvider(String.Empty, provider);
         }
 
-        public bool TryGet<T>(string key, out T value)
+        public void AddProvider(string key, OutFunc<string, object, bool> provider)
         {
-            throw Guard.Exception.NotImplemented();
+            Guard.NotNull(key, "key");
+            Guard.NotNull(provider, "provider");
+
+            List<OutFunc<string, object, bool>> values;
+            if (!storage.TryGetValue(key, out values))
+                values = storage[key] = new List<OutFunc<string, object, bool>>();
+
+            values.Add(provider);
+        }
+
+        protected override bool TryGetDefault<T>(string key, out T value)
+        {
+            Guard.NotNull(key, "key");
+            foreach (OutFunc<string, object, bool> provider in Enumerable.Concat(GetProviders(key), GetProviders(String.Empty)))
+            {
+                object valueBase;
+                if(provider(key, out valueBase))
+                {
+                    Set(key, valueBase);
+                    return ConvertTo(valueBase, out value);
+                }
+            }
+
+            return base.TryGetDefault(key, out value);
+        }
+
+        private IEnumerable<OutFunc<string, object, bool>> GetProviders(string key)
+        {
+            List<OutFunc<string, object, bool>> values;
+            if (!storage.TryGetValue(key, out values))
+                return Enumerable.Empty<OutFunc<string, object, bool>>();
+
+            return values;
         }
     }
 }
--- Neptuo/Properties/VersionInfo.cs
+++ Neptuo/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo
 {
     public static class VersionInfo
     {
-        internal const string Version = "3.5.5";
+        internal const string Version = "3.5.6";
 
         public static Version GetVersion()
         {
