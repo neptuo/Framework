58151cb674225c1c439c86dac037b2caa015c5f1
Marek Fišera <maraf@users.noreply.github.com>
2014-10-28 19:40:24 +0000

    WIP on IReflectionService. Removed CompilerService.

--- Neptuo/Properties/VersionInfo.cs
+++ Neptuo/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo
 {
     public static class VersionInfo
     {
-        internal const string Version = "3.6.2";
+        internal const string Version = "3.6.3";
 
         public static Version GetVersion()
         {
