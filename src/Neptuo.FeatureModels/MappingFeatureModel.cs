4751d31c82aeda5bb833a5e70488fcc0f4bba715
maraf <mara@neptuo.com>
2015-01-31 18:58:34 +0100

    Base interface with extensions. Initial implementation, mapping and object.

---
+++ src/Neptuo.FeatureModels/MappingFeatureModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.FeatureModels
+{
+    /// <summary>
+    /// Implementation of <see cref="IFeatureModel"/> which delegates features to registered objects (with concurrent support.).
+    /// </summary>
+    public class MappingFeatureModel : IFeatureModel
+    {
+        private readonly IDictionary<Type, object> features;
+
+        public MappingFeatureModel(bool isSingleThread)
+        {
+            if (isSingleThread)
+                features = new Dictionary<Type, object>();
+            else
+                features = new ConcurrentDictionary<Type, object>();
+        }
+
+        public MappingFeatureModel(bool isSingleThread, IDictionary<Type, object> features)
+        {
+            Guard.NotNull(features, "features");
+            if (isSingleThread)
+                this.features = new Dictionary<Type, object>(features);
+            else
+                this.features = new ConcurrentDictionary<Type, object>(features);
+        }
+
+        public bool TryWith<TFeature>(out TFeature feature)
+        {
+            Type featureType = typeof(TFeature);
+            object featureBase;
+            if (features.TryGetValue(featureType, out featureBase))
+            {
+                feature = (TFeature)featureBase;
+                return true;
+            }
+
+            feature = default(TFeature);
+            return false;   
+        }
+    }
+}
