d58ef8d33ec7085cf814087d61ccb0e814449de6
maraf <mara@neptuo.com>
2015-02-22 11:14:05 +0100

    Starting with timers.

---
+++ src/Neptuo/Timers/IBackgroundService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Timers
+{
+    /// <summary>
+    /// Base contract for invokable background running 
+    /// </summary>
+    public interface IBackgroundService
+    {
+        void Invoke();
+    }
+}
