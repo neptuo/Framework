eb2985e0b96c88fe31874f8fb123dbd796c50c45
maraf <mara@neptuo.com>
2015-03-07 11:16:34 +0100

    Implementation of UnityActivator.

---
+++ src/Neptuo.Activators.Unity/UnityActivator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Activators
+{
+    /// <summary>
+    /// Implementation of <see cref="IActivator{T}"/> by <see cref="IUnityContainer"/>.
+    /// </summary>
+    /// <typeparam name="T">Type of service to create.</typeparam>
+    public class UnityActivator<T> : IActivator<T>
+    {
+        private readonly IUnityContainer unityContainer;
+
+        public UnityActivator(IUnityContainer unityContainer)
+        {
+            Guard.NotNull(unityContainer, "unityContainer");
+            this.unityContainer = unityContainer;
+        }
+
+        public T Create()
+        {
+            return unityContainer.Resolve<T>();
+        }
+    }
+}
