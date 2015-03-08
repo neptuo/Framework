f3e01cc3a934954816fc3683bb98c07986947e96
maraf <mara@neptuo.com>
2015-03-08 15:19:59 +0100

    Delete old projects.

--- src/Neptuo.Utils/Func.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Neptuo
-{
-    delegate TReturn Func<T, TOutput, TReturn>(T input, out TOutput output);
-}
