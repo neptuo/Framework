f18a1c2faa974c306f7e7db4465eee03d14acb77
maraf <mara@neptuo.com>
2015-04-20 08:27:16 +0200

    Default implementation of error model.

---
+++ src/Neptuo/ComponentModel/DefaultErrorModel.cs
@@ -0,0 +1,24 @@
+﻿using Neptuo.FeatureModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.ComponentModel
+{
+    /// <summary>
+    /// Default extensible implementation of <see cref="IErrorModel"/>.
+    /// This implementation is not thread safe.
+    /// </summary>
+    public class DefaultErrorModel : FeatureCollectionModel
+    {
+        public string Text { get; private set; }
+
+        public DefaultErrorModel(string text)
+            : base(true)
+        {
+            Text = text;
+        }
+    }
+}
