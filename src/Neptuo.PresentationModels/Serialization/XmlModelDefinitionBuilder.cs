2a5d1d21fd635376ef46f9630abe9bc95a69dca3
maraf <mara@neptuo.com>
2015-05-15 07:56:01 +0200

    XSD for xml defined presentation models. Example xml definition.

---
+++ src/Neptuo.PresentationModels/Serialization/XmlModelDefinitionBuilder.cs
@@ -0,0 +1,30 @@
+﻿using Neptuo.Collections.Specialized;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.PresentationModels.Serialization
+{
+    /// <summary>
+    /// XML source based implementation of model definition builder.
+    /// </summary>
+    public class XmlModelDefinitionBuilder : ModelDefinitionBuilderBase
+    {
+        protected override string BuildModelIdentifier()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<IFieldDefinition> BuildFieldDefinitions()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IKeyValueCollection BuildModelMetadata()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
