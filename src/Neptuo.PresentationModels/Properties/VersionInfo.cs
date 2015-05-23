7c99a6f5246e09dba19d438991e9b9e9e2024a8d
maraf <mara@neptuo.com>
2015-05-23 18:17:48 +0200

    Update version info.

--- src/Neptuo.PresentationModels/Properties/VersionInfo.cs
+++ src/Neptuo.PresentationModels/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo.PresentationModels
 {
     public static class VersionInfo
     {
-        internal const string Version = "6.0.1";
+        internal const string Version = "6.1.0";
 
         public static Version GetVersion()
         {
