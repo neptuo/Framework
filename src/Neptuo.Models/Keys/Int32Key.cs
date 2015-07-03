430d8527e8cbb24da070e6484dabbfdd07acba41
maraf <mara@neptuo.com>
2015-07-03 13:33:28 +0200

    Refactoring keys to Neptuo.Models.Keys. Adding short, long and string implementations.

--- src/Neptuo.Models/Keys/Int32Key.cs
+++ src/Neptuo.Models/Keys/Int32Key.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Neptuo.DomainModels
+namespace Neptuo.Models.Keys
 {
+    /// <summary>
+    /// Implementation of <see cref="IKey"/> that takes single <see cref="int"/> value (ID).
+    /// </summary>
     public class Int32Key : KeyBase
     {
+        /// <summary>
+        /// Creates new instance with typical <see cref="int"/> <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id"><see cref="int"/> value of the key.</param>
+        /// <param name="type">Type of the object.</param>
+        /// <returns>Instance with typical <see cref="int"/> <paramref name="id"/>.</returns>
         public static Int32Key Create(int id, string type)
         {
             Ensure.Positive(id, "id");
@@ -15,12 +24,20 @@ namespace Neptuo.DomainModels
             return new Int32Key(id, type);
         }
 
+        /// <summary>
+        /// Creates new instance of empty key for <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type">Type of the object.</param>
+        /// <returns>Instance of empty key for <paramref name="type"/>.</returns>
         public static Int32Key Empty(string type)
         {
             Ensure.NotNullOrEmpty(type, "type");
             return new Int32Key(type);
         }
 
+        /// <summary>
+        /// Key value.
+        /// </summary>
         public int ID { get; private set; }
 
         protected Int32Key(string type)
