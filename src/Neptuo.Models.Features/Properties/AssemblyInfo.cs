ce43b468936ca8fd1f971b47c17e877cd5b40216
maraf <mara@neptuo.com>
2015-07-07 19:57:50 +0200

    Refactor FeatureModels to separate library. Remove IErrorModel from Neptuo.

---
+++ src/Neptuo.Models.Features/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using Neptuo.Models.Features;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Neptuo.Models.Features")]
+[assembly: AssemblyDescription("Feature models (soft contract, endless extensibility).")]
+
+[assembly: AssemblyVersion(VersionInfo.Version)]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
+[assembly: AssemblyFileVersion(VersionInfo.Version)]
