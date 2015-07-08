fa325a64896cfaea92ff41018efd2b4d7d30092f
maraf <mara@neptuo.com>
2015-07-08 08:34:54 +0200

    Add route table to query dispatcher.

--- src/Neptuo.Services.Queries.Http/HttpQueryDispatcher.cs
+++ src/Neptuo.Services.Queries.Http/HttpQueryDispatcher.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Neptuo.Services.Queries.Routing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,14 @@ namespace Neptuo.Services.Queries
 {
     public class HttpQueryDispatcher : IQueryDispatcher
     {
+        private readonly IRouteTable routeTable;
+
+        public HttpQueryDispatcher(IRouteTable routeTable)
+        {
+            Ensure.NotNull(routeTable, "routeTable");
+            this.routeTable = routeTable;
+        }
+
         public Task<TOutput> QueryAsync<TOutput>(IQuery<TOutput> query)
         {
             throw new NotImplementedException();
