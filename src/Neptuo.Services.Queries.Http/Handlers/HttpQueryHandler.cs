455badf4b8dfedae93d382c047346b7b395d6053
maraf <mara@neptuo.com>
2015-07-09 19:42:02 +0200

    Implementation of HttpQueryHandler (for single query processing over HTTP).

---
+++ src/Neptuo.Services.Queries.Http/Handlers/HttpQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Neptuo.Services.Queries.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Services.Queries.Handlers
+{
+    /// <summary>
+    /// Implementation of <see cref="IQueryHandler{TQuery, TResult}"/> that transfers queries over HTTP.
+    /// </summary>
+    /// <typeparam name="TQuery">Type of query.</typeparam>
+    /// <typeparam name="TResult">Type of result.</typeparam>
+    public class HttpQueryHandler<TQuery, TResult> : IQueryHandler<TQuery, TResult>
+        where TQuery : IQuery<TResult>
+    {
+        private readonly HttpQueryDispatcher dispatcher;
+
+        /// <summary>
+        /// Creates new instance that routes queries of type <typeparamref name="TQuery"/> to the <paramref name="route"/>.
+        /// </summary>
+        /// <param name="route">Route definition.</param>
+        public HttpQueryHandler(RouteDefinition route)
+        {
+            this.dispatcher = new HttpQueryDispatcher(new DefaultRouteTable().Add(typeof(TQuery), route));
+        }
+
+        public Task<TResult> HandleAsync(TQuery query)
+        {
+            return dispatcher.QueryAsync(query);
+        }
+    }
+}
