234d67efff38e57a4066082d8ad74dc238758280
maraf <mara@neptuo.com>
2015-07-13 17:18:05 +0200

    Project creation.

---
+++ src/Neptuo.Services.AppServices/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using Neptuo.Services.AppServices;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Neptuo.Services.AppServices")]
+[assembly: AssemblyDescription("Application (backend and recuring) services support.")]
+
+[assembly: AssemblyVersion(VersionInfo.Version)]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
+[assembly: AssemblyFileVersion(VersionInfo.Version)]
