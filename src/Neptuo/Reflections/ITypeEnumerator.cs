d39a64fb77cc301f686e395fa84986cc3f2f6b16
maraf <mara@neptuo.com>
2015-07-15 14:47:42 +0200

    ITypeEnumerator contract. Some extensions for ITypeEnumerator.

---
+++ src/Neptuo/Reflections/ITypeEnumerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Reflections
+{
+    /// <summary>
+    /// Contract for enumerating types.
+    /// </summary>
+    /// <typeparam name="TContext">Type of context information.</typeparam>
+    public interface ITypeEnumerator<TContext>
+    {
+        /// <summary>
+        /// Adds filters. All filters and conjucted.
+        /// </summary>
+        /// <param name="filter">Delegate, that returns <c>true</c> to pass type handler and <c>false</c> to filter out passed type.</param>
+        /// <returns>Self (for fluency).</returns>
+        ITypeEnumerator<TContext> AddFilter(Func<Type, TContext, bool> filter);
+
+        /// <summary>
+        /// Adds handler, that will be execute for every type, that matches all filters.
+        /// </summary>
+        /// <param name="handler">Delegate to be executed for every type, that matches all filters.</param>
+        /// <returns>Self (for fluency).</returns>
+        ITypeEnumerator<TContext> AddHandler(Action<Type, TContext> handler);
+    }
+
+    public interface ITypeEnumerator
+    {
+        /// <summary>
+        /// Adds filters. All filters and conjucted.
+        /// </summary>
+        /// <param name="filter">Delegate, that returns <c>true</c> to pass type handler and <c>false</c> to filter out passed type.</param>
+        /// <returns>Self (for fluency).</returns>
+        ITypeEnumerator AddFilter(Func<Type, bool> filter);
+
+        /// <summary>
+        /// Adds handler, that will be execute for every type, that matches all filters.
+        /// </summary>
+        /// <param name="handler">Delegate to be executed for every type, that matches all filters.</param>
+        /// <returns>Self (for fluency).</returns>
+        ITypeEnumerator AddHandler(Action<Type> handler);
+    }
+}
---
+++ src/Neptuo/Reflections/_TypeEnumeratorExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Reflections
+{
+    /// <summary>
+    /// Common extensions for <see cref="ITypeEnumerator"/> and <see cref="ITypeEnumerator{TContext}"/>.
+    /// </summary>
+    public static class _TypeEnumeratorExtensions
+    {
+        #region AddFilterNotInterface
+
+        /// <summary>
+        /// Adds filter on <paramref name="enumerator"/> to filter out interfaces.
+        /// </summary>
+        /// <param name="enumerator">Type enumerator.</param>
+        /// <returns>Result from calling <see cref="ITypeEnumerator.AddFilter"/>.</returns>
+        public static ITypeEnumerator AddFilterNotInterface(this ITypeEnumerator enumerator)
+        {
+            Ensure.NotNull(enumerator, "enumerator");
+            return enumerator.AddFilter(t => !t.IsInterface);
+        }
+
+        /// <summary>
+        /// Adds filter on <paramref name="enumerator"/> to filter out interfaces.
+        /// </summary>
+        /// <param name="enumerator">Type enumerator.</param>
+        /// <returns>Result from calling <see cref="ITypeEnumerator.AddFilter"/>.</returns>
+        public static ITypeEnumerator<TContext> AddFilterNotInterface<TContext>(this ITypeEnumerator<TContext> enumerator)
+        {
+            Ensure.NotNull(enumerator, "enumerator");
+            return enumerator.AddFilter((t, context) => !t.IsInterface);
+        }
+
+        #endregion
+
+        #region AddTypeFilterNotAbstract
+
+        /// <summary>
+        /// Adds filter on <paramref name="enumerator"/> to filter out abstract classes.
+        /// </summary>
+        /// <param name="enumerator">Type enumerator.</param>
+        /// <returns>Result from calling <see cref="ITypeEnumerator.AddFilter"/>.</returns>
+        public static ITypeEnumerator AddTypeFilterNotAbstract(this ITypeEnumerator enumerator)
+        {
+            Ensure.NotNull(enumerator, "enumerator");
+            return enumerator.AddFilter(t => !t.IsInterface);
+        }
+
+        /// <summary>
+        /// Adds filter on <paramref name="enumerator"/> to filter out abstract classes.
+        /// </summary>
+        /// <param name="enumerator">Type enumerator.</param>
+        /// <returns>Result from calling <see cref="ITypeEnumerator.AddFilter"/>.</returns>
+        public static ITypeEnumerator<TContext> AddTypeFilterNotAbstract<TContext>(this ITypeEnumerator<TContext> enumerator)
+        {
+            Ensure.NotNull(enumerator, "enumerator");
+            return enumerator.AddFilter((t, context) => !t.IsAbstract);
+        }
+
+        #endregion
+    }
+}
