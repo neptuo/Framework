d308043cccbf5c6cbc8c3e4b2f96abf21201ff27
maraf <mara@neptuo.com>
2015-07-17 20:30:43 +0200

    Base implementation of Log4net log serializer.

---
+++ src/Neptuo.Logging.Log4net/Serializers/Log4netSerializer.cs
@@ -0,0 +1,73 @@
+﻿using Neptuo.Logging.Serialization;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ILogger = log4net.ILog;
+
+namespace Neptuo.Logging.Serializers
+{
+    /// <summary>
+    /// Basic integration of <see cref="log4net.ILog"/> to <see cref="ILogSerializer"/>.
+    /// </summary>
+    public class Log4netSerializer : ILogSerializer
+    {
+        private ILogger GetLog(string scopeName)
+        {
+            return LogManager.GetLogger(scopeName);
+        }
+
+        public void Append(string scopeName, LogLevel level, object model)
+        {
+            ILogger log = GetLog(scopeName);
+            if(IsEnabled(log, level))
+            {
+                switch (level)
+                {
+                    case LogLevel.Debug:
+                        log.Debug(model);
+                        break;
+                    case LogLevel.Info:
+                        log.Info(model);
+                        break;
+                    case LogLevel.Warning:
+                        log.Warn(model);
+                        break;
+                    case LogLevel.Error:
+                        log.Error(model);
+                        break;
+                    case LogLevel.Fatal:
+                        log.Fatal(model);
+                        break;
+                }
+            }
+        }
+
+        public bool IsEnabled(string scopeName, LogLevel level)
+        {
+            ILogger log = GetLog(scopeName);
+            return IsEnabled(log, level);
+        }
+
+        private bool IsEnabled(ILogger log, LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.Debug:
+                    return log.IsDebugEnabled;
+                case LogLevel.Info:
+                    return log.IsInfoEnabled;
+                case LogLevel.Warning:
+                    return log.IsWarnEnabled;
+                case LogLevel.Error:
+                    return log.IsErrorEnabled;
+                case LogLevel.Fatal:
+                    return log.IsFatalEnabled;
+                default:
+                    return false;
+            }
+        }
+    }
+}
