984dc428bf49fc8c6a7cda3a78e5c950e673e587
maraf <mara@neptuo.com>
2015-07-22 21:19:32 +0200

    Add (L) a translation helper.

---
+++ src/Neptuo/Localization/L.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Localization
+{/// <summary>
+    /// Localization helper.
+    /// </summary>
+    /// <example>
+    /// string localized = (L)"String to localize";
+    /// </example>
+    public struct L
+    {
+        /// <summary>
+        /// Text to localize
+        /// </summary>
+        string text;
+
+        public static string T(string s)
+        {
+            return s;
+        }
+
+        public static implicit operator L(string s)
+        {
+            return new L() { text = s };
+            //return new L(s);
+        }
+
+        public static implicit operator string(L l)
+        {
+            return T(l.ToString());
+        }
+
+        public override string ToString()
+        {
+            return LocalizationService.Current[text];
+        }
+    }   
+}
