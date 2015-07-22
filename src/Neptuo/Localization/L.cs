3012ce3a0752cb34c4d100f47e5220fea1068fd3
maraf <mara@neptuo.com>
2015-07-22 21:34:28 +0200

    Handler + documentation.

--- src/Neptuo/Localization/L.cs
+++ src/Neptuo/Localization/L.cs
@@ -5,38 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Neptuo.Localization
-{/// <summary>
-    /// Localization helper.
+{
+    /// <summary>
+    /// Localization facade.
+    /// Internally calls <see cref="Translate.Text"/> for translation.
     /// </summary>
     /// <example>
     /// string localized = (L)"String to localize";
     /// </example>
     public struct L
     {
+        private readonly string originalText;
+        private readonly string translatedText;
+
         /// <summary>
-        /// Text to localize
+        /// Text to translate.
         /// </summary>
-        string text;
+        public string OriginalText { get { return originalText; } }
 
-        public static string T(string s)
+        /// <summary>
+        /// <see cref="L.OriginalText"/> after translation.
+        /// </summary>
+        public string TranslatedText { get { return translatedText; } }
+
+        /// <summary>
+        /// Private constructor to create instance and translate <paramref name="text"/>.
+        /// </summary>
+        /// <param name="text">Text to translate.</param>
+        private L(string text)
         {
-            return s;
+            originalText = text;
+            translatedText = Translate.Text(text);
         }
 
-        public static implicit operator L(string s)
+        /// <summary>
+        /// Explicit creation from <see cref="string"/> value of <paramref name="text"/>.
+        /// </summary>
+        /// <param name="text">Original text.</param>
+        /// <returns>Instance of translation structure for <paramref name="text"/>.</returns>
+        public static explicit operator L(string text)
         {
-            return new L() { text = s };
-            //return new L(s);
+            return new L(text);
         }
 
-        public static implicit operator string(L l)
+        /// <summary>
+        /// Implicit serialization to <see cref="string"/>.
+        /// </summary>
+        /// <param name="instance">Instance to be serialized to string.</param>
+        /// <returns>Value of <see cref="L.ToString"/>.</returns>
+        public static implicit operator string(L instance)
         {
-            return T(l.ToString());
+            Ensure.NotNull(instance, "instance");
+            return instance.ToString();
         }
 
+        /// <summary>
+        /// To string implementation using translated text.
+        /// </summary>
+        /// <returns>Translated text.</returns>
         public override string ToString()
         {
-            return LocalizationService.Current[text];
+            return TranslatedText;
         }
-    }   
+    }
 }
--- src/Neptuo/Localization/Translate.cs
+++ src/Neptuo/Localization/Translate.cs
@@ -6,13 +6,42 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Localization
 {
+    /// <summary>
+    /// Translates <paramref name="text"/> to the target language.
+    /// </summary>
+    /// <param name="text">Original text.</param>
+    /// <returns><paramref name="text"/> translated to the target language.</returns>
+    public delegate string TranslateHandler(string text);
+
+    /// <summary>
+    /// Translation helper.
+    /// Enables to use translation handler (<see cref="TranslateHandler"/>) as singleton.
+    /// </summary>
     public static class Translate
     {
+        private static TranslateHandler handler = t => t;
 
+        /// <summary>
+        /// Sets <paramref name="handler"/> to be used for translations.
+        /// If <paramref name="handler"/> is <c>null</c>, proxy/transient handler is used (output text is equal to the input text),
+        /// </summary>
+        /// <param name="handler"></param>
+        public static void SetHandler(TranslateHandler handler)
+        {
+            if (handler == null)
+                handler = t => t;
 
+            Translate.handler = handler;
+        }
+
+        /// <summary>
+        /// Translates <paramref name="text"/> to the target language.
+        /// </summary>
+        /// <param name="text">Original text.</param>
+        /// <returns><paramref name="text"/> translated to the target language.</returns>
         public static string Text(string text)
         {
-
+            return handler(text);
         }
     }
 }
