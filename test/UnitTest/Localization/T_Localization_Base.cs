14c3c4e27e8b6cebabffb64e2ea4925aedb0a273
maraf <mara@neptuo.com>
2015-07-22 21:42:39 +0200

    Add test method for default handler.

---
+++ test/UnitTest/Localization/T_Localization_Base.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Localization
+{
+    [TestClass]
+    public class T_Localization_Base
+    {
+        [TestMethod]
+        public void DefaultHandler()
+        {
+            string text = (L)"Hello, World!";
+            Assert.AreEqual("Hello, World!", text);
+        }
+    }
+}
