414432e73b1aeda3efa7ee85ac344dc0e23f8867
maraf <mara@neptuo.com>
2015-07-23 08:02:55 +0200

    Partial implementation of attribute based mappings.

---
+++ src/Neptuo/Activators/ExportTransientAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Activators
+{
+    /// <summary>
+    /// Exports service as transient.
+    /// </summary>
+    public class ExportTransientAttribute : Attribute
+    {
+        {
+            return DependencyLifetime.Transient;
+        }
+    }
+}
