f875466f45dbfe1cce71375ddbdc659ee58db966
maraf <mara@neptuo.com>
2015-07-26 00:30:23 +0200

    Add project for testing services.

---
+++ test/UnitTest.Services/Queries/T_Services_Queries_AutoExport.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Services.Queries
+{
+    [TestClass]
+    public class T_Services_Queries_AutoExport
+    {
+        [TestMethod]
+        public void Base()
+        {
+
+        }
+    }
+}
