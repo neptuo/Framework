6edc0df9c451f4d3fb42c6aad049d3eafc9a8632
maraf <mara@neptuo.com>
2015-07-26 11:39:48 +0200

    Implementation of DefaultCommandDispatcher.

--- src/Neptuo.Services.Commands/DefaultCommandDispatcher.cs
+++ src/Neptuo.Services.Commands/DefaultCommandDispatcher.cs
@@ -24,12 +24,27 @@ namespace Neptuo.Services.Commands
         public bool TryGet<TCommand>(out ICommandHandler<TCommand> handler)
         {
             Type commandType = typeof(TCommand);
-            if(storage.TryGetValue(commandType, out ))
+            DefaultCommandHandlerDefinition definition;
+            if (storage.TryGetValue(commandType, out definition))
+            {
+                handler = (ICommandHandler<TCommand>)definition.CommandHandler;
+                return true;
+            }
+
+            handler = null;
+            return false;
         }
 
-        public void Handle(object command)
+        public Task Handle(object command)
         {
-            throw new NotImplementedException();
+            Ensure.NotNull(command, "command");
+            Type commandType = command.GetType();
+
+            DefaultCommandHandlerDefinition definition;
+            if (storage.TryGetValue(commandType, out definition))
+                return definition.HandleAsync(command);
+
+            throw Ensure.Exception.ArgumentOutOfRange("command", "There isn't command handler for command of type '{0}'.", commandType.FullName);
         }
     }
 }
