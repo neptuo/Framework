a684b06a97f2eacac9f007c2f7523a43ec88b911
maraf <mara@neptuo.com>
2015-07-27 20:52:24 +0200

    Add example usage of new pipeline (2).

---
+++ src/Neptuo.Behaviors/Temp/_ExampleUsage.cs
@@ -0,0 +1,42 @@
+﻿using Neptuo.Activators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Behaviors
+{
+    public interface IXBackgroundHandler
+    {
+        void Invoke();
+    }
+
+    public interface IXPipeline<T>
+    {
+        Task ExecuteAsync(T instance, Action lastBehavior);
+    }
+
+    public class XBackgroundPipelineHandler<T> : IXBackgroundHandler
+        where T : IXBackgroundHandler
+    {
+        private readonly IXPipeline<T> pipeline;
+        private readonly IActivator<T> handlerFactory;
+
+        public XBackgroundPipelineHandler(IXPipeline<T> pipeline, IActivator<T> handlerFactory)
+        {
+            Ensure.NotNull(pipeline, "pipeline");
+            Ensure.NotNull(handlerFactory, "handlerFactory");
+            this.pipeline = pipeline;
+            this.handlerFactory = handlerFactory;
+        }
+
+        public void Invoke()
+        {
+            T instance = handlerFactory.Create();
+            Task task = pipeline.ExecuteAsync(instance, () => instance.Invoke());
+            if (!task.IsCompleted)
+                task.RunSynchronously();
+        }
+    }
+}
