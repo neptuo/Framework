a2b0f47724cd978920348a937469fb990eb709d2
maraf <mara@neptuo.com>
2015-08-03 09:44:23 +0200

    Add support of Behaviors for Commands.

--- src/Neptuo.Services.Commands.Behaviors/Handlers/BehaviorCommandHandler.cs
+++ src/Neptuo.Services.Commands.Behaviors/Handlers/BehaviorCommandHandler.cs
@@ -8,17 +8,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Neptuo.Services.Queries.Handlers
+namespace Neptuo.Services.Commands.Handlers
 {
     /// <summary>
     /// Pipeline based implementation of query handler.
     /// </summary>
-    /// <typeparam name="T">Type of inner handler.</typeparam>
-    /// <typeparam name="TQuery">Type of query parameters.</typeparam>
-    /// <typeparam name="TResult">Type of query result.</typeparam>
-    public class BehaviorQueryHandler<T, TQuery, TResult> : IQueryHandler<TQuery, TResult>, IBehavior<T>
-        where TQuery : IQuery<TResult>
-        where T : IQueryHandler<TQuery, TResult>
+    public class BehaviorCommandHandler<T, TCommand> : ICommandHandler<TCommand>, IBehavior<T>
+        where T : ICommandHandler<TCommand>
     {
         private readonly IPipeline<T> pipeline;
         private readonly IActivator<T> handlerFactory;
@@ -28,7 +24,7 @@ namespace Neptuo.Services.Queries.Handlers
         /// </summary>
         /// <param name="pipeline">Behavior pipeline.</param>
         /// <param name="handlerFactory">Inner handler factory.</param>
-        public BehaviorQueryHandler(IPipeline<T> pipeline, IActivator<T> handlerFactory)
+        public BehaviorCommandHandler(IPipeline<T> pipeline, IActivator<T> handlerFactory)
         {
             Ensure.NotNull(pipeline, "pipeline");
             Ensure.NotNull(handlerFactory, "handlerFactory");
@@ -36,20 +32,18 @@ namespace Neptuo.Services.Queries.Handlers
             this.handlerFactory = handlerFactory;
         }
 
-        async Task IBehavior<T>.ExecuteAsync(T handler, IBehaviorContext context)
+        Task IBehavior<TCommand>.ExecuteAsync(T handler, IBehaviorContext context)
         {
-            TResult result = await handler.HandleAsync(context.CustomValues.Get<TQuery>("Query"));
-            context.CustomValues.Set("Result", result);
+            return handler.HandleAsync(context.CustomValues.Get<TCommand>("Command"));
         }
 
-        public async Task<TResult> HandleAsync(TQuery query)
+        public Task HandleAsync(TCommand command)
         {
             T instance = handlerFactory.Create();
             IKeyValueCollection customValues = new KeyValueCollection()
-                .Set("Query", query);
+                .Set("Command", command);
 
-            await pipeline.ExecuteAsync(instance, customValues);
-            return customValues.Get<TResult>("Result");
+            return pipeline.ExecuteAsync(instance, customValues);
         }
     }
 }
