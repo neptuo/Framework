c3360f218f60b35b542dc158b6e34bd9f87cd0c7
maraf <mara@neptuo.com>
2015-08-05 08:22:34 +0200

    Refactoring sync extensions for reading and updating/appending file content.

---
+++ src/Neptuo.FileSystems/Features/_FileContentReaderExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.FileSystems.Features
+{
+    /// <summary>
+    /// Extensions for reading file content from <see cref="IFileContentReader"/>.
+    /// </summary>
+    public static class _FileContentReaderExtensions
+    {
+        /// <summary>
+        /// Returns file content.
+        /// </summary>
+        /// <returns>File content.</returns>
+        public static string GetContent(this IFileContentReader file)
+        {
+            Ensure.NotNull(file, "file");
+
+            Task<string> task = file.GetContentAsync();
+            if(!task.IsCompleted && !task.IsCanceled)
+                task.RunSynchronously();
+
+            return task.Result;
+        }
+
+        /// <summary>
+        /// Returns file content as byte array.
+        /// </summary>
+        /// <returns>File content as byte array.</returns>
+        public static byte[] GetContentAsByteArray(this IFileContentReader file)
+        {
+            Ensure.NotNull(file, "file");
+
+            Task<byte[]> task = file.GetContentAsByteArrayAsync();
+            if(!task.IsCompleted && !task.IsCanceled)
+                task.RunSynchronously();
+
+            return task.Result;
+        }
+
+        /// <summary>
+        /// Returns file content as stream.
+        /// </summary>
+        /// <returns>File content as stream.</returns>
+        public static Stream GetContentAsStream(this IFileContentReader file)
+        {
+            Ensure.NotNull(file, "file");
+
+            Task<Stream> task = file.GetContentAsStreamAsync();
+            if(!task.IsCompleted && !task.IsCanceled)
+                task.RunSynchronously();
+
+            return task.Result;
+        }
+    }
+}
---
+++ src/Neptuo.FileSystems/Features/_FileContentUpdaterExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.FileSystems.Features
+{
+    /// <summary>
+    /// Extensions for writing file content to <see cref="IFileContentUpdater"/>.
+    /// </summary>
+    public static class _FileContentUpdaterExtensions
+    {
+        /// <summary>
+        /// Overrides file content to <paramref name="fileContent"/>.
+        /// </summary>
+        /// <param name="fileContent">New file content.</param>
+        public static void SetContent(this IFileContentUpdater file, string fileContent)
+        {
+            Task task = file.SetContentAsync(fileContent);
+            if(!task.IsCompleted && !task.IsCanceled)
+                task.RunSynchronously();
+        }
+
+        /// <summary>
+        /// Overrides file content to <paramref name="fileContent"/>.
+        /// </summary>
+        /// <param name="fileContent">New file content.</param>
+        public static void SetContentFromByteArray(this IFileContentUpdater file, byte[] fileContent)
+        {
+            Task task = file.SetContentFromByteArrayAsync(fileContent);
+            if (!task.IsCompleted && !task.IsCanceled)
+                task.RunSynchronously();
+        }
+
+        /// <summary>
+        /// Overrides file content to <paramref name="fileContent"/>.
+        /// </summary>
+        /// <param name="fileContent">New file content.</param>
+        public static void SetContentFromStream(this IFileContentUpdater file, Stream fileContent)
+        {
+            Task task = file.SetContentFromStreamAsync(fileContent);
+            if (!task.IsCompleted && !task.IsCanceled)
+                task.RunSynchronously();
+        }
+    }
+}
