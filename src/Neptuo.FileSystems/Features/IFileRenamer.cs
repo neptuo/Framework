dd8394aa75ea55a5502c0251b34a5e4e54a6bba4
maraf <mara@neptuo.com>
2015-08-06 22:48:39 +0200

    Add features for renaming and deleting.

---
+++ src/Neptuo.FileSystems/Features/IFileRenamer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.FileSystems.Features
+{
+    /// <summary>
+    /// Contract for renaming current file.
+    /// </summary>
+    public interface IFileRenamer
+    {
+        /// <summary>
+        /// Changes file name (without extension) to <paramref name="fileName" />
+        /// </summary>
+        /// <param name="fileName">New file name (without extension).</param>
+        void ChangeName(string fileName);
+
+        /// <summary>
+        /// Changes file extension to <paramref name="fileExtension" />
+        /// </summary>
+        /// <param name="fileExtension">New extension.</param>
+        void ChangeExtension(string fileExtension);
+    }
+}
