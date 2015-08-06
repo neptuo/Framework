6bea1fd7c7d4cdc278aced9def35bd65d17c6584
maraf <mara@neptuo.com>
2015-08-06 17:45:16 +0200

    Implementation of local file.

--- src/Neptuo.FileSystems.Local/LocalFile.cs
+++ src/Neptuo.FileSystems.Local/LocalFile.cs
@@ -1,4 +1,7 @@
-﻿using Neptuo.FileSystems.Features;
+﻿using Neptuo.Activators;
+using Neptuo.FileSystems.Features;
+using Neptuo.FileSystems.Features.Timestamps;
+using Neptuo.Models.Features;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,156 +14,129 @@ namespace Neptuo.FileSystems
     /// <summary>
     /// Virtual file system file implemented as stadart file system file.
     /// </summary>
-    //public class LocalFile : IFile
-    //{
-    //    private IDirectory parent;
-    //    private long? fileSize;
-
-    //    public string Name { get; private set; }
-    //    public string Extension { get; private set; }
-    //    public string FullPath { get; private set; }
-
-    //    public IDirectory Parent
-    //    {
-    //        get
-    //        {
-    //            if (parent == null)
-    //                parent = GetParentDirectoryFromFullPath(FullPath);
-
-    //            return parent;
-    //        }
-    //        private set { parent = value; }
-    //    }
-
-    //    public long FileSize
-    //    {
-    //        get
-    //        {
-    //            if (fileSize == null)
-    //                fileSize = GetFileSize(FullPath);
-
-    //            return fileSize.Value;
-    //        }
-    //    }
-
-    //    /// <summary>
-    //    /// Creates new instance that points to the <paramref name="fullPath"/>.
-    //    /// </summary>
-    //    /// <param name="fullPath">Standard file system path to file.</param>
-    //    internal LocalFile(string fullPath)
-    //    {
-    //        Ensure.NotNullOrEmpty(fullPath, "fullPath");
-    //        SetFileRelatedProperties(fullPath);
-    //    }
-
-    //    /// <summary>
-    //    /// Creates new instance that points to the <paramref name="fullPath"/> and uses <paramref name="parent"/> as its parent.
-    //    /// </summary>
-    //    /// <param name="parent">Virtual parent directory.</param>
-    //    /// <param name="fullPath">Standard file system path to the file.</param>
-    //    internal LocalFile(IDirectory parent, string fullPath)
-    //        : this(fullPath)
-    //    {
-    //        Ensure.NotNull(parent, "parent");
-    //        Parent = parent;
-    //    }
-
-    //    /// <summary>
-    //    /// Sets file related properties from <paramref name="fullPath"/>.
-    //    /// </summary>
-    //    /// <param name="fullPath">Standard file system path to file.</param>
-    //    private void SetFileRelatedProperties(string fullPath)
-    //    {
-    //        if (!File.Exists(fullPath))
-    //            throw Ensure.Exception.Argument("fullPath", "Provided path must be existing file.");
-
-    //        FullPath = fullPath;
-    //        Name = Path.GetFileNameWithoutExtension(fullPath);
-    //        Extension = Path.GetExtension(fullPath);
-    //    }
-
-    //    /// <summary>
-    //    /// Helper to get length of file.
-    //    /// </summary>
-    //    /// <param name="fullPath">Standard file system path to file.</param>
-    //    /// <returns>Length of file.</returns>
-    //    private static long GetFileSize(string fullPath)
-    //    {
-    //        return new FileInfo(fullPath).Length;
-    //    }
-
-    //    /// <summary>
-    //    /// Helper to create parent directory virtual instance.
-    //    /// </summary>
-    //    /// <param name="fullPath">Standard file system path to file.</param>
-    //    /// <returns>Wrapped parent directory.</returns>
-    //    private static IDirectory GetParentDirectoryFromFullPath(string fullPath)
-    //    {
-    //        return new LocalDirectory(Path.GetDirectoryName(fullPath));
-    //    }
-
-
-    //    public Task<string> GetContentAsync()
-    //    {
-    //        return Task.FromResult(File.ReadAllText(FullPath));
-    //    }
-
-    //    public Task<byte[]> GetContentAsByteArrayAsync()
-    //    {
-    //        return Task.FromResult(File.ReadAllBytes(FullPath));
-    //    }
-
-    //    public Task<Stream> GetContentAsStreamAsync()
-    //    {
-    //        return Task.FromResult<Stream>(new FileStream(FullPath, FileMode.Open));
-    //    }
-
-    //    public Task SetContentAsync(string fileContent)
-    //    {
-    //        File.WriteAllText(FullPath, fileContent);
-    //        return Task.FromResult(true);
-    //    }
-
-    //    public Task SetContentFromByteArrayAsync(byte[] fileContent)
-    //    {
-    //        using (FileStream fileStream = new FileStream(FullPath, FileMode.OpenOrCreate))
-    //        {
-    //            return fileStream.WriteAsync(fileContent, 0, fileContent.Length);
-    //        }
-    //    }
-
-    //    public Task SetContentFromStreamAsync(Stream fileContent)
-    //    {
-    //        using (FileStream fileStream = new FileStream(FullPath, FileMode.OpenOrCreate))
-    //        {
-    //            return fileContent.CopyToAsync(fileStream);
-    //        }
-    //    }
-    //}
-
-    public class LocalFile : IFile, IAbsolutePath
+    public class LocalFile : CollectionFeatureModel, IFile, IAbsolutePath, ICreatedAt, IModefiedAt, 
+        IActivator<IAncestorEnumerator>,
+        IFileContentSize, IFileContentReader, IFileContentUpdater, IFileContentAppender
     {
-        public string Name
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public string Extension
-        {
-            get { throw new NotImplementedException(); }
-        }
-
+        public string Name { get; private set; }
+        public string Extension { get; private set; }
         public string AbsolutePath { get; private set; }
 
-        public LocalFile(string absolutePath)
+        public DateTime CreatedAt
         {
-            AbsolutePath = absolutePath;
+            get { return File.GetCreationTime(AbsolutePath); }
         }
 
-        public bool TryWith<TFeature>(out TFeature feature)
+        public DateTime ModifiedAt
+        {
+            get { return File.GetLastWriteTime(AbsolutePath); }
+        }
+
+        public long FileSize
+        {
+            get { return new FileInfo(AbsolutePath).Length; }
+        }
+
+        /// <summary>
+        /// Creates new instance that points to the <paramref name="absolutePath"/>.
+        /// </summary>
+        /// <param name="absolutePath">Standard file system path to file.</param>
+        internal LocalFile(string absolutePath)
+            : base(true)
+        {
+            Ensure.NotNullOrEmpty(absolutePath, "fullPath");
+            SetFileRelatedProperties(absolutePath);
+
+            this
+                .Add<IAbsolutePath>(this)
+                .AddFactory<IAncestorEnumerator>(this)
+                .Add<IFileContentSize>(this)
+                .Add<IFileContentReader>(this)
+                .Add<IFileContentUpdater>(this)
+                .Add<IFileContentAppender>(this);
+        }
+
+        /// <summary>
+        /// Sets file related properties from <paramref name="fullPath"/>.
+        /// </summary>
+        /// <param name="fullPath">Standard file system path to file.</param>
+        private void SetFileRelatedProperties(string fullPath)
+        {
+            if (!File.Exists(fullPath))
+                throw Ensure.Exception.Argument("fullPath", "Provided path must be existing file.");
+
+            AbsolutePath = fullPath;
+            Name = Path.GetFileNameWithoutExtension(fullPath);
+            Extension = Path.GetExtension(fullPath);
+        }
+
+        IAncestorEnumerator IActivator<IAncestorEnumerator>.Create()
+        {
+            return new LocalAncestorEnumerator(AbsolutePath);
+        }
+
+        #region IFileContentReader
+
+        public Task<string> GetContentAsync()
+        {
+            return Task.FromResult(File.ReadAllText(AbsolutePath));
+        }
+
+        public Task<byte[]> GetContentAsByteArrayAsync()
+        {
+            return Task.FromResult(File.ReadAllBytes(AbsolutePath));
+        }
+
+        public Task<Stream> GetContentAsStreamAsync()
+        {
+            return Task.FromResult<Stream>(new FileStream(AbsolutePath, FileMode.Open));
+        }
+
+        #endregion
+
+        #region IFileContentUpdater
+
+        public Task SetContentAsync(string fileContent)
+        {
+            File.WriteAllText(AbsolutePath, fileContent);
+            return Task.FromResult(true);
+        }
+
+        public Task SetContentFromByteArrayAsync(byte[] fileContent)
+        {
+            using (FileStream fileStream = new FileStream(AbsolutePath, FileMode.OpenOrCreate))
+            {
+                return fileStream.WriteAsync(fileContent, 0, fileContent.Length);
+            }
+        }
+
+        public Task SetContentFromStreamAsync(Stream fileContent)
+        {
+            using (FileStream fileStream = new FileStream(AbsolutePath, FileMode.OpenOrCreate))
+            {
+                return fileContent.CopyToAsync(fileStream);
+            }
+        }
+
+        #endregion
+
+        #region IFileContentAppender
+
+        public Task AppendContentAsync(string fileContent)
         {
             throw new NotImplementedException();
         }
-    }
 
+        public Task AppendContentFromByteArrayAsync(byte[] fileContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AppendContentFromStreamAsync(Stream fileContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
 }
