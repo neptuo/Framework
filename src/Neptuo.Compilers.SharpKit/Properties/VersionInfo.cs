ce706804859de5a20407b7a1c7153515ebe424cf
maraf <mara@neptuo.com>
2015-08-11 22:13:46 +0200

    Final release of Neptuo.Compilers.SharpKit v1.0.1.

--- src/Neptuo.Compilers.SharpKit/Properties/VersionInfo.cs
+++ src/Neptuo.Compilers.SharpKit/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo.Compilers.SharpKit
 {
     public static class VersionInfo
     {
-        internal const string Version = "1.0.0";
+        internal const string Version = "1.0.1";
 
         public static Version GetVersion()
         {
