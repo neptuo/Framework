bb7c0cd9bd62260e1395ead6af2abb81917ec414
maraf <mara@neptuo.com>
2015-08-13 18:24:40 +0200

    Fix Neptuo.Localization package description.

--- src/Neptuo.Localization/Properties/AssemblyInfo.cs
+++ src/Neptuo.Localization/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("Neptuo.Localization")]
-[assembly: AssemblyDescription("Support for localization based on dictionaries and culure info.")]
+[assembly: AssemblyDescription("Support for localization based on dictionaries and culture info.")]
 
 [assembly: AssemblyVersion(VersionInfo.Version)]
 [assembly: AssemblyInformationalVersion(VersionInfo.Version)]
