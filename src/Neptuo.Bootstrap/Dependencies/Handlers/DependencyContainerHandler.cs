e8be018a15cc659ab5176bde94f1686d6e5e08d9
maraf <mara@neptuo.com>
2015-08-17 12:08:08 +0200

    Refactoring dependencies.

---
+++ src/Neptuo.Bootstrap/Dependencies/Handlers/DependencyContainerHandler.cs
@@ -0,0 +1,31 @@
+﻿using Neptuo.Activators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Bootstrap.Dependencies.Handlers
+{
+    public class DependencyContainerHandler : IDependencyExporter, IDependencyImporter
+    {
+        private readonly IDependencyContainer dependencyContainer;
+
+        public DependencyContainerHandler(IDependencyContainer dependencyContainer)
+        {
+            Ensure.NotNull(dependencyContainer, "dependencyContainer");
+            this.dependencyContainer = dependencyContainer;
+        }
+
+        public void Export(Type dependencyType, object instance)
+        {
+            dependencyContainer.Definitions
+                .Add(dependencyType, DependencyLifetime.NameScope(dependencyContainer.ScopeName), instance);
+        }
+
+        public object Import(Type depedencyType)
+        {
+            return dependencyContainer.Resolve(depedencyType);
+        }
+    }
+}
--- src/Neptuo.Bootstrap/Hierarchies/Bootstrapper.cs
+++ src/Neptuo.Bootstrap/Hierarchies/Bootstrapper.cs
@@ -11,6 +11,17 @@ namespace Neptuo.Bootstrap.Hierarchies
     public class Bootstrapper : IBootstrapper, IBootstrapTaskCollection
     {
         private readonly Dictionary<Type, IFactory<IBootstrapTask>> storage = new Dictionary<Type, IFactory<IBootstrapTask>>();
+        private readonly List<Type> defaultDependencies = new List<Type>();
+        private readonly ISortInputProvider inputProvider;
+        private readonly ISortOutputProvider outputProvider;
+
+        public Bootstrapper(ISortInputProvider inputProvider, ISortOutputProvider outputProvider)
+        {
+            Ensure.NotNull(inputProvider, "inputProvider");
+            Ensure.NotNull(outputProvider, "outputProvider");
+            this.inputProvider = inputProvider;
+            this.outputProvider = outputProvider;
+        }
 
         public IBootstrapTaskCollection Add<T>(IFactory<T> factory)
             where T : class, IBootstrapTask
@@ -34,12 +45,34 @@ namespace Neptuo.Bootstrap.Hierarchies
             return true;
         }
 
+        /// <summary>
+        /// Adds <paramref name="type"/> to be known dependency by default.
+        /// </summary>
+        /// <param name="type">Known dependency.</param>
+        /// <returns>Self (for fluency).</returns>
+        public Bootstrapper AddDefaultDependency(Type type)
+        {
+            Ensure.NotNull(type, "type");
+            defaultDependencies.Add(type);
+            return this;
+        }
+
+        /// <summary>
+        /// Adds <typeparamref name="T" /> to be known dependency by default.
+        /// </summary>
+        /// <typeparam name="T">Type of known dependency.</typeparam>
+        /// <returns>Self (for fluency).</returns>
+        public Bootstrapper AddDefaultDependency<T>()
+        {
+            return AddDefaultDependency(typeof(T));
+        }
+
         public void Initialize()
         {
             // Sort tasks.
             IEnumerable<Type> sourceTypes = storage.Keys;
-            Sorter sorter = new Sorter(null, null);
-            IEnumerable<Type> targetTypes = sorter.Sort(sourceTypes, new List<Type>());
+            Sorter sorter = new Sorter(inputProvider, outputProvider);
+            IEnumerable<Type> targetTypes = sorter.Sort(sourceTypes, defaultDependencies);
 
             // Create instances (if needed).
             foreach (Type targetType in targetTypes)
