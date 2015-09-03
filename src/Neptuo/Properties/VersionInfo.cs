cd129681e315dc30b6b8da660425074ebf134b61
maraf <mara@neptuo.com>
2015-09-03 20:52:55 +0200

    Update version info.

--- src/Neptuo/Properties/VersionInfo.cs
+++ src/Neptuo/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo
 {
     public static class VersionInfo
     {
-        internal const string Version = "5.0.0";
+        internal const string Version = "5.0.1";
 
         public static Version GetVersion()
         {
