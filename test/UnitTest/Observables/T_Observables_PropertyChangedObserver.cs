e4377e3f2e7086a89f9d2468a4599f590659dff2
maraf <mara@neptuo.com>
2015-11-18 12:10:19 +0100

    Add PropertyChangedObserver to Observers. Add tests.

---
+++ test/UnitTest/Observables/T_Observables_PropertyChangedObserver.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Observables
+{
+    [TestClass]
+    public class T_Observables_PropertyChangedObserver
+    {
+        [TestMethod]
+        public void Base()
+        {
+            int firstNameCounter = 0;
+            int lastNameCounter = 0;
+            int fullNameCounter = 0;
+
+            Action<MainModel> firstNameHandler = m => firstNameCounter++;
+
+            MainModel model = new MainModel();
+            PropertyChangedObserver<MainModel> modelObserver = new PropertyChangedObserver<MainModel>(model);
+
+            // 1) All observers
+            modelObserver.Add(m => m.FirstName, firstNameHandler);
+            modelObserver.Add(m => m.LastName, m => lastNameCounter++);
+            modelObserver.Add(m => m.FullName, m => fullNameCounter++);
+
+            model.FirstName = "John";
+            model.LastName = "Doe";
+
+            Assert.AreEqual(1, firstNameCounter);
+            Assert.AreEqual(1, lastNameCounter);
+            Assert.AreEqual(2, fullNameCounter);
+
+            // 2) Remove single observer.
+            modelObserver.Remove(m => m.FirstName, firstNameHandler);
+
+            model.FirstName = String.Empty;
+
+            Assert.AreEqual(1, firstNameCounter);
+            Assert.AreEqual(1, lastNameCounter);
+            Assert.AreEqual(3, fullNameCounter);
+
+            // 3) Dispose observer.
+            modelObserver.Dispose();
+
+            model.FirstName = String.Empty;
+            model.LastName = String.Empty;
+
+            Assert.AreEqual(1, firstNameCounter);
+            Assert.AreEqual(1, lastNameCounter);
+            Assert.AreEqual(3, fullNameCounter);
+        }
+    }
+}
---
+++ test/UnitTest/Observables/_Models.cs
@@ -0,0 +1,49 @@
+﻿using Neptuo.Linq.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Observables
+{
+    public class MainModel : ObservableObject
+    {
+        private static readonly string fullNamePropertyName = TypeHelper.PropertyName<MainModel>(m => m.FullName);
+
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    RaisePropertyChanged();
+                    RaisePropertyChanged(fullNamePropertyName);
+                }
+            }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    RaisePropertyChanged();
+                    RaisePropertyChanged(fullNamePropertyName);
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+    }
+}
