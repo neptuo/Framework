4e18edfd9a115ce08b0401051a233b243504990b
maraf <mara@neptuo.com>
2015-11-23 15:43:44 +0100

    Define events.

---
+++ src/Neptuo.FileSystems/Events/DirectoryDeleted.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.FileSystems.Events
+{
+    /// <summary>
+    /// Event raised when directory is deleted.
+    /// </summary>
+    public class DirectoryDeleted
+    {
+        /// <summary>
+        /// Deleted directory.
+        /// </summary>
+        public IDirectory Directory { get; private set; }
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="directory">Deleted directory.</param>
+        public DirectoryDeleted(IDirectory directory)
+        {
+            Ensure.NotNull(directory, "directory");
+            Directory = directory;
+        }
+    }
+}
---
+++ src/Neptuo.FileSystems/Events/FileCreated.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.FileSystems.Events
+{
+    /// <summary>
+    /// Event raised when new file is created.
+    /// </summary>
+    public class FileCreated
+    {
+        /// <summary>
+        /// New file.
+        /// </summary>
+        public IFile File { get; private set; }
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="file">New file.</param>
+        public FileCreated(IFile file)
+        {
+            Ensure.NotNull(file, "file");
+            File = file;
+        }
+    }
+}
