2d27b4e38ff756c1ad1204a079c9807a6e610279
maraf <mara@neptuo.com>
2016-01-02 22:30:24 +0100

    Base of AggregateRoot.

---
+++ src/Neptuo.EventSourcing/Models/Domains/AggregateRoot.cs
@@ -0,0 +1,38 @@
+﻿using Neptuo.Models.Keys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Models.Domains
+{
+    /// <summary>
+    /// Base for aggregate root model.
+    /// </summary>
+    public class AggregateRoot : IDomainModel<GuidKey>
+    {
+        private readonly List<object> events = new List<object>();
+
+        /// <summary>
+        /// Aggregate root unique key.
+        /// </summary>
+        public GuidKey Key { get; private set; }
+
+        /// <summary>
+        /// Enumeration of unsaved events.
+        /// </summary>
+        public IEnumerable<object> Events
+        {
+            get { return events; }
+        }
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        public AggregateRoot()
+        {
+            Key = GuidKey.Create(Guid.NewGuid(), GetType().Name);
+        }
+    }
+}
