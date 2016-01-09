942a7663b41f30baad1656a3446218874e969a7d
maraf <mara@neptuo.com>
2016-01-09 18:20:34 +0100

    Base project.

---
+++ src/Neptuo.Formatters/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using Neptuo.Formatters;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Neptuo.Formatters")]
+[assembly: AssemblyDescription("Serialization and deserialization formatters for Neptuo.")]
+
+[assembly: AssemblyVersion(VersionInfo.Version)]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
+[assembly: AssemblyFileVersion(VersionInfo.Version)]
