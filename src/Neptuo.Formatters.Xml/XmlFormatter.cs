25a6971d4bf404bd2971709dda381de157f08e6a
maraf <mara@neptuo.com>
2016-01-12 07:54:43 +0100

    Add XML implementation of formatter.

---
+++ src/Neptuo.Formatters.Xml/XmlFormatter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Neptuo.Formatters
+{
+    /// <summary>
+    /// System.Xml implementation of <see cref="ISerializer"/> and <see cref="IDeserializer"/>.
+    /// </summary>
+    public class XmlFormatter : ISerializer, IDeserializer
+    {
+        /// <summary>
+        /// Tries to catch XML related exceptions and process them as <c>false</c> result.
+        /// </summary>
+        /// <param name="handler">The action to decorate.</param>
+        /// <returns><c>true</c> if <paramref name="handler"/> executed successfully; <c>false</c> otherwise.</returns>
+        private static bool RunWithCatch(Action handler)
+        {
+            try
+            {
+                handler();
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+            catch (XmlException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public Task<bool> TrySerializeAsync(object input, ISerializerContext context)
+        {
+            return Task.Factory.StartNew(() => RunWithCatch(() =>
+            {
+                XmlSerializer serializer = new XmlSerializer(context.InputType);
+                serializer.Serialize(context.Output, input);
+            }));
+        }
+
+        public Task<bool> TryDeserializeAsync(Stream input, IDeserializerContext context)
+        {
+            return Task.Factory.StartNew(() => RunWithCatch(() =>
+            {
+                XmlSerializer serializer = new XmlSerializer(context.OutputType);
+                object output = serializer.Deserialize(input);
+                context.Output = output;
+            }));
+        }
+    }
+}
