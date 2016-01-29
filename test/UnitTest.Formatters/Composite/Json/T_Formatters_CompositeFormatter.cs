e15b8d8e015a6bb40ef8266b4d5f9585e9a75296
maraf <mara@neptuo.com>
2016-01-29 17:51:43 +0100

    Extend perf test to inclide raw composite storage.

--- test/UnitTest.Formatters/Composite/Json/T_Formatters_CompositeFormatter.cs
+++ test/UnitTest.Formatters/Composite/Json/T_Formatters_CompositeFormatter.cs
@@ -7,6 +7,7 @@ using Neptuo.Formatters.Metadata;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -45,7 +46,7 @@ namespace UnitTest.Formatters.Composite.Json
 
                 stream.Seek(0, SeekOrigin.Begin);
 
-                IDeserializerContext context =  new DefaultDeserializerContext(typeof(UserModel));
+                IDeserializerContext context = new DefaultDeserializerContext(typeof(UserModel));
                 Task<bool> isDeserializedTask = formatter.TryDeserializeAsync(stream, context);
                 isDeserializedTask.Wait();
 
@@ -65,6 +66,7 @@ namespace UnitTest.Formatters.Composite.Json
             long newtonSoft = 0;
             long buildCompositeType = 0;
             long composite = 0;
+            long compositeStorage = 0;
 
             // NEWTONSOFT.JSON DIRECTLY.
             sw.Restart();
@@ -122,13 +124,63 @@ namespace UnitTest.Formatters.Composite.Json
             sw.Stop();
             composite = sw.ElapsedMilliseconds;
 
+
+
+            // COMPOSITE STORAGE
+            sw.Restart();
+            for (int i = 0; i < count; i++)
+            {
+                ICompositeStorage storage = new JsonCompositeStorage();
+                storage.Add("Name", "Test.UserModel");
+                storage.Add("Version", 1);
+
+                ICompositeStorage payloadStorage = storage.Add("Payload");
+                payloadStorage.Add("FirstName", "John");
+                payloadStorage.Add("LastName", "Doe");
+                payloadStorage.Add("Direction", ListSortDirection.Descending);
+                payloadStorage.Add("IDs", new int[] { 1, 2, 3 });
+                payloadStorage.Add("Keys", new List<int>() { 4, 5, 6 });
+
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    storage.StoreAsync(stream).Wait();
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    string json = null;
+                    using (StreamReader reader = new StreamReader(stream, Encoding.UTF8, true, 1024, true))
+                        json = reader.ReadToEnd();
+
+                    stream.Seek(0, SeekOrigin.Begin);
+                    storage.LoadAsync(stream).Wait();
+                }
+
+                string value;
+                storage.TryGet("Name", out value);
+                int version;
+                storage.TryGet("Version", out version);
+                storage.TryGet("Payload", out payloadStorage);
+                payloadStorage.TryGet("FirstName", out value);
+                payloadStorage.TryGet("LastName", out value);
+
+                ListSortDirection direction;
+                Assert.AreEqual(true, payloadStorage.TryGet("Direction", out direction));
+                int[] ids;
+                payloadStorage.TryGet("IDs", out ids);
+                List<int> keys;
+                payloadStorage.TryGet("Keys", out keys);
+            }
+            sw.Stop();
+            compositeStorage = sw.ElapsedMilliseconds;
+
+
             if (Directory.Exists("C:/Temp"))
             {
                 File.WriteAllLines("C:/Temp/FormatterPerf.txt", new string[] 
                 {
                     String.Format("Newtonsoft.Json directly:    {0}ms", newtonSoft),
                     String.Format("Build composite type:        {0}ms", buildCompositeType),
-                    String.Format("Composite+Newtonsoft.Json:   {0}ms", composite)
+                    String.Format("Composite+Newtonsoft.Json:   {0}ms", composite),
+                    String.Format("CompositeStorage:   {0}ms", compositeStorage)
                 });
             }
         }
