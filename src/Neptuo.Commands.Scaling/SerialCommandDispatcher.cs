d9b97a9262036a2745b8e56d78b5a174fb046897
maraf <mara@neptuo.com>
2016-02-15 13:18:25 +0100

    Basic implementation of serial command dispatcher.

--- src/Neptuo.Commands.Scaling/SerialCommandDispatcher.cs
+++ src/Neptuo.Commands.Scaling/SerialCommandDispatcher.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Neptuo.Commands
@@ -10,16 +11,60 @@ namespace Neptuo.Commands
     /// The implementation of <see cref="ICommandDispatcher"/> which executes all commands in serie (one by one - using queue).
     /// Commands are processed in separate thread and <see cref="HandleAsync"/> returns control right after storing command for dispatching.
     /// </summary>
-    public class SerialCommandDispatcher : ICommandDispatcher
+    public class SerialCommandDispatcher : DisposableBase, ICommandDispatcher
     {
         private readonly Queue<object> storage = new Queue<object>();
         private readonly object storageLock = new object();
+        private readonly Thread worker;
+        private readonly ICommandDispatcher innerDispatcher;
+
+        public SerialCommandDispatcher(ICommandDispatcher innerDispatcher)
+        {
+            Ensure.NotNull(innerDispatcher, "innerDispatcher");
+            this.innerDispatcher = innerDispatcher;
+
+            worker = new Thread(OnThread);
+            worker.Start();
+        }
 
         public Task HandleAsync<TCommand>(TCommand command)
         {
+            Ensure.NotNull(command, "command");
 
+            lock (storageLock)
+                storage.Enqueue(command);
 
-            throw new NotImplementedException();
+            return Task.FromResult(true);
+        }
+
+        private void OnThread(object parameter)
+        {
+            while (true)
+            {
+                bool isCommandProcessed = false;
+
+                lock (storageLock)
+                {
+                    if (storage.Count > 0)
+                    {
+                        object command = storage.Dequeue();
+                        isCommandProcessed = true;
+                        innerDispatcher.HandleAsync(command).Wait();
+                    }
+                }
+
+                if (!isCommandProcessed)
+                    Thread.Sleep(100);
+            }
+        }
+
+        protected override void DisposeManagedResources()
+        {
+            base.DisposeManagedResources();
+
+            IDisposable disposable = innerDispatcher as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
         }
     }
 }
---
+++ src/Neptuo.Commands.Scaling/SerialCommandDispatcherConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Commands
+{
+    /// <summary>
+    /// Configuration properties for <see cref="SerialCommandDispatcher"/>.
+    /// </summary>
+    public class SerialCommandDispatcherConfiguration
+    {
+        /// <summary>
+        /// The number of milliseconds to suspend the worker thread before checking for new commands.
+        /// </summary>
+        public int ThreadSleepMilliseconds { get; private set; }
+    }
+}
