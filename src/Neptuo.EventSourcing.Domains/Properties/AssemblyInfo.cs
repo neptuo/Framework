1383b4d43c72125740b3063442ea13941ecc97c1
maraf <mara@neptuo.com>
2016-02-20 15:32:25 +0100

    Separation of Domain stuff (aggregates, events and commands) from CQRS infrastructure for ES.

---
+++ src/Neptuo.EventSourcing.Domains/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using Neptuo.EventSourcing.Domains;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Neptuo.EventSourcing.Domains")]
+[assembly: AssemblyDescription("Base domain classes for implementing EventSourcing and CQRS.")]
+
+[assembly: AssemblyVersion(VersionInfo.Version)]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
+[assembly: AssemblyFileVersion(VersionInfo.Version)]
