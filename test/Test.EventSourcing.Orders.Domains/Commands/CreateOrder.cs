f430021abe5c23438bb300df7858c27dd1f1fef6
maraf <mara@neptuo.com>
2016-02-22 11:35:25 +0100

    Add commands for demo domain.

---
+++ test/Test.EventSourcing.Orders.Domains/Commands/CreateOrder.cs
@@ -0,0 +1,12 @@
+﻿using Neptuo.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Domains.Commands
+{
+    public class CreateOrder : Command
+    { }
+}
---
+++ test/Test.EventSourcing.Orders.Domains/Commands/Handlers/AddOrderItemHandler.cs
@@ -0,0 +1,33 @@
+﻿using Neptuo;
+using Neptuo.Commands.Handlers;
+using Neptuo.Models.Keys;
+using Neptuo.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Domains.Commands.Handlers
+{
+    public class AddOrderItemHandler : ICommandHandler<AddOrderItem>
+    {
+        private readonly IRepository<Order, IKey> repository;
+
+        public AddOrderItemHandler(IRepository<Order, IKey> repository)
+        {
+            Ensure.NotNull(repository, "repository");
+            this.repository = repository;
+        }
+
+        public Task HandleAsync(AddOrderItem command)
+        {
+            Order order = repository.Find(command.OrderKey);
+            if (order == null)
+                throw new MissingOrderException(command.OrderKey);
+
+            order.AddItem(command.ProductKey, command.Count);
+            return Task.FromResult(true);
+        }
+    }
+}
