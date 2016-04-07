0787e5f8cab029784911c860ed74b04b7c2a0558
maraf <mara@neptuo.com>
2016-04-07 13:22:37 +0200

    Add AggregateNotFoundException and Get method (not returning null) to the repository.

---
+++ src/Neptuo.EventSourcing.Domains/Models/Repositories/AggregateNotFoundException.cs
@@ -0,0 +1,29 @@
+﻿using Neptuo.Models.Keys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Models.Repositories
+{
+    /// <summary>
+    /// Exception raised when to such aggregate root exists,
+    /// </summary>
+    public class AggregateNotFoundException : Exception
+    {
+        /// <summary>
+        /// The aggregate root key which is not found in the repository.
+        /// </summary>
+        public IKey AggregateKey { get; private set; }
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="aggregateKey">The aggregate root key which is not found in the repository.</param>
+        public AggregateNotFoundException(IKey aggregateKey)
+        {
+            AggregateKey = aggregateKey;
+        }
+    }
+}
