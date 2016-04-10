2879381c22401c9a875f5b8405f1979566a7ab4e
maraf <mara@neptuo.com>
2016-04-10 11:32:53 +0200

    Redesigning envelope.

---
+++ src/Neptuo/Envelope.cs
@@ -0,0 +1,54 @@
+﻿using Neptuo.Collections.Specialized;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo
+{
+    /// <summary>
+    /// The factory helper for creating envelopes.
+    /// </summary>
+    public static class Envelope
+    {
+        /// <summary>
+        /// Creates new instance of <see cref="Envelope{T}"/> with body.
+        /// </summary>
+        /// <typeparam name="T">Type of the body.</typeparam>
+        /// <param name="body">The body of the evelope.</param>
+        /// <returns>New instance of <see cref="Envelope{T}"/> with <paramref name="body"/>.</returns>
+        public static Envelope<T> Create<T>(T body)
+        {
+            return new Envelope<T>(body);
+        }
+    }
+    
+    /// <summary>
+    /// The wrapper around body of <typeparamref name="T"/> with metadata.
+    /// </summary>
+    /// <typeparam name="T">The type of envelope body.</typeparam>
+    public class Envelope<T>
+    {
+        /// <summary>
+        /// The body of the evelope.
+        /// </summary>
+        public T Body { get; private set; }
+
+        /// <summary>
+        /// The collection of metadata.
+        /// </summary>
+        public IKeyValueCollection Metadata { get; private set; }
+
+        /// <summary>
+        /// Creates new instance with <paramref name="body"/>.
+        /// </summary>
+        /// <param name="body">The body of the evelope.</param>
+        public Envelope(T body)
+        {
+            Ensure.NotNull(body, "body");
+            Body = body;
+            Metadata = new KeyValueCollection();
+        }
+    }
+}
---
+++ src/Neptuo/_EnvelopeExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo
+{
+    /// <summary>
+    /// Common extensions for <see cref="Envelope{T}"/>.
+    /// </summary>
+    public static class _EnvelopeExtensions
+    {
+        public static Envelope<T> AddTimeToLive<T>(this Envelope<T> envelope, TimeSpan timeToLive)
+        {
+            Ensure.NotNull(envelope, "envelope");
+            envelope.Metadata.Add("TimeToLive", timeToLive);
+            return envelope;
+        }
+
+        public static bool TryGetTimeToLive<T>(this Envelope<T> envelope, out TimeSpan timeToLive)
+        {
+            Ensure.NotNull(envelope, "envelope");
+            return envelope.Metadata.TryGet("TimeToLive", out timeToLive);
+        }
+
+
+        public static Envelope<T> AddDelay<T>(this Envelope<T> envelope, TimeSpan delay)
+        {
+            Ensure.NotNull(envelope, "envelope");
+            envelope.Metadata.Add("Delay", delay);
+            return envelope;
+        }
+
+        public static bool TryGetDelay<T>(this Envelope<T> envelope, out TimeSpan delay)
+        {
+            Ensure.NotNull(envelope, "envelope");
+            return envelope.Metadata.TryGet("Delay", out delay);
+        }
+    }
+}
