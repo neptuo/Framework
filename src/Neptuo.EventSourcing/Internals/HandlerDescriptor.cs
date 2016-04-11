cc1d21fc97ea73160d98148f2c7a6ec8dc04d047
maraf <mara@neptuo.com>
2016-04-11 14:54:09 +0200

    Add handler descriptor and starting to implement its provider.

---
+++ src/Neptuo.EventSourcing/Internals/HandlerDescriptor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Internals
+{
+    /// <summary>
+    /// Provides description of (command or event) handler.
+    /// </summary>
+    internal class HandlerDescriptor
+    {
+        /// <summary>
+        /// The instance itself.
+        /// </summary>
+        public object Handler { get; private set; }
+
+        /// <summary>
+        /// The method that executes handler with parameter.
+        /// </summary>
+        public Action<object> Execute { get; private set; }
+
+
+        /// <summary>
+        /// Whether is plain command/event handler.
+        /// </summary>
+        public bool IsPlain { get; private set; }
+
+        /// <summary>
+        /// Whether requires envelope wrapper.
+        /// </summary>
+        public bool IsEnvelope { get; private set; }
+
+        /// <summary>
+        /// Whether required context wrapper.
+        /// </summary>
+        public bool IsContext { get; private set; }
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        public HandlerDescriptor(object handler, Action<object> execute, bool isPlain, bool isEnvelope, bool isContext)
+        {
+            Ensure.NotNull(handler, "handler");
+            Ensure.NotNull(execute, "execute");
+            Handler = handler;
+            Execute = execute;
+            IsPlain = isPlain;
+            IsEnvelope = isEnvelope;
+            IsContext = isContext;
+        }
+    }
+}
---
+++ src/Neptuo.EventSourcing/Internals/HandlerDescriptorProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Internals
+{
+    internal class HandlerDescriptorProvider
+    {
+
+
+        public HandlerDescriptor Create(object handler)
+        {
+            return new HandlerDescriptor(
+                handler,
+
+            );
+        }
+    }
+}
