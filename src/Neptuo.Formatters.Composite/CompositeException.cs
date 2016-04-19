f2e3c15d4ef2852337e817b21e08333853c978a4
maraf <mara@neptuo.com>
2016-04-19 18:44:11 +0200

    CompositeException extends FormatterException.

--- src/Neptuo.Formatters.Composite/CompositeException.cs
+++ src/Neptuo.Formatters.Composite/CompositeException.cs
@@ -8,22 +8,39 @@ using System.Threading.Tasks;
 namespace Neptuo.Formatters
 {
     /// <summary>
-    /// Base composite exception.
+    /// The base for composite exceptions.
     /// </summary>
     [Serializable]
-    public class CompositeException : Exception
+    public class CompositeException : FormatterException
     {
+        /// <summary>
+        /// Creates new empty instance.
+        /// </summary>
         public CompositeException()
         { }
 
+        /// <summary>
+        /// Creates new instance with the <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">The text description of the problem.</param>
         public CompositeException(string message)
             : base(message)
         { }
 
+        /// <summary>
+        /// Creates new instance with the <paramref name="message"/> and <paramref name="inner"/> exception.
+        /// </summary>
+        /// <param name="message">The text description of the problem.</param>
+        /// <param name="inner">The inner cause of the exceptional state.</param>
         public CompositeException(string message, Exception inner)
             : base(message, inner)
         { }
 
+        /// <summary>
+        /// Creates new instance for deserialization.
+        /// </summary>
+        /// <param name="info">The serialization info.</param>
+        /// <param name="context">The streaming context.</param>
         protected CompositeException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         { }
