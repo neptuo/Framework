37485e96bd6c88f3a220d2b502d879b78c52fcc8
maraf <mara@neptuo.com>
2016-04-19 21:16:04 +0200

    Add TODO to remove direct reference.

--- src/Neptuo.Formatters.Composite.Json/Properties/VersionInfo.cs
+++ src/Neptuo.Formatters.Composite.Json/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo.Formatters.Composite.Json
 {
     public static class VersionInfo
     {
-        internal const string Version = "1.0.1";
+        internal const string Version = "1.0.1"; // TODO: Return NUGET reference to the Neptuo.Formatters.Composite.
 
         public static Version GetVersion()
         {
