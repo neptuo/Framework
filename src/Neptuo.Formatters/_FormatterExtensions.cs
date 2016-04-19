ae39913d36a368bd133da237b4ec89eb6f84ed3d
maraf <mara@neptuo.com>
2016-04-19 13:23:10 +0200

    Add DeserializeAsync extensions.

--- src/Neptuo.Formatters/_FormatterExtensions.cs
+++ src/Neptuo.Formatters/_FormatterExtensions.cs
@@ -95,5 +95,37 @@ namespace Neptuo.Formatters
         {
             return (T)Deserialize(deserializer, typeof(T), input);
         }
+
+        /// <summary>
+        /// Deserializes <paramref name="input"/> to the object of the type <paramref name="outputType"/> and returns it.
+        /// </summary>
+        /// <param name="deserializer">The deserializer to use.</param>
+        /// <param name="outputType">The type of the object to create.</param>
+        /// <param name="input">The serialized input.</param>
+        /// <returns>The deserialized object of the type <paramref name="outputType"/>.</returns>
+        public static async Task<object> DeserializeAsync(this IDeserializer deserializer, Type outputType, string input)
+        {
+            using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(input)))
+            {
+                DefaultDeserializerContext context = new DefaultDeserializerContext(outputType);
+                bool result = await deserializer.TryDeserializeAsync(stream, context);
+                if (result)
+                    return context.Output;
+            }
+
+            throw new DeserializationFailedException(outputType, input);
+        }
+
+        /// <summary>
+        /// Deserializes <paramref name="input"/> to the object of the type <typeparamref name="T"/> and returns it.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to create.</typeparam>
+        /// <param name="deserializer">The deserializer to use.</param>
+        /// <param name="input">The serialized input.</param>
+        /// <returns>The deserialized object of the type <paramref name="outputType"/>.</returns>
+        public static async Task<T> DeserializeAsync<T>(this IDeserializer deserializer, string input)
+        {
+            return (T)(await DeserializeAsync(deserializer, typeof(T), input));
+        }
     }
 }
