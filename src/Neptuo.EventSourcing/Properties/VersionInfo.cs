cc0dbdb90eb035c0d7f3f43dcd263fbb6063e564
maraf <mara@neptuo.com>
2016-04-27 21:35:14 +0200

    Update version info.

--- src/Neptuo.EventSourcing/Properties/VersionInfo.cs
+++ src/Neptuo.EventSourcing/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo.EventSourcing
 {
     public static class VersionInfo
     {
-        internal const string Version = "0.4.1";
+        internal const string Version = "0.4.2";
 
         public static Version GetVersion()
         {
