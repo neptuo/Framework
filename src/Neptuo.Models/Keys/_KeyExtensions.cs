e9b5ab24742c32fcba369b083ae2878ce556ab86
maraf <mara@neptuo.com>
2016-04-28 13:44:06 +0200

    Basic As method.

---
+++ src/Neptuo.Models/Keys/_KeyExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Models.Keys
+{
+    /// <summary>
+    /// The extensions for converting <see cref="IKey"/>.
+    /// </summary>
+    public static class _KeyExtensions
+    {
+        /// <summary>
+        /// Tries to convert/cast <paramref name="key"/> to the type <typeparamref name="TKey"/>.
+        /// </summary>
+        /// <typeparam name="TKey">The requested type of key.</typeparam>
+        /// <param name="key">The key to convert/cast.</param>
+        /// <returns>The converted/casted key.</returns>
+        public static TKey As<TKey>(this IKey key)
+            where TKey : class, IKey
+        {
+            TKey target = key as TKey;
+            if(target == null)
+                throw new NotImplementedException();
+
+            return target;
+        }
+    }
+}
