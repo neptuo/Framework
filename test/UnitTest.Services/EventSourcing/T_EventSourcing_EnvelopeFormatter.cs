4f9d951e648d437acb8677e842e035ccb8716faf
maraf <mara@neptuo.com>
2016-04-28 22:51:13 +0200

    Add test case of serialization and deserization of command with envelope which doesn't work because of missing IKey converters.

---
+++ test/UnitTest.Services/EventSourcing/T_EventSourcing_EnvelopeFormatter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neptuo.Formatters;
+using Neptuo.Formatters.Converters;
+using Orders.Domains.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.EventSourcing
+{
+    [TestClass]
+    public class T_EventSourcing_EnvelopeFormatter
+    {
+        [TestMethod]
+        public void SerializeAndDeserializeWithMetadata()
+        {
+            Converts.Repository
+                .AddJsonEnumSearchHandler()
+                .AddJsonObjectSearchHandler()
+                .AddJsonPrimitivesSearchHandler();
+
+            IFormatter formatter = new EnvelopeFormatter(new ExtendedComposityTypeFormatter());
+
+            TimeSpan delay = TimeSpan.FromSeconds(50);
+            string sourceID = "AbcDef";
+
+            Envelope<CreateOrder> envelope = new Envelope<CreateOrder>(new CreateOrder())
+                .AddDelay(delay)
+                .AddSourceID(sourceID);
+
+            string json = formatter.Serialize(envelope);
+            envelope = formatter.Deserialize<Envelope<CreateOrder>>(json);
+
+            Assert.AreEqual(delay, envelope.GetDelay());
+            Assert.AreEqual(sourceID, envelope.GetSourceID());
+        }
+    }
+}
--- test/UnitTest.Services/EventSourcing/_Services.cs
+++ test/UnitTest.Services/EventSourcing/_Services.cs
@@ -1,13 +1,20 @@
-﻿using Neptuo.Data;
+﻿using Neptuo.Activators;
+using Neptuo.Collections.Specialized;
+using Neptuo.Commands;
+using Neptuo.Data;
 using Neptuo.Events;
 using Neptuo.Events.Handlers;
+using Neptuo.Formatters;
+using Neptuo.Formatters.Metadata;
 using Neptuo.Models.Domains;
 using Neptuo.Models.Keys;
 using Neptuo.Models.Repositories;
 using Orders.Domains.Events;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -49,4 +56,85 @@ namespace Neptuo.EventSourcing
         }
     }
 
+
+    public class ExtendedComposityTypeFormatter : CompositeTypeFormatter
+    {
+        private readonly EventExtender events = new EventExtender();
+        private readonly CommandExtender commands = new CommandExtender();
+
+        public const string MetadataKey = "Metadata";
+
+        public ExtendedComposityTypeFormatter()
+            : base(new ReflectionCompositeTypeProvider(new ReflectionCompositeDelegateFactory(), BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public), new DefaultFactory<JsonCompositeStorage>())
+        { }
+
+        protected override bool TryLoad(Stream input, IDeserializerContext context, CompositeType type, ICompositeStorage storage)
+        {
+            if (base.TryLoad(input, context, type, storage))
+            {
+                Event payload = context.Output as Event;
+                if (payload != null)
+                {
+                    events.Load(GetOrAddPayloadStorage(storage), payload);
+                }
+                else
+                {
+                    Command command = context.Output as Command;
+                    if (command != null)
+                        commands.Load(GetOrAddPayloadStorage(storage), command);
+                }
+
+                IKeyValueCollection metadata;
+                ICompositeStorage metadataStorage;
+                if (context.TryGetEnvelopeMetadata(out metadata) && storage.TryGet(MetadataKey, out metadataStorage))
+                {
+                    foreach (string key in metadataStorage.Keys)
+                        metadata.Add(key, metadataStorage.Get<object>(key));
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        protected override bool TryStore(object input, ISerializerContext context, CompositeType type, CompositeVersion typeVersion, ICompositeStorage storage)
+        {
+            if (base.TryStore(input, context, type, typeVersion, storage))
+            {
+                Event payload = input as Event;
+                if (payload != null)
+                {
+                    events.Store(GetOrAddPayloadStorage(storage), payload);
+                }
+                else
+                {
+                    Command command = input as Command;
+                    if (command != null)
+                        commands.Store(GetOrAddPayloadStorage(storage), command);
+                }
+
+                IReadOnlyKeyValueCollection metadata;
+                if (context.TryGetEnvelopeMetadata(out metadata))
+                {
+                    ICompositeStorage metadataStorage = storage.Add(MetadataKey);
+                    foreach (string key in metadata.Keys)
+                        metadataStorage.Add(key, metadata.Get<object>(key));
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private ICompositeStorage GetOrAddPayloadStorage(ICompositeStorage storage)
+        {
+            ICompositeStorage payloadStorage;
+            if (storage.TryGet(CompositeTypeFormatter.Name.Payload, out payloadStorage))
+                return payloadStorage;
+
+            return storage.Add(CompositeTypeFormatter.Name.Payload);
+        }
+    }
 }
