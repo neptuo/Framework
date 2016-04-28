f8e9753806d6917b6ddbd09371f95142a7de510f
maraf <mara@neptuo.com>
2016-04-28 22:54:19 +0200

    Add project for composite formatter in EventSourcing.

---
+++ src/Neptuo.EventSourcing.Formatters.Composite/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using Neptuo.EventSourcing;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Neptuo.EventSourcing.Formatters.Composite")]
+[assembly: AssemblyDescription("Composite formatters support for EventSourcing.")]
+
+[assembly: AssemblyVersion(VersionInfo.Version)]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
+[assembly: AssemblyFileVersion(VersionInfo.Version)]
+
+[assembly: InternalsVisibleTo("UnitTest.Services")]
