29ffec8d31bb1badea3636a5ecf03f9fe4c56bfc
maraf <mara@neptuo.com>
2016-04-28 10:15:41 +0200

    Add contract for generic exception handler. Add empty classes for builders.

---
+++ src/Neptuo.Exceptions/Handlers/ExceptionHandlerBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Exceptions.Handlers
+{
+    /// <summary>
+    /// The builder of various filters for handling specific exceptions.
+    /// </summary>
+    public class ExceptionHandlerBuilder : ExceptionHandlerBuilder<Exception>
+    {
+
+    }
+
+    public class ExceptionHandlerBuilder<T>
+        where T : Exception
+    {
+        public ExceptionHandlerBuilder<TBase> Filter<TBase>()
+            where TBase : T
+        {
+            throw new NotImplementedException();
+        }
+
+        public ExceptionHandlerBuilder<T> Filter(Func<T, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ExceptionHandlerBuilder<T> Add(IExceptionHandler<T> handler)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Neptuo.Exceptions/Handlers/IExceptionHandler.cs
+++ src/Neptuo.Exceptions/Handlers/IExceptionHandler.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Neptuo.Exceptions.Handlers
 {
     /// <summary>
-    /// The handler for exception raised during exection.
+    /// The handler for exceptions raised during execution.
     /// </summary>
     public interface IExceptionHandler
     {
@@ -16,5 +16,19 @@ namespace Neptuo.Exceptions.Handlers
         /// </summary>
         /// <param name="exception">The exception to process.</param>
         void Handle(Exception exception);
+    }
+
+    /// <summary>
+    /// The handler for exceptions of type <typeparamref name="T" /> raised during execution.
+    /// </summary>
+    /// <typeparam name="T">The type of the exception to handle.</typeparam>
+    public interface IExceptionHandler<T>
+        where T : Exception
+    {
+        /// <summary>
+        /// Processes <paramref name="exception"/> raised during execution.
+        /// </summary>
+        /// <param name="exception">The exception to process.</param>
+        void Handle(T exception);
     }
 }
