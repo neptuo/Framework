f0d77d169de823604b2822c451b1058b2c70e885
maraf <mara@neptuo.com>
2016-08-18 14:31:27 +0200

    Add contract for aggregate Snapshot.

---
+++ src/Neptuo.EventSourcing/Models/Snapshots/ISnapshot.cs
@@ -0,0 +1,20 @@
+﻿using Neptuo.Models.Keys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Models.Snapshots
+{
+    /// <summary>
+    /// The marker interface for aggregate root snapshot class.
+    /// </summary>
+    public interface ISnapshot
+    {
+        /// <summary>
+        /// Returns key of the aggregate.
+        /// </summary>
+        IKey AggregateKey { get; }
+    }
+}
