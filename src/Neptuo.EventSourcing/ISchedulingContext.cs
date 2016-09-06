c900391cbd2d55e17d42a61be946b12781cf9de6
maraf <mara@neptuo.com>
2016-09-06 21:45:17 +0200

    Add comments to the ISchedulingContext.

--- src/Neptuo.EventSourcing/ISchedulingContext.cs
+++ src/Neptuo.EventSourcing/ISchedulingContext.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace Neptuo
 {
+    /// <summary>
+    /// The context information passed to the <see cref="ISchedulingProvider.Schedule(ISchedulingContext)"/> to be executed in the future.
+    /// </summary>
     public interface ISchedulingContext
     {
+        /// <summary>
+        /// Gets the envelope to schedule for execution in the future.
+        /// </summary>
         Envelope Envelope { get; }
 
+        /// <summary>
+        /// The method that should be executed when it's time.
+        /// </summary>
         void Execute();
     }
 }
