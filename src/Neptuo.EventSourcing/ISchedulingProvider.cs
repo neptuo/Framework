e787c070797af1d52a33a96c01f6af5afa8e6b94
maraf <mara@neptuo.com>
2016-09-06 21:22:11 +0200

    Rewriting ISchedulingProvider to be the provided of delayed execution.

--- src/Neptuo.EventSourcing/ISchedulingProvider.cs
+++ src/Neptuo.EventSourcing/ISchedulingProvider.cs
@@ -16,6 +16,23 @@ namespace Neptuo
         /// </summary>
         /// <param name="executeAt">The date and time when execution should be done.</param>
         /// <returns>A delay after which command or event will be executed.</returns>
+        [Obsolete]
         TimeSpan Compute(DateTime executeAt);
+
+        /// <summary>
+        /// When <c>true</c> is returned, the <paramref name="envelope"/> should be scheduled for later execution using <see cref="Schedule(ISchedulingContext)"/>.
+        /// </summary>
+        /// <param name="envelope">The envelope to be tested for later execution.</param>
+        /// <returns>
+        /// <c>true</c> is returned, the <paramref name="envelope"/> should be scheduled for later execution using <see cref="Schedule(ISchedulingContext)"/>.
+        /// <c>false</c> when the <paramref name="envelope"/> should be executed immediately.
+        /// </returns>
+        bool IsLaterExecutionRequired(Envelope envelope);
+
+        /// <summary>
+        /// Schedules <paramref name="context"/> to be executed in the future.
+        /// </summary>
+        /// <param name="context">The context to be executed in the future.</param>
+        void Schedule(ISchedulingContext context);
     }
 }
