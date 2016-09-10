08386290993db529610bce4d085b6a7243c9dbf9
maraf <mara@neptuo.com>
2016-09-10 21:43:46 +0200

    Update VersionInfo. Update nuget references.

--- src/Neptuo.EventSourcing.Domains/Properties/AssemblyInfo.cs
+++ src/Neptuo.EventSourcing.Domains/Properties/AssemblyInfo.cs
@@ -5,14 +5,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Neptuo.EventSourcing.Domains")]
 [assembly: AssemblyDescription("Domain classes for implementing EventSourcing and CQRS.")]
 
-#if !DEBUG
-
-Fix version attribute!
-
-#endif
-
 [assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version + "-beta1")]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
 [assembly: AssemblyFileVersion(VersionInfo.Version)]
 
 [assembly: InternalsVisibleTo("Neptuo.EventSourcing")]
--- src/Neptuo.EventSourcing/Properties/AssemblyInfo.cs
+++ src/Neptuo.EventSourcing/Properties/AssemblyInfo.cs
@@ -5,15 +5,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Neptuo.EventSourcing")]
 [assembly: AssemblyDescription("Event sourcing support for Models and Events.")]
 
-#if !DEBUG
-
-Fix version attribute!
-Fix reference to Domains!
-
-#endif
-
 [assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version + "-beta5")]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
 [assembly: AssemblyFileVersion(VersionInfo.Version)]
 
 [assembly: InternalsVisibleTo("UnitTest.Services")]
