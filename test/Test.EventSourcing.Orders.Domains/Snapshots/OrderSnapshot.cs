bb740b62cae34c1f73556e93105a982aae5ecb6a
maraf <mara@neptuo.com>
2016-09-10 18:04:51 +0200

    Update demo Order to support snapshot.

---
+++ test/Test.EventSourcing.Orders.Domains/Snapshots/OrderSnapshot.cs
@@ -0,0 +1,18 @@
+﻿using Neptuo.Models.Keys;
+using Neptuo.Models.Snapshots;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Domains.Snapshots
+{
+    public class OrderSnapshot : ISnapshot
+    {
+        public IKey AggregateKey { get; set; }
+        public int Version { get; set; }
+        public List<OrderItem> Items { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
