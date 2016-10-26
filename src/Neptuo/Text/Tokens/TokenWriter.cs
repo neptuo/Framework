708389a350d6920310ca0f99314235652de1302a
maraf <mara@neptuo.com>
2016-10-26 06:38:46 +0200

    Add support for attributes to the TokenWriter. Add tests for TokenWriter.

--- src/Neptuo/Text/Tokens/TokenWriter.cs
+++ src/Neptuo/Text/Tokens/TokenWriter.cs
@@ -15,19 +15,29 @@ namespace Neptuo.Text.Tokens
         private readonly List<TokenWriterItem> items = new List<TokenWriterItem>();
 
         /// <summary>
-        /// Creates new instance for format string as <paramref name="format"/>.
+        /// Creates new instance for format string as <paramref name="format"/> without support for attributes on tokens.
         /// </summary>
         /// <param name="format">Format string.</param>
         public TokenWriter(string format)
+            : this(format, false)
         {
-            Ensure.NotNullOrEmpty(format, "format");
-            TokenFormatHelper.Parse(format, items);
         }
 
         /// <summary>
-        /// For each found token, calles <paramref name="tokenMapper"/> with token name and replaces that token with returned value.
+        /// Creates new instance for format string as <paramref name="format"/>.
         /// </summary>
-        /// <param name="tokenMapper">Token name to token value replacer.</param>
+        /// <param name="format">Format string.</param>
+        /// <param name="isAttributeSupported">If <c>true</c> attributes on tokens are supported.</param>
+        public TokenWriter(string format, bool isAttributeSupported)
+        {
+            Ensure.NotNullOrEmpty(format, "format");
+            TokenFormatHelper.Parse(format, items, isAttributeSupported);
+        }
+
+        /// <summary>
+        /// For each found token, calls <paramref name="tokenMapper"/> with token name and replaces that token with returned value.
+        /// </summary>
+        /// <param name="tokenMapper">Token name to value replacer.</param>
         /// <returns>Formatted string with replaced tokens.</returns>
         public string Format(Func<string, string> tokenMapper)
         {
@@ -35,9 +45,28 @@ namespace Neptuo.Text.Tokens
             foreach (TokenWriterItem item in items)
             {
                 if (item.IsToken)
-                    result.Append(tokenMapper(item.Value));
+                    result.Append(tokenMapper(item.Token.Fullname));
                 else
-                    result.Append(item.Value);
+                    result.Append(item.Text);
+            }
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// For each found token, calls <paramref name="tokenMapper"/> with token name and replaces that token with returned value.
+        /// </summary>
+        /// <param name="tokenMapper">Token to value replacer.</param>
+        /// <returns>Formatted string with replaced tokens.</returns>
+        public string Format(Func<Token, string> tokenMapper)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (TokenWriterItem item in items)
+            {
+                if (item.IsToken)
+                    result.Append(tokenMapper(item.Token));
+                else
+                    result.Append(item.Text);
             }
 
             return result.ToString();
@@ -54,9 +83,9 @@ namespace Neptuo.Text.Tokens
             foreach (TokenWriterItem item in items)
             {
                 if (item.IsToken)
-                    result.Append(tokenMapper.Get(item.Value, ""));
+                    result.Append(tokenMapper.Get(item.Token.Fullname, ""));
                 else
-                    result.Append(item.Value);
+                    result.Append(item.Text);
             }
 
             return result.ToString();
@@ -68,7 +97,7 @@ namespace Neptuo.Text.Tokens
     /// </summary>
     internal class TokenFormatHelper
     {
-        public static void Parse(string format, List<TokenWriterItem> items)
+        public static void Parse(string format, List<TokenWriterItem> items, bool isAttributeSupported)
         {
             int lastTokenEndIndex = 0;
 
@@ -76,16 +105,17 @@ namespace Neptuo.Text.Tokens
             parser.Configuration.AllowTextContent = true;
             parser.Configuration.AllowEscapeSequence = true;
             parser.Configuration.AllowMultipleTokens = true;
+            parser.Configuration.AllowAttributes = isAttributeSupported;
             parser.OnParsedToken += (sender, e) =>
             {
                 if (e.StartPosition > 0)
                 {
                     int startIndex = items.Any() ? lastTokenEndIndex : 0;
                     if (startIndex < e.StartPosition)
-                        items.Add(new TokenWriterItem(false, format.Substring(startIndex, e.StartPosition - startIndex)));
+                        items.Add(new TokenWriterItem(format.Substring(startIndex, e.StartPosition - startIndex)));
                 }
 
-                items.Add(new TokenWriterItem(true, e.Token.Fullname));
+                items.Add(new TokenWriterItem(e.Token));
                 lastTokenEndIndex = e.EndPosition;
             };
 
@@ -93,7 +123,7 @@ namespace Neptuo.Text.Tokens
                 throw Ensure.Exception.ArgumentOutOfRange("format", "Format string '{0}' doesn't contain valid token format string.", format);
 
             if (lastTokenEndIndex < format.Length)
-                items.Add(new TokenWriterItem(false, format.Substring(lastTokenEndIndex)));
+                items.Add(new TokenWriterItem(format.Substring(lastTokenEndIndex)));
         }
     }
 
@@ -103,13 +133,22 @@ namespace Neptuo.Text.Tokens
     /// </summary>
     internal class TokenWriterItem
     {
-        public bool IsToken { get; private set; }
-        public string Value { get; private set; }
+        public string Text { get; private set; }
+        public Token Token { get; private set; }
 
-        public TokenWriterItem(bool isToken, string value)
+        public bool IsToken
         {
-            IsToken = isToken;
-            Value = value;
+            get { return Token != null; }
+        }
+
+        public TokenWriterItem(string text)
+        {
+            Text = text;
+        }
+
+        public TokenWriterItem(Token token)
+        {
+            Token = token;
         }
     }
 }
---
+++ test/UnitTest/Text/Tokens/T_Text_Tokens_TokenWriter.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neptuo.Collections.Specialized;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Text.Tokens
+{
+    [TestClass]
+    public class T_Text_Tokens_TokenWriter
+    {
+        [TestMethod]
+        public void KeyValueCollection()
+        {
+            TokenWriter writer = new TokenWriter("Hello, my name is {Name} and I am from {City}.");
+            string result = writer.Format(new KeyValueCollection().Add("Name", "Peter").Add("City", "Prague"));
+            Assert.AreEqual("Hello, my name is Peter and I am from Prague.", result);
+        }
+
+        [TestMethod]
+        public void NameMapper()
+        {
+            TokenWriter writer = new TokenWriter("Hello, my name is {Name} and I am from {City}.");
+            string result = writer.Format(name =>
+            {
+                if (name == "Name")
+                    return "Peter";
+
+                if (name == "City")
+                    return "Prague";
+
+                return null;
+            });
+            Assert.AreEqual("Hello, my name is Peter and I am from Prague.", result);
+        }
+
+        [TestMethod]
+        public void FullWithAttributes()
+        {
+            TokenWriter writer = new TokenWriter("Hello, my name is {Name} and I am from {City, Default=Prague}.", true);
+            string result = writer.Format(FormatToken);
+        }
+
+        private string FormatToken(Token token)
+        {
+            if (token.Fullname == "Name")
+                return "Peter";
+
+            if (token.Fullname == "City")
+                return token.Attributes.First(a => a.Name == "Default").Value;
+
+            return null;
+        }
+    }
+}
