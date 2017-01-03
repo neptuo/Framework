55aabcdc15e33b5ff7ea9f30a1cf04187b90b19b
maraf <mara@neptuo.com>
2017-01-03 16:44:08 +0100

    Adding support for convention event handling methods in aggregates.

--- src/Neptuo.EventSourcing.Domains/Models/Domains/AggregateRootHandlerCollection.cs
+++ src/Neptuo.EventSourcing.Domains/Models/Domains/AggregateRootHandlerCollection.cs
@@ -15,19 +15,47 @@ namespace Neptuo.Models.Domains
     /// </summary>
     internal class AggregateRootHandlerCollection
     {
+        private static readonly string eventMethodName = "Handle";
         private static readonly string eventHandlerMethodName = TypeHelper.MethodName<IEventHandler<object>, object, Task>(h => h.HandleAsync);
         private readonly Dictionary<Type, Dictionary<Type, Action<object, object>>> storage = new Dictionary<Type, Dictionary<Type, Action<object, object>>>();
 
         public AggregateRootHandlerCollection Map(Type type)
         {
             Ensure.NotNull(type, "type");
+            MapConventionMethods(type);
+            MapInterfaceMethods(type);
+            return this;
+        }
 
+        private void MapConventionMethods(Type type)
+        {
+            MethodInfo[] methodInfos = type.GetMethods(BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.NonPublic);
+            foreach (MethodInfo methodInfo in methodInfos)
+            {
+                if (methodInfo.Name == eventMethodName)
+                {
+                    ParameterInfo[] parameters = methodInfo.GetParameters();
+                    if (parameters.Length == 1)
+                    {
+                        Dictionary<Type, Action<object, object>> typeHandlers;
+                        if (!storage.TryGetValue(type, out typeHandlers))
+                            storage[type] = typeHandlers = new Dictionary<Type, Action<object, object>>();
+
+                        Type eventType = parameters[0].ParameterType;
+                        typeHandlers[eventType] = (model, payload) => methodInfo.Invoke(model, new object[] { payload });
+                    }
+                }
+            }
+        }
+
+        private void MapInterfaceMethods(Type type)
+        {
             foreach (Type interfaceType in type.GetInterfaces())
             {
                 if (interfaceType.IsGenericType && interfaceType.GetGenericTypeDefinition() == typeof(IEventHandler<>))
                 {
                     Type eventType = interfaceType.GetGenericArguments()[0];
-                    
+
                     //MethodInfo eventMethod = type.GetMethod(eventHandlerMethodName, new Type[] { eventType });
                     // EndsWith is here because explicitly implemented methods have very complicated name.
                     MethodInfo eventMethod = type.GetInterfaceMap(interfaceType).TargetMethods
@@ -40,8 +68,6 @@ namespace Neptuo.Models.Domains
                     typeHandlers[eventType] = (model, payload) => eventMethod.Invoke(model, new object[] { payload });
                 }
             }
-
-            return this;
         }
 
         public bool Has(Type type)
