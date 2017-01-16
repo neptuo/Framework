d5140be553ba7543b0d4a3533e0ac42d53014671
maraf <mara@neptuo.com>
2017-01-16 16:32:56 +0100

    Add test case for NotSupported.

---
+++ test/UnitTest/Exceptions/EnsureExceptionTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Exceptions
+{
+    [TestClass]
+    public class EnsureExceptionTest
+    {
+        public enum Value
+        {
+            One,
+            Two
+        }
+
+        [TestMethod]
+        public void NotSupportedEnumValue()
+        {
+            NotSupportedException exception = Ensure.Exception.NotSupported(Value.Two);
+            Assert.IsNotNull(exception);
+            Assert.AreEqual($"The value '{nameof(Value.Two)}' from the '{typeof(Value).FullName}' is not supported in this context.", exception.Message);
+        }
+    }
+}
