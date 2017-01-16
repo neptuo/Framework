1635a51faf27dcd436c325f04e4df288fb2b3b26
maraf <mara@neptuo.com>
2017-01-16 17:10:07 +0100

    Update VersionInfo.

--- src/Neptuo.Formatters.Composite/Properties/VersionInfo.cs
+++ src/Neptuo.Formatters.Composite/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo.Formatters.Composite
 {
     public static class VersionInfo
     {
-        internal const string Version = "2.1.0";
+        internal const string Version = "2.1.1";
 
         public static Version GetVersion()
         {
