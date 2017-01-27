120f73e077953c354ef3ca19babf94a7d433524c
Marek Fišera <mara@neptuo.com>
2017-01-27 16:37:12 +0100

    Change description. Add preview version.

--- src/Neptuo.EventSourcing/Properties/AssemblyInfo.cs
+++ src/Neptuo.EventSourcing/Properties/AssemblyInfo.cs
@@ -3,10 +3,10 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Neptuo.EventSourcing")]
-[assembly: AssemblyDescription("Event sourcing support for Models and Events.")]
+[assembly: AssemblyDescription("Event sourcing hosting infrastructure.")]
 
 [assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version + VersionInfo.Preview)]
 [assembly: AssemblyFileVersion(VersionInfo.Version)]
 
 [assembly: InternalsVisibleTo("UnitTest.Services")]
--- src/Neptuo.EventSourcing/Properties/VersionInfo.cs
+++ src/Neptuo.EventSourcing/Properties/VersionInfo.cs
@@ -4,7 +4,8 @@ namespace Neptuo.EventSourcing
 {
     public static class VersionInfo
     {
-        internal const string Version = "1.1.3";
+        internal const string Version = "1.2.0";
+        internal const string Preview = "beta1";
 
         public static Version GetVersion()
         {
