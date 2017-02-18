c8b8354e2c23b99d719869c8c4ee953b1471fe27
Marek Fišera <mara@neptuo.com>
2017-02-18 20:50:02 +0100

    Add comments. Add test case for ReThrowExceptionHandler.

--- src/Neptuo.Exceptions/Handlers/ReThrowExceptionHandler.cs
+++ src/Neptuo.Exceptions/Handlers/ReThrowExceptionHandler.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Exceptions.Handlers
 {
+    /// <summary>
+    /// An implementation of <see cref="IExceptionHandler"/> and <see cref="IExceptionHandler{T}"/> which simply rethrows the exception.
+    /// It uses <see cref="ExceptionDispatchInfo"/> to restore captured exception state.
+    /// </summary>
     public class ReThrowExceptionHandler : IExceptionHandler, IExceptionHandler<Exception>
     {
         public void Handle(Exception exception)
---
+++ test/UnitTest.Services/Exceptions/T_Exceptions_Rethrow.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neptuo.Exceptions.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Exceptions
+{
+    [TestClass]
+    public class T_Exceptions_Rethrow
+    {
+        [TestMethod]
+        public void Rethrow()
+        {
+            IExceptionHandler handler = new ExceptionHandlerBuilder()
+                .Handler(new ReThrowExceptionHandler());
+
+            string message1 = null;
+            string message2 = null;
+
+            try
+            {
+                try
+                {
+                    Level1();
+                }
+                catch (Exception e1)
+                {
+                    message1 = e1.ToString();
+                    Assert.IsTrue(message1.Contains("Level3"));
+                    handler.Handle(e1);
+                }
+            }
+            catch (Exception e2)
+            {
+                message2 = e2.ToString();
+                Assert.IsTrue(message1.Contains("Level3"));
+                Assert.IsTrue(message2.StartsWith(message1));
+            }
+        }
+
+        private void Level1()
+        {
+            Level2();
+        }
+
+        private void Level2()
+        {
+            Level3();
+        }
+
+        private void Level3()
+        {
+            throw new InvalidOperationException("Exception");
+        }
+    }
+}
