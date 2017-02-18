fe4f3f6fadbcbff3b75c9ee8e5139e5968605d2b
Marek Fišera <mara@neptuo.com>
2017-02-18 15:07:30 +0100

    Preparing ReThrowExceptionHandler which can be used when no handler is suitable and the exception should be rethrow.

---
+++ src/Neptuo.Exceptions/Handlers/ReThrowExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Exceptions.Handlers
+{
+    public class ReThrowExceptionHandler : IExceptionHandler, IExceptionHandler<Exception>
+    {
+        public void Handle(Exception exception)
+        {
+            ExceptionDispatchInfo info = ExceptionDispatchInfo.Capture(exception);
+            info.Throw();
+        }
+    }
+}
