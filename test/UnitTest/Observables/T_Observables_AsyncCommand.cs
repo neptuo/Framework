8f0c2fe721399502911257460ec14d45c6f055c5
Marek Fišera <mara@neptuo.com>
2017-02-20 20:31:10 +0100

    Add test case for async commands.

---
+++ test/UnitTest/Observables/T_Observables_AsyncCommand.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neptuo.Observables.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Neptuo.Observables
+{
+    [TestClass]
+    public class T_Observables_AsyncCommand
+    {
+        [TestMethod]
+        public void Cancellations()
+        {
+            TestAsyncCommand mainCommand = new TestAsyncCommand();
+            CancelCommand cancelCommand = new CancelCommand(mainCommand);
+
+            mainCommand.Execute();
+            Assert.AreEqual(true, mainCommand.IsRunning);
+            Assert.AreEqual(true, mainCommand.IsPhase1Reached);
+
+            cancelCommand.Execute();
+            Thread.Sleep(10000);
+
+            Assert.AreEqual(false, mainCommand.IsRunning);
+            Assert.AreEqual(false, mainCommand.IsPhase2Reached);
+        }
+    }
+}
--- test/UnitTest/Observables/_Models.cs
+++ test/UnitTest/Observables/_Models.cs
@@ -1,9 +1,11 @@
 ﻿using Neptuo.Linq.Expressions;
+using Neptuo.Observables.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Neptuo.Observables
 {
@@ -44,6 +46,30 @@ namespace Neptuo.Observables
         public string FullName
         {
             get { return FirstName + " " + LastName; }
+        }
+    }
+
+    public class TestAsyncCommand : AsyncCommand
+    {
+        public bool IsExecutable { get; set; } = true;
+
+        public bool IsPhase1Reached { get; set; }
+        public bool IsPhase2Reached { get; set; }
+
+        protected override bool CanExecuteOverride()
+        {
+            return IsExecutable;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            IsPhase1Reached = true;
+            await Task.Delay(500);
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            IsPhase2Reached = true;
+            await Task.Delay(500);
         }
     }
 }
