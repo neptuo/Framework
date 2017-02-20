857bdd5d55db73b39f427d225a8e056e7c7f031b
Marek Fišera <mara@neptuo.com>
2017-02-20 20:20:56 +0100

    Add AsyncCommand and AsyncCommand<T>.

---
+++ src/Neptuo.Observables/Commands/ICancellableCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Neptuo.Observables.Commands
+{
+    /// <summary>
+    /// A command which runs asynchronously and can be cancelled.
+    /// </summary>
+    public interface ICancellableCommand : ICommand
+    {
+        /// <summary>
+        /// Stops execution of the command.
+        /// If not running, nothing should happen.
+        /// </summary>
+        void Cancel();
+    }
+}
