e3ed839c336bf2218cb148b2bf9fde7c24dfd135
Marek Fišera <mara@neptuo.com>
2017-03-06 17:20:21 +0100

    Basic project setup.

---
+++ src/Neptuo.Commands.Validators/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using Neptuo.Commands.Validators;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Neptuo.Commands.Validators")]
+[assembly: AssemblyDescription("Command validation support.")]
+
+[assembly: AssemblyVersion(VersionInfo.Version)]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version + VersionInfo.Preview)]
+[assembly: AssemblyFileVersion(VersionInfo.Version)]
