b57c44178c0099360388a88a4f23f1c52beb7fdb
Marek Fišera <mara@neptuo.com>
2017-03-06 17:53:00 +0100

    Prepare Validators VersionInfo.

--- src/Neptuo.Validators/Properties/VersionInfo.cs
+++ src/Neptuo.Validators/Properties/VersionInfo.cs
@@ -4,7 +4,8 @@ namespace Neptuo.Validators
 {
     public static class VersionInfo
     {
-        internal const string Version = "1.0.0";
+        internal const string Version = "1.1.0";
+        internal const string Preview = "-beta1";
 
         public static Version GetVersion()
         {
