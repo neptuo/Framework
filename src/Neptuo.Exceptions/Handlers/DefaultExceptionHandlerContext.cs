52503f6e9eebd66926da6f7a17140641b7b52bdd
Marek Fišera <mara@neptuo.com>
2017-03-06 21:07:04 +0100

    Add default implementation of the IExceptionHandlerContext.

---
+++ src/Neptuo.Exceptions/Handlers/DefaultExceptionHandlerContext.cs
@@ -0,0 +1,48 @@
+﻿using Neptuo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Exceptions.Handlers
+{
+    /// <summary>
+    /// A default implementation of the <see cref="IExceptionHandlerContext"/>.
+    /// </summary>
+    public class DefaultExceptionHandlerContext : IExceptionHandlerContext
+    {
+        public bool IsHandled { get; set; }
+        public Exception Exception { get; private set; }
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="exception">An exception to handle.</param>
+        public DefaultExceptionHandlerContext(Exception exception)
+        {
+            Ensure.NotNull(exception, "exception");
+            Exception = exception;
+        }
+    }
+
+    /// <summary>
+    /// A default implementation of the <see cref="IExceptionHandlerContext"/>.
+    /// </summary>
+    public class DefaultExceptionHandlerContext<T> : IExceptionHandlerContext<T>
+        where T : Exception
+    {
+        public bool IsHandled { get; set; }
+        public T Exception { get; private set; }
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="exception">An exception to handle.</param>
+        public DefaultExceptionHandlerContext(T exception)
+        {
+            Ensure.NotNull(exception, "exception");
+            Exception = exception;
+        }
+    }
+}
--- src/Neptuo.Exceptions/Handlers/IExceptionHandlerContext.cs
+++ src/Neptuo.Exceptions/Handlers/IExceptionHandlerContext.cs
@@ -9,6 +9,22 @@ namespace Neptuo.Exceptions.Handlers
     /// <summary>
     /// A context for marking exception as handled.
     /// </summary>
+    public interface IExceptionHandlerContext
+    {
+        /// <summary>
+        /// Gets or sets whether the exception is handled.
+        /// </summary>
+        bool IsHandled { get; }
+
+        /// <summary>
+        /// Gets an instance of the exception.
+        /// </summary>
+        Exception Exception { get; }
+    }
+
+    /// <summary>
+    /// A context for marking exception as handled.
+    /// </summary>
     /// <typeparam name="T">A type of the exception.</typeparam>
     public interface IExceptionHandlerContext<T>
         where T : Exception
@@ -21,6 +37,6 @@ namespace Neptuo.Exceptions.Handlers
         /// <summary>
         /// Gets an instance of the exception.
         /// </summary>
-        Exception Exception { get; }
+        T Exception { get; }
     }
 }
