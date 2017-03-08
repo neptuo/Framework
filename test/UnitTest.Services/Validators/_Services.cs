f285861205c988d851fed0600ac26c140eb92177
Marek Fišera <mara@neptuo.com>
2017-03-08 19:01:55 +0100

    Add tests for IValidationHandlerCollection.AddSearchHandler.

---
+++ test/UnitTest.Services/Validators/_Services.cs
@@ -0,0 +1,20 @@
+﻿using Neptuo.Validators.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Validators
+{
+    public class StringValidationHandler : ValidationHandlerBase<string>
+    {
+        protected override void Handle(IValidationResultBuilder builder, string model)
+        {
+            base.Handle(builder, model);
+
+            if (model != "Hello, World!" && model != "Hall Welt!")
+                builder.AddTextMessage(null, "Message be a 'Hello, World!' kind of message.");
+        }
+    }
+}
