bb22967c31992fddc40e55362ee35eaa47439f4b
Marek Fišera <mara@neptuo.com>
2017-03-08 18:57:51 +0100

    Base implementation of IValidationHandler with synchronous support and overload with IValidationResultBuilder as parameter.

--- src/Neptuo.Validators/Handlers/IValidationHandler.cs
+++ src/Neptuo.Validators/Handlers/IValidationHandler.cs
@@ -15,8 +15,8 @@ namespace Neptuo.Validators.Handlers
         /// <summary>
         /// Validates <paramref name="model"/> and returns validation result.
         /// </summary>
-        /// <param name="model">Model instance to validate.</param>
-        /// <returns><see cref="IValidationResult"/> describing succes or validation failure.</returns>
+        /// <param name="model">An instance to validate.</param>
+        /// <returns><see cref="IValidationResult"/> describing success or validation failure.</returns>
         Task<IValidationResult> HandleAsync(TModel model);
     }
 }
