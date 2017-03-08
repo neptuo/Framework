00247d315658a0a018aa19836d0262d5b8a3045c
Marek Fišera <mara@neptuo.com>
2017-03-08 19:15:03 +0100

    Remove beta suffix from version. Update reference to Validators-v1.1.0.

--- src/Neptuo.Commands.Validators/Properties/VersionInfo.cs
+++ src/Neptuo.Commands.Validators/Properties/VersionInfo.cs
@@ -5,7 +5,7 @@ namespace Neptuo.Commands.Validators
     public static class VersionInfo
     {
         internal const string Version = "1.0.0";
-        internal const string Preview = "-beta1";
+        internal const string Preview = null;
 
         public static Version GetVersion()
         {
