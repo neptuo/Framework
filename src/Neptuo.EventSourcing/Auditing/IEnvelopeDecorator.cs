7297e4bbe6b76457720a61546be82fdf1141e00b
Marek Fišera <mara@neptuo.com>
2017-03-14 21:01:56 +0100

    Add interface for auditing decoration.

---
+++ src/Neptuo.EventSourcing/Auditing/IEnvelopeDecorator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Auditing
+{
+    /// <summary>
+    /// An auditing decorator for envelopes just before they a processed.
+    /// </summary>
+    public interface IEnvelopeDecorator
+    {
+        /// <summary>
+        /// Decorates <paramref name="envelope"/> with reqired metadata.
+        /// </summary>
+        /// <param name="envelope">An envelope to decorate.</param>
+        void Decorate(Envelope envelope);
+    }
+}
