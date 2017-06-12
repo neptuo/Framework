04a1d8c348232851ad7825fb5cf06c271374b4e5
Marek Fišera <mara@neptuo.com>
2017-06-12 21:35:09 +0200

    Add test for using repository builder after repository is created.

---
+++ test/UnitTest/Converters/T_Converters_ConverterRepositoryBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Converters
+{
+    [TestClass]
+    public class T_Converters_ConverterRepositoryBuilder
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void UseBuilderAfterRepositoryIsCreated()
+        {
+            Converts.Repository.Add<string, int>(Int32.TryParse);
+            Converts.RepositoryBuilder.UseExceptionSink();
+        }
+    }
+}
