7299956c069261cacdb4451ed2249058fe57d832
Marek Fišera <mara@neptuo.com>
2017-08-24 21:09:09 +0200

    Update KeyTypeCollection to support mapping in one-way direction.

--- src/Neptuo.Models/Keys/KeyTypeCollection.cs
+++ src/Neptuo.Models/Keys/KeyTypeCollection.cs
@@ -6,13 +6,22 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Models.Keys
 {
+    /// <summary>
+    /// An implementation of <see cref="IKeyTypeProvider"/> with manual registration.
+    /// </summary>
     public class KeyTypeCollection : IKeyTypeProvider
     {
         private readonly object storageLock = new object();
         private readonly Dictionary<string, Type> keyTypeToType = new Dictionary<string, Type>();
         private readonly Dictionary<Type, string> typeToKeyType = new Dictionary<Type, string>();
 
-        public KeyTypeCollection Add(string keyType, Type type)
+        /// <summary>
+        /// Adds mapping from <paramref name="keyType"/> to <paramref name="type"/> and vice-versa.
+        /// </summary>
+        /// <param name="keyType">A <see cref="IKey.Type"/> string.</param>
+        /// <param name="type">A clr type.</param>
+        /// <returns>Self (for fluency).</returns>
+        public KeyTypeCollection AddDual(string keyType, Type type)
         {
             Ensure.NotNullOrEmpty(keyType, "keyType");
             Ensure.NotNull(type, "type");
@@ -21,6 +30,44 @@ namespace Neptuo.Models.Keys
             {
                 keyTypeToType[keyType] = type;
                 typeToKeyType[type] = keyType;
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds mapping from <paramref name="keyType"/> to <paramref name="type"/>.
+        /// </summary>
+        /// <param name="keyType">A <see cref="IKey.Type"/> string.</param>
+        /// <param name="type">A clr type.</param>
+        /// <returns>Self (for fluency).</returns>
+        public KeyTypeCollection Add(string keyType, Type type)
+        {
+            Ensure.NotNullOrEmpty(keyType, "keyType");
+            Ensure.NotNull(type, "type");
+
+            lock (storageLock)
+            {
+                keyTypeToType[keyType] = type;
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds mapping from <paramref name="type"/> to <paramref name="keyType"/>.
+        /// </summary>
+        /// <param name="type">A clr type.</param>
+        /// <param name="keyType">A <see cref="IKey.Type"/> string.</param>
+        /// <returns>Self (for fluency).</returns>
+        public KeyTypeCollection Add(Type type, string keyType)
+        {
+            Ensure.NotNull(type, "type");
+            Ensure.NotNullOrEmpty(keyType, "keyType");
+
+            lock (storageLock)
+            {
+                keyTypeToType[keyType] = type;
             }
 
             return this;
