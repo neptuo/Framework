ccf222d0c1d9abe8a616ff786d5814bba3e363a4
Miroslav Bartl <email@miroslav-bartl.cz>
2018-04-07 10:01:23 +0200

    Neptuo.EventSourcing converted to .NET Core 2.0 and version updated to 2.0.0

--- src/Neptuo.EventSourcing/Formatters/CommandExtender.cs
+++ src/Neptuo.EventSourcing/Formatters/CommandExtender.cs
@@ -1,11 +1,6 @@
 ﻿using Neptuo.Collections.Specialized;
 using Neptuo.Commands;
 using Neptuo.Models.Keys;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Neptuo.Formatters
 {
