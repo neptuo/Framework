236303946ff8df56322c24bf93ed4b4d3c0c2902
Miroslav Bartl <email@miroslav-bartl.cz>
2018-04-07 10:03:38 +0200

    Neptuo.Queries converted to .NET Core 2.0 and version updated to 2.0.0

--- src/Neptuo.Queries/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Queries;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Queries")]
-[assembly: AssemblyDescription("Query support.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
