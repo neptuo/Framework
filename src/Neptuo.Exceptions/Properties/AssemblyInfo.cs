95d81163122bc87d55bbc2c3fb866a6989301ccc
Miroslav Bartl <email@miroslav-bartl.cz>
2018-04-07 09:46:05 +0200

    Neptuo.Exceptions converted to .NET Core 2.0 and version updated to 2.0.0

--- src/Neptuo.Exceptions/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Exceptions;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Exceptions")]
-[assembly: AssemblyDescription("Generic exception handling mainly for asynchronous processing.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
