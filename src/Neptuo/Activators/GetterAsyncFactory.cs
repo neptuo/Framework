0e9c23ee399da8abecca0d6d5f37e509d261e5ec
Marek Fišera <mara@neptuo.com>
2018-04-16 21:44:31 +0200

    #53 - Add GetterAsyncFactory implmentation.

--- src/Neptuo/Activators/GetterAsyncFactory.cs
+++ src/Neptuo/Activators/GetterAsyncFactory.cs
@@ -9,7 +9,7 @@ namespace Neptuo.Activators
     /// <summary>
     /// An implementation of the <see cref="IFactory{T}"/> that for every call to create instance calls passed delegate.
     /// </summary>
-    public class GetterFactory<T> : IFactory<T>
+    public class GetterAsyncFactory<T> : IAsyncFactory<T>
     {
         private readonly Func<T> getter;
 
@@ -17,22 +17,20 @@ namespace Neptuo.Activators
         /// Creates new instance that uses <paramref name="getter"/> for providing instances.
         /// </summary>
         /// <param name="getter">An instance provider delegate.</param>
-        public GetterFactory(Func<T> getter)
+        public GetterAsyncFactory(Func<T> getter)
         {
             Ensure.NotNull(getter, "getter");
             this.getter = getter;
         }
 
-        public T Create()
-        {
-            return getter();
-        }
+        public Task<T> Create()
+            => Task.FromResult(getter());
     }
 
     /// <summary>
     /// An implementation of the <see cref="IFactory{T, TContext}"/> that for every call to create instance calls passed delegate.
     /// </summary>
-    public class GetterFactory<T, TContext> : IFactory<T, TContext>
+    public class GetterAsyncFactory<T, TContext> : IAsyncFactory<T, TContext>
     {
         private readonly Func<TContext, T> getter;
 
@@ -40,15 +38,13 @@ namespace Neptuo.Activators
         /// Creates a new instance that uses <paramref name="getter"/> for providing instances.
         /// </summary>
         /// <param name="getter">An instance provider delegate.</param>
-        public GetterFactory(Func<TContext, T> getter)
+        public GetterAsyncFactory(Func<TContext, T> getter)
         {
             Ensure.NotNull(getter, "getter");
             this.getter = getter;
         }
 
-        public T Create(TContext context)
-        {
-            return getter(context);
-        }
+        public Task<T> Create(TContext context)
+            => Task.FromResult(getter(context));
     }
 }
