ebaeb3828e74936d1fcd70193214a59c9da96d71
Marek Fišera <mara@neptuo.com>
2018-04-16 21:37:33 +0200

    #53 - Add IAsyncFactory. Updated comments on IFactory.

--- src/Neptuo/Activators/IAsyncFactory.cs
+++ src/Neptuo/Activators/IAsyncFactory.cs
@@ -7,30 +7,30 @@ using System.Threading.Tasks;
 namespace Neptuo.Activators
 {
     /// <summary>
-    /// An activator for <typeparamref name="T"/>
+    /// An asynchronous activator for <typeparamref name="T"/>
     /// </summary>
-    /// <typeparam name="T">Type of service to create.</typeparam>
-    public interface IFactory<out T>
+    /// <typeparam name="T">A type of the service to create.</typeparam>
+    public interface IAsyncFactory<T>
     {
         /// <summary>
         /// Creates service of type <typeparamref name="T"/>.
         /// </summary>
         /// <returns>An instance of the type <typeparamref name="T"/>.</returns>
-        T Create();
+        Task<T> Create();
     }
 
     /// <summary>
-    /// An activator for <typeparamref name="T"/>.
+    /// An asynchronous activator for <typeparamref name="T"/>.
     /// </summary>
     /// <typeparam name="T">A type of the service to create.</typeparam>
     /// <typeparam name="TContext">A type of the activation parameter.</typeparam>
-    public interface IFactory<out T, in TContext>
+    public interface IAsyncFactory<T, in TContext>
     {
         /// <summary>
         /// Creates a service of the type <typeparamref name="T"/> with <paramref name="context"/> as activation parameter.
         /// </summary>
         /// <param name="context">An activation context.</param>
         /// <returns>An instance of the type <typeparamref name="T"/>.</returns>
-        T Create(TContext context);
+        Task<T> Create(TContext context);
     }
 }
