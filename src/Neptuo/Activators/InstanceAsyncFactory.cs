9bfdd36cbdc45bffbc2fcc2e47ea5cd94acf6589
Marek Fišera <mara@neptuo.com>
2018-04-20 08:45:57 +0200

    #53 - Add InstanceAsyncFactory.

--- src/Neptuo/Activators/InstanceAsyncFactory.cs
+++ src/Neptuo/Activators/InstanceAsyncFactory.cs
@@ -9,8 +9,8 @@ namespace Neptuo.Activators
     /// <summary>
     /// A singleton activator with support for creating singleton from function (on first call).
     /// </summary>
-    /// <typeparam name="T">Type of service to create.</typeparam>
-    public class InstanceFactory<T> : IFactory<T>
+    /// <typeparam name="T">A type of the service to create.</typeparam>
+    public class InstanceAsyncFactory<T> : IAsyncFactory<T>
     {
         private T instance;
         private readonly Func<T> instanceGetter;
@@ -19,7 +19,7 @@ namespace Neptuo.Activators
         /// Creates a new instance from already existing instance.
         /// </summary>
         /// <param name="instance">A singleton object.</param>
-        public InstanceFactory(T instance)
+        public InstanceAsyncFactory(T instance)
         {
             Ensure.NotNull(instance, "instance");
             this.instance = instance;
@@ -28,14 +28,14 @@ namespace Neptuo.Activators
         /// <summary>
         /// Creates a new instance from <paramref name="instanceGetter"/>.
         /// </summary>
-        /// <param name="instanceGetter">A function to access singleton object.</param>
-        public InstanceFactory(Func<T> instanceGetter)
+        /// <param name="instanceGetter">The function to access singleton object.</param>
+        public InstanceAsyncFactory(Func<T> instanceGetter)
         {
             Ensure.NotNull(instanceGetter, "instanceGetter");
             this.instanceGetter = instanceGetter;
         }
 
-        public T Create()
+        public Task<T> Create()
         {
             if (instance == null)
             {
@@ -46,7 +46,7 @@ namespace Neptuo.Activators
                 }
             }
 
-            return instance;
+            return Task.FromResult(instance);
         }
     }
 }
