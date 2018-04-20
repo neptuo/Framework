ca23331965551d9c1790bf96cf68b3927f7c9624
Marek Fišera <mara@neptuo.com>
2018-04-20 12:02:43 +0200

    #57 - SharpKit compiler refactored to new csproj.

--- src/Neptuo.Compilers.SharpKit/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Compilers.SharpKit;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Compilers.SharpKit")]
-[assembly: AssemblyDescription("In-proc SharpKit compiler.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
