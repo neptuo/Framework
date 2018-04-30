469055e057de4b170474f596cf71cb33a281113a
Marek Fišera <mara@neptuo.com>
2018-04-30 22:23:45 +0200

    #77 - Add test case.

--- src/Neptuo/Collections/Generic/_CollectionExtensions.cs
+++ src/Neptuo/Collections/Generic/_CollectionExtensions.cs
@@ -28,5 +28,22 @@ namespace Neptuo.Collections.Generic
 
             return collection;
         }
+
+        /// <summary>
+        /// Adds all items from <paramref name="items"/> to a <paramref name="collection"/>.
+        /// </summary>
+        /// <typeparam name="T">A type of the item.</typeparam>
+        /// <param name="collection">A target collection to add items to.</param>
+        /// <param name="items">An enumeration of items to add to <paramref name="collection"/>.</param>
+        /// <returns><paramref name="collection"/> with added items.</returns>
+        public static ICollection<T> AddRange<T>(this ICollection<T> collection, params T[] items)
+        {
+            Ensure.NotNull(collection, "collection");
+            Ensure.NotNull(items, "items");
+            foreach (T item in items)
+                collection.Add(item);
+
+            return collection;
+        }
     }
 }
---
+++ test/UnitTest/Collections/Specialized/DisposableCollectionTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neptuo.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Collections.Specialized
+{
+    [TestClass]
+    public class DisposableCollectionTest
+    {
+        [TestMethod]
+        public void Basic()
+        {
+            Disposable d1 = new Disposable();
+            Disposable d2 = new Disposable();
+
+            using (DisposableCollection collection = new DisposableCollection())
+                collection.AddRange(d1, d2);
+
+            Assert.IsTrue(d1.IsDisposed);
+            Assert.IsTrue(d2.IsDisposed);
+        }
+    }
+
+    public class Disposable : DisposableBase
+    {
+        protected override void DisposeManagedResources()
+        {
+            base.DisposeManagedResources();
+        }
+    }
+}
