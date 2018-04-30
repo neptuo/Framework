9fc975ce4db539f38539d25dbe0fb7e5fcca5fde
Marek Fišera <mara@neptuo.com>
2018-04-30 22:17:06 +0200

    #77 - Add DisposableCollection.

---
+++ src/Neptuo/Collections/Generic/_CollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Neptuo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Collections.Generic
+{
+    /// <summary>
+    /// A common extensions for <see cref="ICollection{T}"/>.
+    /// </summary>
+    public static class _CollectionExtensions
+    {
+        /// <summary>
+        /// Adds all items from <paramref name="items"/> to a <paramref name="collection"/>.
+        /// </summary>
+        /// <typeparam name="T">A type of the item.</typeparam>
+        /// <param name="collection">A target collection to add items to.</param>
+        /// <param name="items">An enumeration of items to add to <paramref name="collection"/>.</param>
+        /// <returns><paramref name="collection"/> with added items.</returns>
+        public static ICollection<T> AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            Ensure.NotNull(collection, "collection");
+            Ensure.NotNull(items, "items");
+            foreach (T item in items)
+                collection.Add(item);
+
+            return collection;
+        }
+    }
+}
---
+++ src/Neptuo/Collections/Specialized/DisposableCollection.cs
@@ -0,0 +1,71 @@
+﻿using Neptuo;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Collections.Specialized
+{
+    /// <summary>
+    /// A collection of disposable objects.
+    /// </summary>
+    public class DisposableCollection : ICollection<IDisposable>, IDisposable
+    {
+        private readonly List<IDisposable> storage = new List<IDisposable>();
+
+        /// <summary>
+        /// Adds <paramref name="item"/> to a list of objects to dispose.
+        /// </summary>
+        /// <param name="item">An item to add.</param>
+        /// <returns>Self (for fluency).</returns>
+        public DisposableCollection Add(IDisposable item)
+        {
+            Ensure.NotNull(item, "item");
+            storage.Add(item);
+            return this;
+        }
+
+        /// <summary>
+        /// Disposes all registered objects and removes them from the collection.
+        /// The collection can be than reused.
+        /// </summary>
+        public void Dispose()
+        {
+            foreach (IDisposable disposable in storage)
+                disposable.Dispose();
+
+            Clear();
+        }
+
+        #region ICollection<IDisposable>
+
+        public int Count => storage.Count;
+
+        public bool IsReadOnly => false;
+
+        void ICollection<IDisposable>.Add(IDisposable item)
+            => Add(item);
+
+        public void Clear()
+            => storage.Clear();
+
+        public bool Contains(IDisposable item)
+            => storage.Contains(item);
+
+        public void CopyTo(IDisposable[] array, int arrayIndex)
+            => storage.CopyTo(array, arrayIndex);
+
+        public bool Remove(IDisposable item)
+            => storage.Remove(item);
+
+        public IEnumerator<IDisposable> GetEnumerator()
+            => storage.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        #endregion
+    }
+}
