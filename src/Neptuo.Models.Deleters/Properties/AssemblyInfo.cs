d55c5ec912d15daa4b85899da2ee267de09906e6
Marek Fišera <mara@neptuo.com>
2018-05-07 22:46:58 +0200

    #74 - New csproj format for Neptuo.Models.Deleters.

--- src/Neptuo.Models.Deleters/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Models.Deleters;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Models.Deleters")]
-[assembly: AssemblyDescription("Deleter facade.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
