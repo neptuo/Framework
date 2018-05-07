afaff254481e92cf560667c4427258e157deca00
Marek Fišera <mara@neptuo.com>
2018-05-07 22:35:01 +0200

    #74 - New csproj format for Neptuo.Models.

--- src/Neptuo.Models/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Models;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Models")]
-[assembly: AssemblyDescription("Neptuo support for creating and accessing anemic and domain models.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
