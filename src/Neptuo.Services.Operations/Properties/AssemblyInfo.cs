5932fee42e57b3d981712007458c4b629f357c62
Marek Fišera <mara@neptuo.com>
2018-05-07 22:00:46 +0200

    #78 - Delete Neptuo.Services.Operations.

--- src/Neptuo.Services.Operations/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Services.Operations;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Services.Operations")]
-[assembly: AssemblyDescription("Request-reply support.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
