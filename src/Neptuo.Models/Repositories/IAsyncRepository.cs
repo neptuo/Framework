2bd807b2b9324d63e2f72a23bf1103f0cb4b0bfc
Marek Fišera <mara@neptuo.com>
2018-05-07 22:41:54 +0200

    #71 - Async repository and read-only repository.

--- src/Neptuo.Models/Repositories/IAsyncRepository.cs
+++ src/Neptuo.Models/Repositories/IAsyncRepository.cs
@@ -8,19 +8,19 @@ using System.Threading.Tasks;
 namespace Neptuo.Models.Repositories
 {
     /// <summary>
-    /// Describes contract for getting domain models by its key.
+    /// Describes contract for getting and storing domain models by its key (using TPL).
     /// </summary>
     /// <typeparam name="TDomainModel">A type of the domain model.</typeparam>
     /// <typeparam name="TKey">A type of the domain model key.</typeparam>
-    public interface IReadOnlyRepository<TDomainModel, in TKey>
+    public interface IAsyncRepository<TDomainModel, in TKey> : IReadOnlyRepository<TDomainModel, TKey>
         where TKey : IKey
         where TDomainModel : IDomainModel<TKey>
     {
         /// <summary>
-        /// Tries to find model with the <paramref name="key"/>.
+        /// Saves changes to the <paramref name="model"/> to the underlaying storage.
         /// </summary>
-        /// <param name="key">A key of the model to find.</param>
-        /// <returns>An instance of the model with the key; <c>null</c> if such model doesn't exist.</returns>
-        TDomainModel Find(TKey key);
+        /// <param name="model">An instance of the model to save.</param>
+        /// <returns>A continuation task.</returns>
+        Task SaveAsync(TDomainModel model);
     }
 }
--- src/Neptuo.Models/Repositories/IRepository.cs
+++ src/Neptuo.Models/Repositories/IRepository.cs
@@ -10,8 +10,8 @@ namespace Neptuo.Models.Repositories
     /// <summary>
     /// Describes contract for getting and storing domain models by its key.
     /// </summary>
-    /// <typeparam name="TDomainModel">The type of the domain model.</typeparam>
-    /// <typeparam name="TKey">The type of the domain model key.</typeparam>
+    /// <typeparam name="TDomainModel">A type of the domain model.</typeparam>
+    /// <typeparam name="TKey">A type of the domain model key.</typeparam>
     public interface IRepository<TDomainModel, in TKey> : IReadOnlyRepository<TDomainModel, TKey>
         where TKey : IKey
         where TDomainModel : IDomainModel<TKey>
@@ -19,7 +19,7 @@ namespace Neptuo.Models.Repositories
         /// <summary>
         /// Saves changes to the <paramref name="model"/> to the underlaying storage.
         /// </summary>
-        /// <param name="model">The instance of the model to save.</param>
+        /// <param name="model">A instance of the model to save.</param>
         void Save(TDomainModel model);
     }
 }
