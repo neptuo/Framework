1ec9a3b7d2bbfd29f87a0b323c20b094c764ade9
Marek Fišera <mara@neptuo.com>
2018-05-08 21:15:49 +0200

    #74 - New csproj format for Neptuo.Behaviors.

--- src/Neptuo.Behaviors/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Behaviors;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Behaviors")]
-[assembly: AssemblyDescription("Generic (compilable) interception pipeline.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
