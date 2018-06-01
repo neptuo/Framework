c6d9796d6730baa5fcb72d949195235dde19d0c1
Marek Fišera <mara@neptuo.com>
2018-06-01 11:55:40 +0200

    #74 - New csproj format for Neptuo.Observables.

--- src/Neptuo.Observables/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Observables;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Observables")]
-[assembly: AssemblyDescription("Observable objects, collections and commands.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
