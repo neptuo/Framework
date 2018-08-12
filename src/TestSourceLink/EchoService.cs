337e28b84ea72624a05898c89439ae55dc3cfe9d
Marek Fišera <mara@neptuo.com>
2018-08-12 16:56:49 +0200

    #99 - Testing SourceLink support.

---
+++ src/TestSourceLink/EchoService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestSourceLink
+{
+    /// <summary>
+    /// An echo service.
+    /// </summary>
+    public class EchoService
+    {
+        /// <summary>
+        /// Outputs greeting.
+        /// </summary>
+        /// <param name="name">A caller name.</param>
+        public static void SayHello(string name) => Console.WriteLine($"Hello, {name}!");
+    }
+}
