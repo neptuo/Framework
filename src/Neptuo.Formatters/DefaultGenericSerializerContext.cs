346ffe3edd2c25c0eda69c4f5e7343f9f908373d
Marek Fišera <mara@neptuo.com>
2018-08-13 18:47:41 +0200

    #56 - Default implementation of generic serializer context.

---
+++ src/Neptuo.Formatters/DefaultGenericSerializerContext.cs
@@ -0,0 +1,46 @@
+﻿using Neptuo.Collections.Specialized;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Formatters
+{
+    /// <summary>
+    /// A default implementation of <see cref="IGenericSerializerContext"/>
+    /// </summary>
+    public class DefaultGenericSerializerContext : IGenericSerializerContext
+    {
+        public Stream Output { get; private set; }
+
+        private KeyValueCollection metadata;
+
+        public IKeyValueCollection Metadata
+        {
+            get
+            {
+                if (metadata == null)
+                    metadata = new KeyValueCollection();
+
+                return metadata;
+            }
+        }
+
+        IReadOnlyKeyValueCollection IGenericSerializerContext.Metadata
+        {
+            get { return Metadata; }
+        }
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="output">A serialization output.</param>
+        public DefaultGenericSerializerContext(Stream output)
+        {
+            Ensure.NotNull(output, "output");
+            Output = output;
+        }
+    }
+}
--- src/Neptuo.Formatters/DefaultSerializerContext.cs
+++ src/Neptuo.Formatters/DefaultSerializerContext.cs
@@ -1,5 +1,4 @@
-﻿using Neptuo.Collections.Specialized;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,42 +8,22 @@ using System.Threading.Tasks;
 namespace Neptuo.Formatters
 {
     /// <summary>
-    /// Default implementation of <see cref="ISerializerContext"/>.
+    /// A default implementation of <see cref="ISerializerContext"/>.
     /// </summary>
-    public class DefaultSerializerContext : ISerializerContext
+    public class DefaultSerializerContext : DefaultGenericSerializerContext, ISerializerContext
     {
-        public Stream Output { get; private set; }
         public Type InputType { get; private set; }
 
-        private KeyValueCollection metadata;
-
-        public IKeyValueCollection Metadata
-        {
-            get
-            {
-                if (metadata == null)
-                    metadata = new KeyValueCollection();
-
-                return metadata;
-            }
-        }
-
-        IReadOnlyKeyValueCollection ISerializerContext.Metadata
-        {
-            get { return Metadata; }
-        }
-
         /// <summary>
-        /// Creates new instance.
+        /// Creates a new instance.
         /// </summary>
-        /// <param name="inputType">The type to serialize.</param>
-        /// <param name="output">The serialization output.</param>
+        /// <param name="inputType">A type to serialize.</param>
+        /// <param name="output">A serialization output.</param>
         public DefaultSerializerContext(Type inputType, Stream output)
+            : base(output)
         {
             Ensure.NotNull(inputType, "inputType");
-            Ensure.NotNull(output, "output");
             InputType = inputType;
-            Output = output;
         }
     }
 }
