f3e73007110ac8262015925873e31058346d1620
Marek Fišera <mara@neptuo.com>
2018-08-13 18:49:15 +0200

    #56 - Default implementation of generic deserializer context.

--- src/Neptuo.Formatters/DefaultDeserializerContext.cs
+++ src/Neptuo.Formatters/DefaultDeserializerContext.cs
@@ -1,5 +1,4 @@
-﻿using Neptuo.Collections.Specialized;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,35 +7,16 @@ using System.Threading.Tasks;
 namespace Neptuo.Formatters
 {
     /// <summary>
-    /// Default implementation of <see cref="IDeserializerContext"/>.
+    /// A default implementation of <see cref="IDeserializerContext"/>.
     /// </summary>
-    public class DefaultDeserializerContext : IDeserializerContext
+    public class DefaultDeserializerContext : DefaultGenericDeserializerContext, IDeserializerContext
     {
-        public object Output { get; set; }
         public Type OutputType { get; private set; }
 
-        private KeyValueCollection metadata;
-
-        public IKeyValueCollection Metadata
-        {
-            get
-            {
-                if (metadata == null)
-                    metadata = new KeyValueCollection();
-
-                return metadata;
-            }
-        }
-
-        IReadOnlyKeyValueCollection IDeserializerContext.Metadata
-        {
-            get { return Metadata; }
-        }
-
         /// <summary>
-        /// Creates new instance.
+        /// Creates a new instance.
         /// </summary>
-        /// <param name="outputType">The type that is required to deserialize.</param>
+        /// <param name="outputType">A type that is required to deserialize.</param>
         public DefaultDeserializerContext(Type outputType)
         {
             Ensure.NotNull(outputType, "outputType");
---
+++ src/Neptuo.Formatters/DefaultGenericDeserializerContext.cs
@@ -0,0 +1,35 @@
+﻿using Neptuo.Collections.Specialized;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Formatters
+{
+    /// <summary>
+    /// A default implementation of <see cref="IGenericDeserializerContext"/>.
+    /// </summary>
+    public class DefaultGenericDeserializerContext : IGenericDeserializerContext
+    {
+        public object Output { get; set; }
+
+        private KeyValueCollection metadata;
+
+        public IKeyValueCollection Metadata
+        {
+            get
+            {
+                if (metadata == null)
+                    metadata = new KeyValueCollection();
+
+                return metadata;
+            }
+        }
+
+        IReadOnlyKeyValueCollection IGenericDeserializerContext.Metadata
+        {
+            get { return Metadata; }
+        }
+    }
+}
