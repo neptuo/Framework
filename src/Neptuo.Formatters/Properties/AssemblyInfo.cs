6d2ca7a2a72e1c0d6e15d5916a8a34aa21d5f0a9
Marek Fišera <mara@neptuo.com>
2018-08-13 18:26:39 +0200

    #74 - New csproj format for Neptuo.Formatters.

--- src/Neptuo.Formatters/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Formatters;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Formatters")]
-[assembly: AssemblyDescription("Serialization and deserialization formatters for Neptuo.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
