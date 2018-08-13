e44af2fadc55b9fbea3d5c9857f6e58bf82df5b8
Marek Fišera <mara@neptuo.com>
2018-08-13 21:57:09 +0200

    #74 - New csproj format for Neptuo.Formatters.Json.

--- src/Neptuo.Formatters.Json/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Formatters.Json;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Formatters.Json")]
-[assembly: AssemblyDescription("JSON implementation of Neptuo.Formatters.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
