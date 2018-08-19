013f4e699be2e3527ba6c17137a29e452fa951c0
Marek Fišera <mara@neptuo.com>
2018-08-19 15:51:54 +0200

    #56 - CompositeModelFormatter for generic formatter.

---
+++ src/Neptuo.Formatters.Composite/Generics/CompositeModelFormatter.cs
@@ -0,0 +1,135 @@
+﻿using Neptuo.Activators;
+using Neptuo.Collections.Specialized;
+using Neptuo.TypeMapping;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Formatters.Generics
+{
+    /// <summary>
+    /// An implementation of <see cref="IFormatter"/> which works on types implementing <see cref="ICompositeModel"/>.
+    /// </summary>
+    public class CompositeModelFormatter : IFormatter, ISerializer, IDeserializer
+    {
+        /// <summary>
+        /// Nnames of the keys used in store/load methods.
+        /// </summary>
+        protected static class Name
+        {
+            /// <summary>
+            /// A name where the name of the type is stored.
+            /// </summary>
+            public const string Type = "Type";
+
+            /// <summary>
+            /// A name where the content of instance is stored.
+            /// </summary>
+            public const string Body = "Body";
+        }
+
+        private readonly Func<Type, object> modelFactory;
+        private readonly IFactory<ICompositeStorage> storageFactory;
+        private readonly ITypeNameMapper typeNameMapper;
+
+        /// <summary>
+        /// Creates a new instance with factories from model and storages.
+        /// </summary>
+        /// <param name="modelFactory">A factory for empty models when deserializing.</param>
+        /// <param name="storageFactory">A factory for empty composite storages.</param>
+        /// <param name="typeNameMapper">A type name mapper.</param>
+        public CompositeModelFormatter(Func<Type, object> modelFactory, IFactory<ICompositeStorage> storageFactory, ITypeNameMapper typeNameMapper)
+        {
+            Ensure.NotNull(modelFactory, "modelFactory");
+            Ensure.NotNull(storageFactory, "storageFactory");
+            Ensure.NotNull(typeNameMapper, "typeNameMapper");
+            this.modelFactory = modelFactory;
+            this.storageFactory = storageFactory;
+            this.typeNameMapper = typeNameMapper;
+        }
+
+        private ICompositeStorage TrySerializeInternal(object input, ISerializerContext context)
+        {
+            Ensure.NotNull(input, "input");
+            Ensure.NotNull(context, "context");
+
+            ICompositeModel model = input as ICompositeModel;
+            if (model == null)
+                return null;
+
+            ICompositeStorage storage = storageFactory.Create();
+
+            if (!typeNameMapper.TryGet(model.GetType(), out string typeName))
+                return null;
+
+            storage.Add(Name.Type, typeName);
+            ICompositeStorage childStorage = storage.Add(Name.Body);
+
+            model.Save(childStorage);
+            return storage;
+        }
+
+        public async Task<bool> TrySerializeAsync(object input, ISerializerContext context)
+        {
+            ICompositeStorage storage = TrySerializeInternal(input, context);
+            if (storage == null)
+                return false;
+
+            await storage.StoreAsync(context.Output).ConfigureAwait(false);
+            return true;
+        }
+
+        public bool TrySerialize(object input, ISerializerContext context)
+        {
+            ICompositeStorage storage = TrySerializeInternal(input, context);
+            if (storage == null)
+                return false;
+
+            storage.Store(context.Output);
+            return true;
+        }
+
+        private bool TryDeserializeInternal(IDeserializerContext context, ICompositeStorage storage)
+        {
+            string typeName = storage.Get<string>(Name.Type);
+            if (!typeNameMapper.TryGet(typeName, out Type outputType))
+                return false;
+
+            if (!storage.TryGet(Name.Body, out ICompositeStorage childStorage))
+                return false;
+
+            ICompositeModel model = modelFactory.Invoke(outputType) as ICompositeModel;
+            if (model == null)
+                return false;
+
+            model.Load(childStorage);
+            context.Output = model;
+            return true;
+        }
+        
+        public async Task<bool> TryDeserializeAsync(Stream input, IDeserializerContext context)
+        {
+            Ensure.NotNull(input, "input");
+            Ensure.NotNull(context, "context");
+
+            ICompositeStorage storage = storageFactory.Create();
+            await storage.LoadAsync(input).ConfigureAwait(false);
+
+            return TryDeserializeInternal(context, storage);
+        }
+
+        public bool TryDeserialize(Stream input, IDeserializerContext context)
+        {
+            Ensure.NotNull(input, "input");
+            Ensure.NotNull(context, "context");
+
+            ICompositeStorage storage = storageFactory.Create();
+            storage.Load(input);
+
+            return TryDeserializeInternal(context, storage);
+        }
+    }
+}
