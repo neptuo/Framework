563026d678746e5d79b8525b68437b77553c2745
Marek Fišera <mara@neptuo.com>
2018-10-15 19:20:33 +0200

    #118 - Add IgnoreExceptionHandler.

---
+++ src/Neptuo.Exceptions/Handlers/IgnoreExceptionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Exceptions.Handlers
+{
+    /// <summary>
+    /// An exception handler which marks all exceptions as handled.
+    /// </summary>
+    /// <typeparam name="T">A type of the exception.</typeparam>
+    public class IgnoreExceptionHandler : IExceptionHandler<IExceptionHandlerContext>
+    {
+        public void Handle(IExceptionHandlerContext context)
+            => context.IsHandled = true;
+    }
+
+    /// <summary>
+    /// An exception handler which marks all exceptions as handled.
+    /// </summary>
+    /// <typeparam name="T">A type of the exception.</typeparam>
+    public class IgnoreExceptionHandler<T> : IExceptionHandler<IExceptionHandlerContext<T>>
+        where T : Exception
+    {
+        public void Handle(IExceptionHandlerContext<T> context)
+            => context.IsHandled = true;
+    }
+}
