f6d6d0fc849fccef1432f126a69214fbfcc7f6e9
Marek Fišera <mara@neptuo.com>
2018-11-09 16:34:16 +0100

    #74 - New csproj format for Neptuo.Localization.

--- src/Neptuo.Localization/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Localization;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Localization")]
-[assembly: AssemblyDescription("Support for localization based on dictionaries and culture info.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
