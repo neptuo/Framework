589d00e7811db11e425a0cc9518e45b80a7427b2
Marek Fišera <mara@neptuo.com>
2018-11-09 16:41:18 +0100

    #74 - New csproj format for Neptuo.Logging.Log4net.

--- src/Neptuo.Logging.Log4net/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Logging.Log4net;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Logging.Log4net")]
-[assembly: AssemblyDescription("Log4net implementation of Neptuo.Logging.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
