dd3ddd2948e35bc58908a372d5d499463a34ad0d
Marek Fišera <mara@neptuo.com>
2019-01-07 22:30:47 +0100

    #74 - New csproj format for Neptuo.Bootstrap.

--- src/Neptuo.Bootstrap/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Bootstrap;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Bootstrap")]
-[assembly: AssemblyDescription("Application bootstrapper.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
