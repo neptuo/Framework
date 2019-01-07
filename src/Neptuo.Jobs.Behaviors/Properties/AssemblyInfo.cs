8500efa6ae599ac627785b46cac0f81111f7209f
Marek Fišera <mara@neptuo.com>
2019-01-07 22:20:02 +0100

    #78 - Remove projects Neptuo.Jobs and Neptuo.Jobs.Behaviors.

--- src/Neptuo.Jobs.Behaviors/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Jobs.Behaviors;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Jobs.Behaviors")]
-[assembly: AssemblyDescription("Behaviors extension for Jobs.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
--- src/Neptuo.Jobs/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Jobs;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Jobs")]
-[assembly: AssemblyDescription("Application (backend and recurring) services support.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
