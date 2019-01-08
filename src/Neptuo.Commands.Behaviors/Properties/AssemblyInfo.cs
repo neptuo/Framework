8f90d1b7ca9ccdd93774243266713ec2f3935d50
Marek Fišera <mara@neptuo.com>
2019-01-08 18:45:16 +0100

    #74 - New csproj format for Neptuo.Commands.Behaviors.

--- src/Neptuo.Commands.Behaviors/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Commands.Behaviors;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Commands.Behaviors")]
-[assembly: AssemblyDescription("Behaviors extension for command handling.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
