c335ad13cfd916f031da1c18bf90b365f34c3927
Marek Fišera <mara@neptuo.com>
2019-01-08 18:51:39 +0100

    #74 - New csproj format for Neptuo.Queries.Behaviors.

--- src/Neptuo.Queries.Behaviors/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Queries.Behaviors;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Queries.Behaviors")]
-[assembly: AssemblyDescription("Behaviors extension for query handling.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
