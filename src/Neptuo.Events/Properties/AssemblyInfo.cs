a91cce95d8a5c3a7ee5a75e7186fe2e017bda214
Marek Fišera <mara@neptuo.com>
2019-01-08 18:47:38 +0100

    #74 - New csproj format for Neptuo.Events.

--- src/Neptuo.Events/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Events;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Events")]
-[assembly: AssemblyDescription("Event support.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
