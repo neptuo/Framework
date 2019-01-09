d53386a3ad6a798ea97dc971a81854c948934333
Marek Fišera <mara@neptuo.com>
2019-01-09 18:04:38 +0100

    #74 - New csproj format for Neptuo.PresentationModels.TypeModels.DataAnnotations.

--- src/Neptuo.PresentationModels.TypeModels.DataAnnotations/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.PresentationModels.TypeModels.DataAnnotations;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.PresentationModels.TypeModels.DataAnnotations")]
-[assembly: AssemblyDescription("Support for using DataAnnotations as PresentationModels metadata.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
