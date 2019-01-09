1c742761e179049a1cca19764fb688a2dbf3f40e
Marek Fišera <mara@neptuo.com>
2019-01-09 18:01:12 +0100

    #74 - New csproj format for Neptuo.PresentationModels.Validators.

--- src/Neptuo.PresentationModels.Validators/FieldMetadataValidatorCollection.cs
+++ src/Neptuo.PresentationModels.Validators/FieldMetadataValidatorCollection.cs
@@ -1,4 +1,5 @@
 ﻿using Neptuo.Activators;
+using Neptuo.Collections.Specialized;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- src/Neptuo.PresentationModels.Validators/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.PresentationModels.Validators;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.PresentationModels.Validators")]
-[assembly: AssemblyDescription("Validation support for PresentationModels.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
