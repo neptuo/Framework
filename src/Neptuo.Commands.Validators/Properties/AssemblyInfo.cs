f1ba251e758686bad0ee0f98fbb1ef333e9a7b37
Marek Fišera <mara@neptuo.com>
2019-01-09 18:18:49 +0100

    #74 - Add root namespace to Neptuo.Commands.Validators.

--- src/Neptuo.Commands.Validators/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.Commands.Validators;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.Commands.Validators")]
-[assembly: AssemblyDescription("Command validation support.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version + VersionInfo.Preview)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
