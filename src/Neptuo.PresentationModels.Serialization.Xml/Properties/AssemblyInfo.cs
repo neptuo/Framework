44a667585a13342da88aaa4082f459eadf13679c
Marek Fišera <mara@neptuo.com>
2019-01-09 18:07:13 +0100

    #74 - New csproj format for Neptuo.PresentationModels.Serialization.Xml.

--- src/Neptuo.PresentationModels.Serialization.Xml/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.PresentationModels.Serialization.Xml;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.PresentationModels.Serialization.Xml")]
-[assembly: AssemblyDescription("XML Serialization and deserialization support for model definitions and values.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
--- src/Neptuo.PresentationModels.Serialization.Xml/XmlTypeMappingCollection.cs
+++ src/Neptuo.PresentationModels.Serialization.Xml/XmlTypeMappingCollection.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Neptuo.Collections.Specialized;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
