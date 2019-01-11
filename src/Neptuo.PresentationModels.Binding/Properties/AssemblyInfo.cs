ba5c9128fb9d0ea73e36987d1fcdd46e1429413e
Marek Fišera <mara@neptuo.com>
2019-01-11 13:00:41 +0100

    #78 - Remove Neptuo.PresentationModels.Binding.

--- src/Neptuo.PresentationModels.Binding/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Neptuo.PresentationModels.Binding;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Neptuo.PresentationModels.Binding")]
-[assembly: AssemblyDescription("Binding (eg. web request) converters for provinding model values.")]
-
-[assembly: AssemblyVersion(VersionInfo.Version)]
-[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
-[assembly: AssemblyFileVersion(VersionInfo.Version)]
