415b7b24e29126a97b2c6fb24784071b32990f66
Marek Fišera <mara@neptuo.com>
2019-02-04 17:30:15 +0100

    #66 - Rewrite CommandExtender to match pattern from the AggregateRootException.

--- src/Neptuo.EventSourcing/Formatters/CommandExtender.cs
+++ src/Neptuo.EventSourcing/Formatters/CommandExtender.cs
@@ -10,39 +10,15 @@ using System.Threading.Tasks;
 namespace Neptuo.Formatters
 {
     /// <summary>
-    /// Stores/Loads all-commands-shared properties with internal setters.
+    /// Provides methods for setting <see cref="Command"/> key.
     /// </summary>
     public class CommandExtender
     {
         /// <summary>
-        /// The names of the keys used in store/load methods.
+        /// Sets <see cref="Command.Key"/> to a <paramref name="commandKey"/>.
         /// </summary>
-        protected static class Name
-        {
-            /// <summary>
-            /// The name where the key of the command is stored.
-            /// </summary>
-            public const string Key = "Key";
-        }
-
-        /// <summary>
-        /// Stores <paramref name="payload"/> properties to the <paramref name="storage"/>.
-        /// </summary>
-        /// <param name="storage">The storage to save values to.</param>
-        /// <param name="payload">The command payload to store.</param>
-        public void Store(IKeyValueCollection storage, Command payload)
-        {
-            storage.Add(Name.Key, payload.Key);
-        }
-
-        /// <summary>
-        /// Loads <paramref name="payload"/> properties from the <paramref name="storage"/>.
-        /// </summary>
-        /// <param name="storage">The storage to load values from.</param>
-        /// <param name="payload">The command payload to load.</param>
-        public void Load(IReadOnlyKeyValueCollection storage, Command payload)
-        {
-            payload.Key = storage.Get<IKey>(Name.Key);
-        }
+        /// <param name="payload">An command to change.</param>
+        /// <param name="commandKey">A key to set as the command key.</param>
+        public void SetKey(Command payload, IKey commandKey) => payload.Key = commandKey;
     }
 }
