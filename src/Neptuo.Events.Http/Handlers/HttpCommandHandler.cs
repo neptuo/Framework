5d2339189db4010d312641727cf9f6d7030dfea3
Marek Fišera <mara@neptuo.com>
2019-02-17 12:13:25 +0100

    #147 - Http support for events.

---
+++ src/Neptuo.Events.Http/Handlers/HttpCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Neptuo.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Events.Handlers
+{
+    /// <summary>
+    /// An implementation of <see cref="IEventHandler{TEvent}"/> that transfers events over HTTP.
+    /// </summary>
+    /// <typeparam name="TEvent">A type of the event.</typeparam>
+    public class HttpEventHandler<TEvent> : IEventHandler<TEvent>
+    {
+        private readonly HttpEventDispatcher dispatcher;
+
+        /// <summary>
+        /// Creates a new instance that sends events of type <typeparamref name="TEvent"/> throught <paramref name="objectSender"/>.
+        /// </summary>
+        /// <param name="objectSender">An object sender.</param>
+        public HttpEventHandler(ObjectSender objectSender) => dispatcher = new HttpEventDispatcher(objectSender);
+
+        public Task HandleAsync(TEvent payload) => dispatcher.PublishAsync(payload);
+    }
+}
---
+++ src/Neptuo.Events.Http/HttpEventDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using Neptuo.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Events
+{
+    /// <summary>
+    /// An implementation of <see cref="ICommandDispatcher"/> that transfers commands over HTTP.
+    /// </summary>
+    public class HttpEventDispatcher : IEventDispatcher
+    {
+        private readonly ObjectSender objectSender;
+
+        /// <summary>
+        /// Creates a new instance which sends objects using <paramref name="objectSender"/>.
+        /// </summary>
+        /// <param name="objectSender">An object sender.</param>
+        public HttpEventDispatcher(ObjectSender objectSender)
+        {
+            Ensure.NotNull(objectSender, "objectSender");
+            this.objectSender = objectSender;
+        }
+
+        public Task PublishAsync<TEvent>(TEvent payload)
+        {
+            Ensure.NotNull(payload, "payload");
+            return objectSender.SendAsync(payload, default);
+        }
+    }
+}
